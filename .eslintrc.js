405abe2290ba4e36dfab3a794af7cc3898093fb8
Daithi Hearn <daithi.hearn@gmail.com>
2023-01-13 12:22:36 +0100

    chore: configuring double quotes on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
                 "no-shadow": "off",
                 "no-undef": "off",
                 semi: "off",
+                quotes: [2, "double", { avoidEscape: true }],
             },
         },
     ],
--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,64 +1,70 @@
-import type {BaseTranslation} from '../i18n-types'
+import type { BaseTranslation } from "../i18n-types"
 
 const en: BaseTranslation = {
     // dynamic example
     // HI: 'Hi {name:string}! This is a test translation',
 
     // BUTTONS
-    BTN_GET_STARTED: 'GET STARTED',
-    BTN_ONBOARDING_SLIDE_01: 'NEXT: SUSTAINABLE',
-    BTN_ONBOARDING_SLIDE_02: 'NEXT: SAFE AND FAST',
-    BTN_ONBOARDING_SLIDE_03: 'NEXT: CREATE PASSWORD',
-    BTN_ONBOARDING_SKIP: 'Skip ahead to create password',
-    BTN_WALLET_TUTORIAL_SLIDE_01: 'NEXT: CUSTODY',
-    BTN_WALLET_TUTORIAL_SLIDE_02: 'NEXT: SAFETY',
-    BTN_WALLET_TUTORIAL_SLIDE_03: 'NEXT: SECRET PHRASE',
-    BTN_WALLET_TUTORIAL_SKIP: 'Skip ahead to recovery phrase',
-    BTN_SECURTY_USE_TYPE: 'Use {type:string}',
-    BTN_SECURITY_CREATE_PASSWORD: 'Create password',
-    BTN_CREATE_WALLET_TYPE_CREATE_NEW: 'Create new wallet',
-    BTN_CREATE_WALLET_TYPE_IMPORT: 'Import wallet',
-    BTN_MNEMONIC_BACKUP: 'Backup',
-    BTN_MNEMONIC_CHECKBOX: 'I state that I have saved the secret phrase and I am aware that I am responsible if I lose it.',
-    BTN_MNEMONIC_CLIPBOARD: 'Copy mnemonic to clipboard',
+    BTN_GET_STARTED: "GET STARTED",
+    BTN_ONBOARDING_SLIDE_01: "NEXT: SUSTAINABLE",
+    BTN_ONBOARDING_SLIDE_02: "NEXT: SAFE AND FAST",
+    BTN_ONBOARDING_SLIDE_03: "NEXT: CREATE PASSWORD",
+    BTN_ONBOARDING_SKIP: "Skip ahead to create password",
+    BTN_WALLET_TUTORIAL_SLIDE_01: "NEXT: CUSTODY",
+    BTN_WALLET_TUTORIAL_SLIDE_02: "NEXT: SAFETY",
+    BTN_WALLET_TUTORIAL_SLIDE_03: "NEXT: SECRET PHRASE",
+    BTN_WALLET_TUTORIAL_SKIP: "Skip ahead to recovery phrase",
+    BTN_SECURTY_USE_TYPE: "Use {type:string}",
+    BTN_SECURITY_CREATE_PASSWORD: "Create password",
+    BTN_CREATE_WALLET_TYPE_CREATE_NEW: "Create new wallet",
+    BTN_CREATE_WALLET_TYPE_IMPORT: "Import wallet",
+    BTN_MNEMONIC_BACKUP: "Backup",
+    BTN_MNEMONIC_CHECKBOX:
+        "I state that I have saved the secret phrase and I am aware that I am responsible if I lose it.",
+    BTN_MNEMONIC_CLIPBOARD: "Copy mnemonic to clipboard",
 
     // BODY
     BD_GDPR:
-        'By pressing ‘get started’ you are agreeing to VeWorld’s Terms and conditions and Privacy policy, compliant with Art. 5 GDPR (Required)',
+        "By pressing ‘get started’ you are agreeing to VeWorld’s Terms and conditions and Privacy policy, compliant with Art. 5 GDPR (Required)",
     BD_WELCOME_SCREEN:
-        'The Future of Sustainable Self-Custody Web extension Wallet, Enabled By VeChain’s Advanced Blockchain Technology',
+        "The Future of Sustainable Self-Custody Web extension Wallet, Enabled By VeChain’s Advanced Blockchain Technology",
     BD_ONBOARDING_SLIDE_01:
-        'Viewing and managing your crypto assets has never been easier.',
+        "Viewing and managing your crypto assets has never been easier.",
     BD_ONBOARDING_SLIDE_02:
-        'Using Innovative Green Technology, Continues Building The Future of Safe.',
+        "Using Innovative Green Technology, Continues Building The Future of Safe.",
     BD_ONBOARDING_SLIDE_03:
-        'Wallet Extension provides a simpler and secured way to manage your coins.',
-    BD_WALLET_TUTORIAL_SLIDE_01: 'The secret phrase is your only way to access your funds. This will be needed when importing your crypto wallet from or to VeWorld.',
-    BD_WALLET_TUTORIAL_SLIDE_02: 'If you lose this phrase you will lose access to all the funds available on your wallet. You should never share your secret phrase.',
-    BD_WALLET_TUTORIAL_SLIDE_03: 'You can write it down and store it somewhere safe, save it in an encrypted password manager or best of all memorize and never write it down.',
-    BD_MNEMONIC_BACKUP: 'Store this phrase in a password manager, write it down or memorize it.',
-    BD_MNEMONIC_DISCLAIMER: 'Never disclose your Secret Recovery Phrase. Anyone with this phrase can take your VeChain cryptos forever.',
-    BD_MNEMONIC_SUBTITLE: 'Your Secret Recovery Phrase makes it easy to back up and restore your account.',
+        "Wallet Extension provides a simpler and secured way to manage your coins.",
+    BD_WALLET_TUTORIAL_SLIDE_01:
+        "The secret phrase is your only way to access your funds. This will be needed when importing your crypto wallet from or to VeWorld.",
+    BD_WALLET_TUTORIAL_SLIDE_02:
+        "If you lose this phrase you will lose access to all the funds available on your wallet. You should never share your secret phrase.",
+    BD_WALLET_TUTORIAL_SLIDE_03:
+        "You can write it down and store it somewhere safe, save it in an encrypted password manager or best of all memorize and never write it down.",
+    BD_MNEMONIC_BACKUP:
+        "Store this phrase in a password manager, write it down or memorize it.",
+    BD_MNEMONIC_DISCLAIMER:
+        "Never disclose your Secret Recovery Phrase. Anyone with this phrase can take your VeChain cryptos forever.",
+    BD_MNEMONIC_SUBTITLE:
+        "Your Secret Recovery Phrase makes it easy to back up and restore your account.",
 
     // TITLES
-    TITLE_WELCOME_TO: 'Welcome to ',
-    TITLE_ONBARDING_SLIDE_01: 'Seamlessly manage your crypto',
-    TITLE_ONBARDING_SLIDE_02: 'Sustainable',
-    TITLE_ONBARDING_SLIDE_03: 'Customizable, safe and fast',
-    TITLE_SECURITY: 'Protect your wallet',
-    TITLE_WALLET_TUTORIAL_SLIDE_01: 'Secret recovery phrase',
-    TITLE_WALLET_TUTORIAL_SLIDE_02: 'The key to your crypto',
-    TITLE_WALLET_TUTORIAL_SLIDE_03: 'Keep your phrase safe',
-    TITLE_CREATE_WALLET_TYPE: 'Create Wallet',
-    TITLE_MNEMONIC: 'Your Mnemonic',
+    TITLE_WELCOME_TO: "Welcome to ",
+    TITLE_ONBARDING_SLIDE_01: "Seamlessly manage your crypto",
+    TITLE_ONBARDING_SLIDE_02: "Sustainable",
+    TITLE_ONBARDING_SLIDE_03: "Customizable, safe and fast",
+    TITLE_SECURITY: "Protect your wallet",
+    TITLE_WALLET_TUTORIAL_SLIDE_01: "Secret recovery phrase",
+    TITLE_WALLET_TUTORIAL_SLIDE_02: "The key to your crypto",
+    TITLE_WALLET_TUTORIAL_SLIDE_03: "Keep your phrase safe",
+    TITLE_CREATE_WALLET_TYPE: "Create Wallet",
+    TITLE_MNEMONIC: "Your Mnemonic",
 
     // ELEMENTS
-    FACE_ID: 'Face ID',
-    FINGERPRINT: 'Fingerprint',
-    IRIS: 'Iris',
-    DEVICE_PIN: 'Device Pin',
-    TAP_TO_VIEW: 'Tap to view',
-
+    FACE_ID: "Face ID",
+    FINGERPRINT: "Fingerprint",
+    IRIS: "Iris",
+    DEVICE_PIN: "Device Pin",
+    TAP_TO_VIEW: "Tap to view",
 }
 
 export default en
