febfd8e61a9906d1339da88270ddd0848177124e
Darren Kelly <darren.kelly@vechain.org>
2023-01-14 17:13:23 +0000

    chore: basic cucumber running - tests are skipped

---
+++ e2e/step_definitions/open_mobile.ts
@@ -0,0 +1,9 @@
+import { Given, Then } from "@cucumber/cucumber"
+
+Given("The user opens the app", function () {
+    return "skipped"
+})
+
+Then("The user should see the welcome screen", function () {
+    return "skipped"
+})
