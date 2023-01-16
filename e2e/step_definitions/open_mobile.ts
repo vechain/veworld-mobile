afb954ef858618e0c91a704f96ef7cad167a4526
grenos <vasilisgreen@icloud.com>
2023-01-16 22:02:55 +0200

    test(detox): add testID for initial test

--- e2e/step_definitions/open_mobile.ts
+++ e2e/step_definitions/open_mobile.ts
@@ -1,10 +1,13 @@
 import { Given, Then } from "@cucumber/cucumber"
-import detox from "detox"
+import { waitFor } from "detox"
 
 Given("The user opens the app", function () {
     //TODO: Not implemented
 })
 
 Then("The user should see the welcome screen", async function () {
-    await detox.element(by.text("GET STARTED")).tap()
+    await element(by.id("GET_STARTED_BTN")).tap()
+    await waitFor(element(by.id("ONBOARDING_SCREEN")))
+        .toExist()
+        .withTimeout(2000)
 })
