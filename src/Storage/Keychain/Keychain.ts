1b8f5980d63f534240272f8911d436b728c7c888
grenos <vasilisgreen@icloud.com>
2023-02-13 10:29:26 +0100

    fix(biometrics): fix double faceid prompt

--- src/Storage/Keychain/Keychain.ts
+++ src/Storage/Keychain/Keychain.ts
@@ -8,11 +8,20 @@ export async function set(encKey: string, accessControl: boolean) {
     const locale = i18n.detectLocale()
     let promptTitle = i18n.i18n()[locale].BIOMETRICS_PROMPT()
 
-    let options = {
-        requireAuthentication: accessControl,
-        keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
-        keychainService: "VeWorld_Wallet_key",
-        authenticationPrompt: promptTitle.toString(),
+    let options: any
+
+    if (accessControl) {
+        options = {
+            requireAuthentication: accessControl,
+            keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+            keychainService: "VeWorld_Wallet_key",
+            authenticationPrompt: promptTitle.toString(),
+        }
+    } else {
+        options = {
+            keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+            keychainService: "VeWorld_Wallet_key",
+        }
     }
 
     await SecureStore.setItemAsync("VeWorld_Wallet_key", encKey, options)
@@ -22,11 +31,20 @@ export async function get(accessControl: boolean): Promise<string | null> {
     const locale = i18n.detectLocale()
     let promptTitle = i18n.i18n()[locale].BIOMETRICS_PROMPT()
 
-    let options = {
-        requireAuthentication: accessControl,
-        keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
-        keychainService: "VeWorld_Wallet_key",
-        authenticationPrompt: promptTitle.toString(),
+    let options: any
+
+    if (accessControl) {
+        options = {
+            requireAuthentication: accessControl,
+            keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+            keychainService: "VeWorld_Wallet_key",
+            authenticationPrompt: promptTitle.toString(),
+        }
+    } else {
+        options = {
+            keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+            keychainService: "VeWorld_Wallet_key",
+        }
     }
 
     return await SecureStore.getItemAsync("VeWorld_Wallet_key", options)
