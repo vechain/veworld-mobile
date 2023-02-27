2c7fac3e509158a74578665d03dd178553b37992
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-02-27 12:59:57 +0100

    refactor(i18n): remove currently unused translation keys

--- src/i18n/i18n-util.ts
+++ src/i18n/i18n-util.ts
@@ -11,7 +11,7 @@ import type { Formatters, Locales, Translations, TranslationFunctions } from './
 export const baseLocale: Locales = 'en'
 
 export const locales: Locales[] = [
-	'en',
+	'en'
 ]
 
 export const isLocale = (locale: string): locale is Locales => locales.includes(locale as Locales)
