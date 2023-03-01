547ae0c55aaf900036c79a3ae64c7cd89cbf82b3
ve-akanoce <erik.nucibella@vechain.org>
2023-03-01 19:23:57 +0100

    fix: bottomSheet handle customization and polish

--- src/Components/Base/BaseBottomSheet.tsx
+++ src/Components/Base/BaseBottomSheet.tsx
@@ -3,13 +3,13 @@ import { StyleSheet } from "react-native"
 import {
     BottomSheetBackdrop,
     BottomSheetBackdropProps,
+    BottomSheetHandleProps,
     BottomSheetModal,
     BottomSheetProps,
 } from "@gorhom/bottom-sheet"
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types"
 import { BaseView } from "./BaseView"
-import { useThemedStyles } from "~Common"
-import { ThemeType } from "~Model"
+import { ColorThemeType, useThemedStyles } from "~Common"
 
 type Props = BottomSheetProps & {
     children: React.ReactNode
@@ -31,6 +31,13 @@ const BaseBottomSheet = React.forwardRef<BottomSheetModalMethods, Props>(
             [],
         )
 
+        const renderHandle = useCallback(
+            (props_: BottomSheetHandleProps) => (
+                <BaseView {...props_} style={styles.handleStyle} />
+            ),
+            [styles],
+        )
+
         return (
             <BottomSheetModal
                 ref={ref}
@@ -38,6 +45,7 @@ const BaseBottomSheet = React.forwardRef<BottomSheetModalMethods, Props>(
                 index={0}
                 backgroundStyle={[styles.backgroundStyle]}
                 backdropComponent={renderBackdrop}
+                handleComponent={renderHandle}
                 {...props}>
                 <BaseView w={100} p={24} align="flex-start">
                     {children}
@@ -47,12 +55,20 @@ const BaseBottomSheet = React.forwardRef<BottomSheetModalMethods, Props>(
     },
 )
 
-const baseStyles = (theme: ThemeType) =>
+const baseStyles = (theme: ColorThemeType) =>
     StyleSheet.create({
         backgroundStyle: {
             backgroundColor: theme.colors.background,
             borderTopLeftRadius: 24,
             borderTopRightRadius: 24,
+        },
+        handleStyle: {
+            width: 60,
+            height: 4,
+            borderRadius: 8,
+            backgroundColor: theme.colors.text,
+            alignSelf: "center",
+            marginTop: 24,
         },
     })
 
