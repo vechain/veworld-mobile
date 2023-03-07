f7739507a21fbec5063532bcb4a1935d3c3c6df5
ve-akanoce <erik.nucibella@vechain.org>
2023-03-07 17:46:47 +0100

    feat(WalletManagement): back navigation button

--- src/Screens/Flows/App/WalletManagementScreen/WalletManagementScreen.tsx
+++ src/Screens/Flows/App/WalletManagementScreen/WalletManagementScreen.tsx
@@ -1,12 +1,25 @@
-import React from "react"
+import { useNavigation, useTheme } from "@react-navigation/native"
+import React, { useCallback } from "react"
 
 import { SafeAreaView } from "react-native"
+import { BaseIcon, BaseSpacer } from "~Components"
 import { WalletManagementHeader } from "./components"
 
 export const WalletManagementScreen = () => {
+    const nav = useNavigation()
+    const theme = useTheme()
+
+    const goBack = useCallback(() => nav.goBack(), [nav])
     return (
         <>
             <SafeAreaView />
+            <BaseIcon
+                style={{ paddingHorizontal: 20 }}
+                name="arrow-back-outline"
+                color={theme.colors.text}
+                action={goBack}
+            />
+            <BaseSpacer height={20} />
             <WalletManagementHeader />
         </>
     )
