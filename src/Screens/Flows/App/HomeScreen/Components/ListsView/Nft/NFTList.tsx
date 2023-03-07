2afc660335e9d11b73dcb5c4cbec6e71e6948538
Vasilis Green <Vasilis.green@gmail.com>
2023-03-07 18:18:13 +0100

    feat(nft): add nft list (#163)

--- src/Screens/Flows/App/HomeScreen/Components/ListsView/Nft/NFTList.tsx
+++ src/Screens/Flows/App/HomeScreen/Components/ListsView/Nft/NFTList.tsx
@@ -1,9 +1,12 @@
-import React, { memo, useCallback, useState } from "react"
+import React, { memo, useCallback } from "react"
 import { StyleSheet } from "react-native"
-import { FlatList, ViewProps } from "react-native"
+import { ViewProps } from "react-native"
 import Animated, { AnimateProps } from "react-native-reanimated"
 import { ColorThemeType, useThemedStyles } from "~Common"
-import { BaseSpacer, BaseText, BaseView } from "~Components"
+import { BaseText, BaseView } from "~Components"
+import { NFTAccordion } from "./NFTAccordion"
+import { FlashList } from "@shopify/flash-list"
+import DropShadow from "react-native-drop-shadow"
 
 const NUM_ITEMS = 20
 function getColor(i: number) {
@@ -31,40 +34,78 @@ const initialData: Item[] = [...Array(NUM_ITEMS)].map((d, index) => {
     }
 })
 
+const ARRAY_LIST = [
+    { title: "Not Boored Apes", list: initialData },
+    { title: "Erik's Adventures", list: initialData },
+    { title: "Daje Roma", list: initialData },
+    { title: "Pokemon", list: initialData },
+]
+
 interface Props extends AnimateProps<ViewProps> {}
 
 export const NFTList = memo(({ ...animatedViewProps }: Props) => {
-    const [data] = useState(initialData)
-
     const { styles } = useThemedStyles(baseStyles)
 
-    const renderSeparator = useCallback(() => <BaseSpacer height={16} />, [])
+    const renderSeparator = useCallback(
+        () => <BaseView style={{ width: 16 }} />,
+        [],
+    )
 
     return (
         <Animated.View style={styles.container} {...animatedViewProps}>
-            <FlatList
-                data={data}
-                numColumns={2}
-                keyExtractor={item => item.key}
-                columnWrapperStyle={{
-                    justifyContent: "space-evenly",
-                }}
-                ItemSeparatorComponent={renderSeparator}
-                renderItem={({ item }) => {
-                    return (
-                        <BaseView
-                            style={[
-                                styles.nftCard,
-                                {
-                                    backgroundColor: item.backgroundColor,
-                                },
-                            ]}>
-                            <BaseText>{item.label}</BaseText>
-                        </BaseView>
-                    )
-                }}
-                showsVerticalScrollIndicator={false}
-            />
+            {ARRAY_LIST.map((data, index) => (
+                <BaseView key={index} style={{ paddingVertical: 12 }}>
+                    <NFTAccordion
+                        headerComponent={
+                            <BaseView orientation="row" align="center" px={20}>
+                                <BaseView style={styles.nftPreviewImage} />
+                                <BaseText typographyFont="subTitle">
+                                    {data.title}
+                                </BaseText>
+                            </BaseView>
+                        }
+                        bodyComponent={
+                            <BaseView w={100} style={{ height: 184 }}>
+                                <FlashList
+                                    data={data.list}
+                                    keyExtractor={item => item.key}
+                                    ItemSeparatorComponent={renderSeparator}
+                                    contentContainerStyle={styles.listContainer}
+                                    renderItem={({ item }) => {
+                                        return (
+                                            <DropShadow
+                                                style={styles.cardDhadow}>
+                                                <BaseView
+                                                    style={[
+                                                        styles.nftCard,
+                                                        {
+                                                            backgroundColor:
+                                                                item.backgroundColor,
+                                                        },
+                                                    ]}>
+                                                    <BaseText>
+                                                        {item.label}
+                                                    </BaseText>
+                                                </BaseView>
+                                            </DropShadow>
+                                        )
+                                    }}
+                                    showsVerticalScrollIndicator={false}
+                                    showsHorizontalScrollIndicator={false}
+                                    horizontal
+                                    estimatedItemSize={152}
+                                    estimatedListSize={{
+                                        height: 184,
+                                        width:
+                                            152 * NUM_ITEMS +
+                                            (NUM_ITEMS - 1) * 16,
+                                    }}
+                                />
+                            </BaseView>
+                        }
+                    />
+                </BaseView>
+            ))}
         </Animated.View>
     )
 })
@@ -75,9 +116,27 @@ const baseStyles = (theme: ColorThemeType) =>
             width: "100%",
             backgroundColor: theme.colors.background,
         },
+        listContainer: {
+            paddingVertical: 16,
+            paddingHorizontal: 20,
+        },
         nftCard: {
             width: 152,
             height: 152,
+            borderRadius: 16,
             backgroundColor: theme.colors.card,
+            overflow: "hidden",
+            justifyContent: "center",
+            alignItems: "center",
         },
+
+        nftPreviewImage: {
+            width: 32,
+            height: 32,
+            backgroundColor: "pink",
+            borderRadius: 16,
+            marginRight: 10,
+        },
+
+        cardDhadow: theme.shadows.nftCard,
     })
