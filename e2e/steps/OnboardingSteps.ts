e687c298b24e887b334c501edc7c4c48d0c037ad
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-03-08 10:11:17 +0100

    test(e2e): onboarding process

--- e2e/steps/OnboardingSteps.ts
+++ e2e/steps/OnboardingSteps.ts
@@ -1,22 +1,47 @@
-import { Given, Then } from "@cucumber/cucumber"
-import { waitFor } from "detox"
+import { Given, Then, When } from "@cucumber/cucumber"
+import { waitFor, element } from "detox"
+import OnboardingFlows from "../helpers/flows/OnboardingFlows"
 
 Given("The user follows the onboarding process", async () => {
+    await OnboardingFlows.onboard()
+})
+
+When("The user skips to password creation", async () => {
     await element(by.text("GET STARTED")).tap()
 
-    await waitFor(element(by.text("NEXT: SUSTAINABLE")))
-        .toExist()
-        .withTimeout(2_000)
-    await element(by.text("NEXT: SUSTAINABLE")).tap()
+    await element(by.text("Skip ahead to create password")).tap()
+})
 
-    await waitFor(element(by.text("NEXT: SAFE AND FAST")))
-        .toExist()
-        .withTimeout(2_000)
-    await element(by.text("NEXT: SAFE AND FAST")).tap()
+Given("The user follows the wallet creation process", async () => {
+    await OnboardingFlows.onboard()
+
+    await element(by.text("NEXT: CREATE PASSWORD")).tap()
+
+    await OnboardingFlows.createWallet()
+})
+
+When("The user skips to recovery phase", async () => {
+    await element(by.text("NEXT: CREATE PASSWORD")).tap()
+
+    await element(by.text("Create new wallet")).tap()
+
+    await element(by.text("Skip ahead to recovery phrase")).tap()
 })
 
 Then("The user should be onboarded", async () => {
     await waitFor(element(by.text("NEXT: CREATE PASSWORD")))
+        .toBeVisible()
+        .withTimeout(2_000)
+})
+
+Then("The user should see password creation", async () => {
+    await waitFor(element(by.text("Create Wallet")))
+        .toBeVisible()
+        .withTimeout(2_000)
+})
+
+Then("The user can create wallet", async () => {
+    await waitFor(element(by.text("Your Mnemonic")))
         .toBeVisible()
         .withTimeout(2_000)
 })
