b2e3acf5e843ff10b564cdd929e1148c0e7ac5c7
Davide Carpini <davide.carpini@vechain.org>
2023-03-10 10:03:28 +0100

    fix: add width to the spacer

--- src/Components/Base/BaseSpacer.tsx
+++ src/Components/Base/BaseSpacer.tsx
@@ -4,16 +4,20 @@ import { useTheme } from "~Common"
 import { BaseView } from "./BaseView"
 
 type Props = {
-    height: number
+    height?: number
+    width?: number
     background?: string
 } & ViewProps
 
 export const BaseSpacer: FC<Props> = (props: Props) => {
     const { style, ...otherProps } = props
     const theme = useTheme()
+    if (!props.height && !props.width) {
+        throw new Error("BaseSpacer: height and width are not provided")
+    }
     return (
         <BaseView
-            style={[{ height: props.height }, style]}
+            style={[{ height: props.height, width: props.width }, style]}
             background={
                 props.background ? props.background : theme.colors.transparent
             }
