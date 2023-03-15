37559dee1ef80f8a90eb336b8e57531de6a2159f
ve-akanoce <erik.nucibella@vechain.org>
2023-03-15 17:29:25 +0100

    fix(Settings): heading of PrivacyScreen

--- src/Screens/Flows/App/PrivacyScreen/PrivacyScreen.tsx
+++ src/Screens/Flows/App/PrivacyScreen/PrivacyScreen.tsx
@@ -1,14 +1,46 @@
-import React from "react"
-import { BaseSafeArea, BaseView } from "~Components"
+import { useNavigation } from "@react-navigation/native"
+import React, { useCallback } from "react"
+import { StyleSheet } from "react-native"
+import { useTheme } from "~Common"
+import {
+    BaseIcon,
+    BaseSafeArea,
+    BaseSpacer,
+    BaseText,
+    BaseView,
+} from "~Components"
+import { useI18nContext } from "~i18n"
 import { EnableBiometrics, SecureApp } from "./Components"
 
 export const PrivacyScreen = () => {
+    const nav = useNavigation()
+    const theme = useTheme()
+    const { LL } = useI18nContext()
+
+    const goBack = useCallback(() => nav.goBack(), [nav])
     return (
         <BaseSafeArea grow={1}>
+            <BaseIcon
+                style={baseStyles.backIcon}
+                size={36}
+                name="chevron-left"
+                color={theme.colors.text}
+                action={goBack}
+            />
+            <BaseSpacer height={12} />
             <BaseView mx={20}>
+                <BaseText typographyFont="title">{LL.TITLE_PRIVACY()}</BaseText>
+                <BaseSpacer height={24} />
                 <SecureApp />
                 <EnableBiometrics />
             </BaseView>
         </BaseSafeArea>
     )
 }
+
+const baseStyles = StyleSheet.create({
+    backIcon: {
+        marginHorizontal: 8,
+        alignSelf: "flex-start",
+    },
+})
