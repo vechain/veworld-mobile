f1366da1e67b35c04541b6235ee16e718aec6b89
ve-akanoce <erik.nucibella@vechain.org>
2023-03-15 17:18:53 +0100

    fix(SettingsScreen): polish styles

--- src/Screens/Flows/App/SettingsScreen/SettingsScreen.tsx
+++ src/Screens/Flows/App/SettingsScreen/SettingsScreen.tsx
@@ -5,6 +5,7 @@ import { Routes } from "~Navigation"
 import { FlashList } from "@shopify/flash-list"
 import { StyleSheet, View, ViewToken } from "react-native"
 import { RowProps, SettingsRow } from "./Components/SettingsRow"
+import { ColorThemeType, useThemedStyles } from "~Common"
 
 export const SettingsScreen = () => {
     const { LL } = useI18nContext()
@@ -13,9 +14,11 @@ export const SettingsScreen = () => {
 
     const [isScrollable, setIsScrollable] = useState(false)
 
-    const Seperator = useCallback(
-        () => <View style={baseStyles.seperator} />,
-        [],
+    const { styles: themedStyles } = useThemedStyles(baseStyles)
+
+    const renderSeparator = useCallback(
+        () => <View style={themedStyles.separator} />,
+        [themedStyles],
     )
 
     const checkViewableItems = useCallback(
@@ -29,7 +32,7 @@ export const SettingsScreen = () => {
         [SCREEN_LIST.length],
     )
 
-    const _renderItem = useCallback(
+    const renderItem = useCallback(
         ({ item }: { item: RowProps }) => (
             <SettingsRow
                 title={item.title}
@@ -50,13 +53,13 @@ export const SettingsScreen = () => {
 
             <FlashList
                 data={SCREEN_LIST}
-                contentContainerStyle={baseStyles.contentContainerStyle}
-                ItemSeparatorComponent={Seperator}
+                contentContainerStyle={themedStyles.contentContainerStyle}
+                ItemSeparatorComponent={renderSeparator}
                 estimatedItemSize={56}
                 scrollEnabled={isScrollable}
                 keyExtractor={item => item.screenName}
                 onViewableItemsChanged={checkViewableItems}
-                renderItem={_renderItem}
+                renderItem={renderItem}
             />
 
             <BaseSpacer height={40} />
@@ -64,15 +67,16 @@ export const SettingsScreen = () => {
     )
 }
 
-const baseStyles = StyleSheet.create({
-    contentContainerStyle: {
-        paddingHorizontal: 20,
-    },
-    seperator: {
-        borderBottomColor: "rgba(0,0,0,0.1)",
-        borderBottomWidth: 1,
-    },
-})
+const baseStyles = (theme: ColorThemeType) =>
+    StyleSheet.create({
+        contentContainerStyle: {
+            paddingHorizontal: 20,
+        },
+        separator: {
+            borderBottomColor: theme.colors.text,
+            borderBottomWidth: 0.5,
+        },
+    })
 
 const getList = (LL: TranslationFunctions) => [
     {
