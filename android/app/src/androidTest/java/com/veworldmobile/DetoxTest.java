ac7810719c32ba41886223a9a7e1cfb3e4fa25db
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-03-16 10:56:51 +0100

    test(e2e): add e2e android  workflow

--- android/app/src/androidTest/java/com/veworldmobile/DetoxTest.java
+++ android/app/src/androidTest/java/com/veworldmobile/DetoxTest.java
@@ -9,13 +9,14 @@ import org.junit.runner.RunWith;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
+//import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class DetoxTest {
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, false, false);
+    public ActivityScenarioRule<MainActivity> mActivityRule = new ActivityScenarioRule<>(MainActivity.class, false, false);
 
     @Test
     public void runDetoxTests() {
