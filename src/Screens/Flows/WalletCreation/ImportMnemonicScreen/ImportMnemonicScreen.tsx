a2b381531f6702698eeb872935152eadb1b65040
ve-akanoce <erik.nucibella@vechain.org>
2023-03-21 10:21:00 +0100

    fix: names

--- src/Screens/Flows/WalletCreation/ImportMnemonicScreen/ImportMnemonicScreen.tsx
+++ src/Screens/Flows/WalletCreation/ImportMnemonicScreen/ImportMnemonicScreen.tsx
@@ -14,7 +14,7 @@ import { CryptoUtils, SeedUtils, useDeviceUtils, useTheme } from "~Common"
 import { Keyboard } from "react-native"
 import { getConfig, getMnemonic, useRealm } from "~Storage"
 import { Routes } from "~Navigation"
-import { ImportMnemonicView } from "./Components/ImportMnemonicView"
+import { ImportMnemonicView } from "./Components/ImportMnemonicInput"
 import { useNavigation } from "@react-navigation/native"
 
 const DEMO_MNEMONIC =
