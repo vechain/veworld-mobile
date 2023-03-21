8f69dc84a68d5c84feee1747db9796e4c7428c98
Piero Bassa <pierobassa222@gmail.com>
2023-03-21 15:35:04 +0100

    feat: added languageMap (ISO 639-1 code to human readable language name)

---
+++ src/Model/Language/Languages.ts
@@ -0,0 +1,15 @@
+/**
+ * Map of ISO 639-1 language codes to human readable language names.
+ */
+export const languageMap: { [key: string]: string } = {
+    en: "English",
+    es: "Spanish",
+    fr: "French",
+    de: "German",
+    pt: "Portuguese",
+    it: "Italian",
+    nl: "Dutch",
+    ja: "Japanese",
+    ko: "Korean",
+    zh: "Chinese",
+}
