d286cc9def452e7e911867b40f04c0cb449f4e2d
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-03-21 19:52:15 +0100

    test(e2e): add e2e android  workflow

--- e2e/config/TestHooks.ts
+++ e2e/config/TestHooks.ts
@@ -1,33 +1,23 @@
 import { AfterAll, Before, BeforeAll } from "@cucumber/cucumber"
 import * as detoxConfig from "detox/internals"
 
-//const DEFAULT_TIMEOUT = 300 * 1000
-
 BeforeAll({ timeout: 600 * 1000 }, async () => {
     console.log("Starting a new Detox test session...")
     await detoxConfig.init()
     console.log("Detox test session started!")
 
-    if (detox.device.getPlatform() !== "android") {
+    if (detox.device.getPlatform() === "ios") {
         console.log("Launching app...")
         await detox.device.launchApp()
         console.log("App launched!")
     }
 })
 
-Before({ timeout: 600 * 1000 }, async function (feature) {
-    // Do not reload app for the "app-lifecycle" tagged tests
-    // as reloading is part of the test itself and is handled manually
-    if (feature.pickle.tags.some(tag => tag.name !== "@app-lifecycle")) {
-        // On android, reloading RN bundle closes realms, causing the tests to crash
-    }
-    if (detox.device.getPlatform() === "android") {
+Before({ timeout: 300 * 1000 }, async () => {
+    if (detox.device.getPlatform() === "ios") {
         console.log("Relaunching app before test...")
-        await detox.device.terminateApp()
-        await detox.device.launchApp({ newInstance: true })
-        console.log("App relaunched!")
-    } else {
         await detox.device.reloadReactNative()
+        console.log("App relaunched!")
     }
 })
 
