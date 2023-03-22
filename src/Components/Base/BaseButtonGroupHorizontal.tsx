b807eba0668de11d7792c32bf9029b64ee98d99d
Piero Bassa <pierobassa222@gmail.com>
2023-03-22 15:18:43 +0100

    refactor: moved text and icon as children of BaseButton

--- src/Components/Base/BaseButtonGroupHorizontal.tsx
+++ src/Components/Base/BaseButtonGroupHorizontal.tsx
@@ -50,57 +50,44 @@ export const BaseButtonGroupHorizontal = ({
                 {buttons.map((button, _) => {
                     const { id, label, disabled, icon } = button
                     const selected = selectedButtonIds.includes(id)
+                    const bgColor = selected
+                        ? theme.colors.primary
+                        : theme.colors.card
+                    const textIconColor = selected
+                        ? theme.colors.card
+                        : theme.colors.text
 
                     return (
                         <BaseButton
                             key={id}
                             action={onPress(button)}
                             disabled={disabled}
-                            title={
-                                <BaseView
-                                    justifyContent="center"
-                                    alignItems="center"
-                                    flexDirection="row"
-                                    style={{
-                                        marginBottom: -5,
-                                    }}>
-                                    {icon && (
-                                        <BaseIcon
-                                            size={18}
-                                            name={icon}
-                                            color={
-                                                selected
-                                                    ? theme.colors.card
-                                                    : theme.colors.text
-                                            }
-                                        />
-                                    )}
-                                    <BaseText
-                                        color={
-                                            selected
-                                                ? theme.colors.card
-                                                : theme.colors.text
-                                        }
-                                        typographyFont="buttonPrimary"
-                                        px={5}>
-                                        {label}
-                                    </BaseText>
-                                </BaseView>
-                            }
-                            bgColor={
-                                selected
-                                    ? theme.colors.primary
-                                    : theme.colors.card
-                            }
-                            textColor={
-                                selected
-                                    ? theme.colors.textReversed
-                                    : theme.colors.text
-                            }
+                            bgColor={bgColor}
                             typographyFont="bodyMedium"
                             w={buttonWidth}
-                            style={styles.buttonStyle}
-                        />
+                            style={styles.buttonStyle}>
+                            <BaseView
+                                justifyContent="center"
+                                alignItems="center"
+                                flexDirection="row"
+                                style={{
+                                    marginBottom: -5,
+                                }}>
+                                {icon && (
+                                    <BaseIcon
+                                        size={18}
+                                        name={icon}
+                                        color={textIconColor}
+                                    />
+                                )}
+                                <BaseText
+                                    color={textIconColor}
+                                    typographyFont="buttonPrimary"
+                                    px={5}>
+                                    {label}
+                                </BaseText>
+                            </BaseView>
+                        </BaseButton>
                     )
                 })}
             </BaseView>
