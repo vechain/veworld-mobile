db8a7f8ee5d52eb8b12782a67587a6cf91c5cc79
Piero Bassa <pierobassa222@gmail.com>
2023-03-23 14:42:54 +0100

    feat(redux): added currency & language selectors

--- src/Storage/Redux/Selectors/UserPreferences.ts
+++ src/Storage/Redux/Selectors/UserPreferences.ts
@@ -6,3 +6,11 @@ const reducer = (state: RootState) => state.userPreferences
 export const selectTheme = createSelector(reducer, state => {
     return state.theme
 })
+
+export const selectCurrency = createSelector(reducer, state => {
+    return state.currency
+})
+
+export const selectLangauge = createSelector(reducer, state => {
+    return state.language
+})
