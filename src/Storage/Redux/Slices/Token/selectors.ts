691cd693903566f21a4d9718efe0742e8b70a044
ve-akanoce <erik.nucibella@vechain.org>
2023-03-28 18:46:11 +0200

    fix: import in token selector

--- src/Storage/Redux/Slices/Token/selectors.ts
+++ src/Storage/Redux/Slices/Token/selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from "@reduxjs/toolkit"
 import { FungibleToken } from "~Model"
-import { getCurrentNetwork } from "~Storage/Redux/Selectors"
+import { getSelectedNetwork } from "~Storage/Redux/Selectors"
 import { RootState } from "~Storage/Redux/Types"
 
 export const getAllTokens = (state: RootState) => state.tokens
@@ -15,7 +15,7 @@ export const getAllFungibleTokens = createSelector(
  */
 export const getNetworkFungibleTokens = createSelector(
     getAllFungibleTokens,
-    getCurrentNetwork,
+    getSelectedNetwork,
     (tokens, network) =>
         tokens.filter(
             (token: FungibleToken) => token.genesisId === network.genesisId,
