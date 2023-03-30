d104f888db19138ff1453665f7b27d0cecfb1fae
ve-akanoce <erik.nucibella@vechain.org>
2023-03-30 12:42:15 +0200

    fix: baseIcon test

--- src/Components/Base/BaseIcon.test.tsx
+++ src/Components/Base/BaseIcon.test.tsx
@@ -1,26 +1,24 @@
 import { TestWrapper } from "~Test"
 import React from "react"
-import {
-    render,
-    fireEvent,
-    screen,
-    waitFor,
-} from "@testing-library/react-native"
+import { render, fireEvent, screen } from "@testing-library/react-native"
 import { BaseIcon } from "./BaseIcon"
 
-const findIcon = async () => await screen.findByTestId("BaseIcon")
+const testId = "BaseIcon"
+const findIcon = async () => await screen.findByTestId(testId)
 
 describe("BaseIcon", () => {
     it("renders the icon with default values", async () => {
-        render(<BaseIcon testID="BaseIcon" name="star" />, {
+        render(<BaseIcon testID={testId} name="star" />, {
             wrapper: TestWrapper,
         })
-        await waitFor(() => expect(screen.getByTestId("BaseIcon")).toBeTruthy())
+        const icon = await findIcon()
+
+        expect(icon).toBeVisible()
     })
 
     it("calls the action prop when clicked", async () => {
         const mockAction = jest.fn()
-        render(<BaseIcon testID="BaseIcon" name="star" action={mockAction} />, {
+        render(<BaseIcon testID={testId} name="star" action={mockAction} />, {
             wrapper: TestWrapper,
         })
         const icon = await findIcon()
@@ -36,7 +34,7 @@ describe("BaseIcon", () => {
 
         render(
             <BaseIcon
-                testID="BaseIcon"
+                testID={testId}
                 name="star"
                 bg="red"
                 size={32}
@@ -55,7 +53,7 @@ describe("BaseIcon", () => {
 
         render(
             <BaseIcon
-                testID="BaseIcon"
+                testID={testId}
                 name="star"
                 bg="red"
                 action={mockAction}
@@ -68,7 +66,7 @@ describe("BaseIcon", () => {
 
         expect(icon).toBeVisible()
 
-        render(<BaseIcon testID="BaseIcon" name="star" bg="red" disabled />, {
+        render(<BaseIcon testID={testId} name="star" bg="red" disabled />, {
             wrapper: TestWrapper,
         })
         icon = await findIcon()
