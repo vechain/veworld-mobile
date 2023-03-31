124e7ca221112682e540365b23687e2117535d51
ve-akanoce <erik.nucibella@vechain.org>
2023-03-31 17:06:25 +0200

    refactor(ChangeNetworkBottomSheet): display networks by type, show selected network

--- src/Screens/Flows/App/NetworkScreen/Components/ChangeNetworkBottomSheet.tsx
+++ src/Screens/Flows/App/NetworkScreen/Components/ChangeNetworkBottomSheet.tsx
@@ -1,26 +1,71 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useMemo } from "react"
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types"
 import BaseBottomSheet from "~Components/Base/BaseBottomSheet"
-import { BaseSpacer, BaseText, BaseTouchableBox, BaseView } from "~Components"
+import { BaseSpacer, BaseText, BaseView } from "~Components"
 import { useI18nContext } from "~i18n"
-import { StringUtils } from "~Common"
-import { useAppDispatch } from "~Storage/Redux"
+import { useAppDispatch, useAppSelector } from "~Storage/Redux"
 import { changeSelectedNetwork } from "~Storage/Redux/Actions"
-import { Network } from "~Model"
+import { Network, NETWORK_TYPE } from "~Model"
+import {
+    selectNetworksByType,
+    selectSelectedNetwork,
+} from "~Storage/Redux/Selectors"
+import { NetworkBox } from "./NetworkBox"
+import { BottomSheetSectionList } from "@gorhom/bottom-sheet"
+import {
+    SectionListData,
+    SectionListRenderItemInfo,
+    StyleSheet,
+} from "react-native"
 
 type Props = {
-    networks: Network[]
     onClose: () => void
 }
 
-const snapPoints = ["35%"]
+const snapPoints = ["50%", "90%"]
 
 export const ChangeNetworkBottomSheet = React.forwardRef<
     BottomSheetModalMethods,
     Props
->(({ networks, onClose }, ref) => {
+>(({ onClose }, ref) => {
     const { LL } = useI18nContext()
     const dispatch = useAppDispatch()
+
+    const selectedNetwork = useAppSelector(selectSelectedNetwork)
+
+    const mainNetworks = useAppSelector(selectNetworksByType(NETWORK_TYPE.MAIN))
+    const testNetworks = useAppSelector(selectNetworksByType(NETWORK_TYPE.TEST))
+    const otherNetworks = useAppSelector(
+        selectNetworksByType(NETWORK_TYPE.OTHER),
+    )
+
+    type Section = {
+        title: string
+        data: Network[]
+    }
+    // variables
+    const sections: Section[] = useMemo(() => {
+        const data: Section[] = []
+        if (mainNetworks.length > 0) {
+            data.push({
+                title: LL.NETWORK_LABEL_MAIN_NETWORKS(),
+                data: mainNetworks,
+            })
+        }
+        if (testNetworks.length > 0) {
+            data.push({
+                title: LL.NETWORK_LABEL_TEST_NETWORKS(),
+                data: testNetworks,
+            })
+        }
+        if (otherNetworks.length > 0) {
+            data.push({
+                title: LL.NETWORK_LABEL_CUSTOM_NETWORKS(),
+                data: otherNetworks,
+            })
+        }
+        return data
+    }, [mainNetworks, testNetworks, otherNetworks, LL])
 
     const onPress = useCallback(
         (network: Network) => {
@@ -28,6 +73,39 @@ export const ChangeNetworkBottomSheet = React.forwardRef<
             onClose()
         },
         [onClose, dispatch],
+    )
+
+    const renderSectionHeader = useCallback(
+        ({ section }: { section: SectionListData<Network, Section> }) => {
+            return (
+                <BaseText typographyFont="bodyMedium">{section.title}</BaseText>
+            )
+        },
+        [],
+    )
+
+    const renderItem = useCallback(
+        ({ item }: SectionListRenderItemInfo<Network, Section>) => {
+            const isSelected = selectedNetwork.currentUrl === item.currentUrl
+            return (
+                <NetworkBox
+                    network={item}
+                    isSelected={isSelected}
+                    onPress={() => onPress(item)}
+                />
+            )
+        },
+        [selectedNetwork, onPress],
+    )
+
+    const renderItemSeparator = useCallback(
+        () => <BaseSpacer height={16} />,
+        [],
+    )
+
+    const renderSectionSeparator = useCallback(
+        () => <BaseSpacer height={24} />,
+        [],
     )
 
     return (
@@ -39,16 +117,26 @@ export const ChangeNetworkBottomSheet = React.forwardRef<
             </BaseView>
 
             <BaseSpacer height={16} />
-
-            {networks.map(network => (
-                <BaseView my={10} w={100} key={network.type}>
-                    <BaseTouchableBox action={() => onPress(network)}>
-                        <BaseText>
-                            {StringUtils.capitalize(network.type)}
-                        </BaseText>
-                    </BaseTouchableBox>
-                </BaseView>
-            ))}
+            <BaseView flexDirection="row" style={baseStyles.list}>
+                <BottomSheetSectionList
+                    sections={sections}
+                    keyExtractor={i => i.id}
+                    ItemSeparatorComponent={renderItemSeparator}
+                    SectionSeparatorComponent={renderSectionSeparator}
+                    renderSectionHeader={renderSectionHeader}
+                    renderItem={renderItem}
+                    scrollEnabled={true}
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                />
+            </BaseView>
         </BaseBottomSheet>
     )
+})
+
+const baseStyles = StyleSheet.create({
+    list: {
+        width: "100%",
+        height: "100%",
+    },
 })
