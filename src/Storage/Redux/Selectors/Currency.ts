324f7b7356f646ba5b3fed72876d766564aab54c
Davide Carpini <davide.carpini@vechain.org>
2023-04-03 14:49:56 +0200

    fix: typo

--- src/Storage/Redux/Selectors/Currency.ts
+++ src/Storage/Redux/Selectors/Currency.ts
@@ -1,12 +1,12 @@
 import { createSelector } from "@reduxjs/toolkit"
+import { VeChainToken } from "~Model"
 import { RootState } from "../Types"
 
 export const getCurrency = (state: RootState) => state.currency
 
 export const getCurrencyExchangeRate = createSelector(
-    [getCurrency, (_, symbol: string) => symbol],
+    [getCurrency, (_, symbol: VeChainToken) => symbol],
     (currency, symbol) => {
-        if (symbol === "VET") return currency.vet
-        if (symbol === "VTHO") return currency.vtho
+        return currency[symbol]
     },
 )
