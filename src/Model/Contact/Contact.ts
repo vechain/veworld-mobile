976ffeeb61b1c22e4540d91d8134a3529a9dc0f1
Piero Bassa <pierobassa222@gmail.com>
2023-04-04 11:31:14 +0200

    feat: added Contact modelling

--- src/Model/Contact/Contact.ts
+++ src/Model/Contact/Contact.ts
@@ -1,12 +1,6 @@
-// import { Account } from "../Account"
-// import { ContactType } from "./enum"
+import { Account } from "~Model"
+import { ContactType } from "./enum"
 
-// /**
-//  * A model for a contact in the address book
-//  * @field `id` - the unique ID of the contact
-//  * @field `type` - the type of contact
-//  */
-// export interface Contact extends Account {
-//     id: string
-//     type: ContactType
-// }
+export interface Contact extends Account {
+    type: ContactType
+}
