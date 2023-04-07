e2f227ff0292b0aaef47e06157696d87847eff98
ve-akanoce <erik.nucibella@vechain.org>
2023-04-07 11:13:39 +0200

    fix(EditContacts): unecessary baseView

--- src/Screens/Flows/App/ContactsScreen/Components/EditContactBottomSheet.tsx
+++ src/Screens/Flows/App/ContactsScreen/Components/EditContactBottomSheet.tsx
@@ -55,77 +55,76 @@ export const EditContactBottomSheet = React.forwardRef<
 
     return (
         <BaseBottomSheet snapPoints={snapPoints} ref={ref}>
-            <BaseView flexGrow={1}>
-                <BaseView
-                    alignItems="center"
-                    justifyContent="space-between"
-                    flexGrow={1}>
-                    <BaseView alignSelf="flex-start">
-                        <BaseView
-                            flexDirection="row"
-                            justifyContent="space-between"
-                            w={100}
-                            alignItems="center">
-                            <BaseText typographyFont="subTitleBold">
-                                {LL.SB_EDIT_CONTACT()}
-                            </BaseText>
-                        </BaseView>
-
-                        <BaseSpacer height={16} />
-
-                        <ContactForm
-                            titleName={LL.BD_CONTACT_NAME()}
-                            titleAddress={LL.BD_CONTACT_ADDRESS()}
-                            nameError={nameError}
-                            addressError={addressError}
-                            setName={setAlias}
-                            setAddress={setAddress}
-                            valueName={alias}
-                            valueAddress={address}
-                        />
-
-                        <BaseSpacer height={16} />
-
-                        <BaseView flexDirection="row">
-                            <BaseTouchableBox
-                                action={() => {}} // TODO: add action
-                                w="auto"
-                                flex={1}
-                                justifyContent="space-between">
-                                <BaseIcon
-                                    name="clipboard-outline"
-                                    size={20}
-                                    color={theme.colors.text}
-                                />
-                                <BaseText>{LL.BTN_PASTE_ADDRESS()}</BaseText>
-                            </BaseTouchableBox>
-                            <BaseSpacer width={16} />
-                            <BaseTouchableBox
-                                action={() => {}} // TODO: add action
-                                w="auto"
-                                flex={1}
-                                justifyContent="space-between">
-                                <BaseIcon
-                                    name="flip-horizontal"
-                                    size={20}
-                                    color={theme.colors.text}
-                                />
-                                <BaseText>{LL.BTN_SCAN_QR_CODE()}</BaseText>
-                            </BaseTouchableBox>
-                        </BaseView>
+            <BaseView
+                h={100}
+                alignItems="center"
+                justifyContent="space-between"
+                flexGrow={1}>
+                <BaseView alignSelf="flex-start">
+                    <BaseView
+                        flexDirection="row"
+                        justifyContent="space-between"
+                        w={100}
+                        alignItems="center">
+                        <BaseText typographyFont="subTitleBold">
+                            {LL.SB_EDIT_CONTACT()}
+                        </BaseText>
                     </BaseView>
 
-                    <BaseSpacer height={28} />
+                    <BaseSpacer height={16} />
 
-                    <BaseView flexDirection="row" mb={20}>
-                        <BaseButton
-                            action={() => onEditContact(alias, address)}
-                            w={100}
-                            disabled={!isFormValid}
-                            title={LL.COMMON_BTN_SAVE().toUpperCase()}
-                            bgColor={theme.colors.primary}
-                        />
+                    <ContactForm
+                        titleName={LL.BD_CONTACT_NAME()}
+                        titleAddress={LL.BD_CONTACT_ADDRESS()}
+                        nameError={nameError}
+                        addressError={addressError}
+                        setName={setAlias}
+                        setAddress={setAddress}
+                        valueName={alias}
+                        valueAddress={address}
+                    />
+
+                    <BaseSpacer height={16} />
+
+                    <BaseView flexDirection="row">
+                        <BaseTouchableBox
+                            action={() => {}} // TODO: add action
+                            w="auto"
+                            flex={1}
+                            justifyContent="space-between">
+                            <BaseIcon
+                                name="clipboard-outline"
+                                size={20}
+                                color={theme.colors.text}
+                            />
+                            <BaseText>{LL.BTN_PASTE_ADDRESS()}</BaseText>
+                        </BaseTouchableBox>
+                        <BaseSpacer width={16} />
+                        <BaseTouchableBox
+                            action={() => {}} // TODO: add action
+                            w="auto"
+                            flex={1}
+                            justifyContent="space-between">
+                            <BaseIcon
+                                name="flip-horizontal"
+                                size={20}
+                                color={theme.colors.text}
+                            />
+                            <BaseText>{LL.BTN_SCAN_QR_CODE()}</BaseText>
+                        </BaseTouchableBox>
                     </BaseView>
+                </BaseView>
+
+                <BaseSpacer height={28} />
+
+                <BaseView flexDirection="row" mb={20}>
+                    <BaseButton
+                        action={() => onEditContact(alias, address)}
+                        w={100}
+                        disabled={!isFormValid}
+                        title={LL.COMMON_BTN_SAVE().toUpperCase()}
+                        bgColor={theme.colors.primary}
+                    />
                 </BaseView>
             </BaseView>
         </BaseBottomSheet>
