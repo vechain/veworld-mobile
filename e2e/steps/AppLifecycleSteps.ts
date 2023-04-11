51afbfa99d3114060d12b888049567cde875eadd
Piero Bassa <pierobassa222@gmail.com>
2023-04-11 13:34:31 +0200

    refactor: removed skipping for android

--- e2e/steps/AppLifecycleSteps.ts
+++ e2e/steps/AppLifecycleSteps.ts
@@ -4,48 +4,26 @@ import { LONG_TIMEOUT } from "../helpers"
 
 // Default cucumber timeout has to be deactivated for this step
 Given("The user has installed the app", { timeout: -1 }, async () => {
-    if (isAndroid()) {
-        return "skipped"
-    } else {
-        await detox.device.launchApp({ delete: true })
-    }
+    await detox.device.launchApp({ delete: true })
 })
 
 // Default cucumber timeout has to be deactivated for this step
 Given("The user opens the app", { timeout: -1 }, async () => {
-    if (isAndroid()) {
-        return "skipped"
-    } else {
-        // Terminates and launches a new instance of the app
-        await detox.device.launchApp({ newInstance: true })
-    }
+    // Terminates and launches a new instance of the app
+    await detox.device.launchApp({ newInstance: true })
 })
 
 Given("The user brings the app from background", { timeout: -1 }, async () => {
-    if (isAndroid()) {
-        return "skipped"
-    } else {
-        // Brings the app from the background
-        await detox.device.launchApp({ newInstance: false })
-    }
+    // Brings the app from the background
+    await detox.device.launchApp({ newInstance: false })
 })
 
 Given("The user closes the app", { timeout: -1 }, async () => {
-    if (isAndroid()) {
-        return "skipped"
-    } else {
-        await detox.device.terminateApp()
-    }
+    await detox.device.terminateApp()
 })
 
 Then("The app is started successfully", { timeout: -1 }, async () => {
-    if (isAndroid()) {
-        return "skipped"
-    } else {
-        await waitFor(element(by.id("welcome-title-id")))
-            .toBeVisible()
-            .withTimeout(LONG_TIMEOUT)
-    }
+    await waitFor(element(by.id("welcome-title-id")))
+        .toBeVisible()
+        .withTimeout(LONG_TIMEOUT)
 })
-
-const isAndroid = () => detox.device.getPlatform() === "android"
