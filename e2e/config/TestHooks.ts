1b6d66a7955b4bf1056de5a098d5d21a678baea0
grenos <vasilisgreen@icloud.com>
2023-04-13 15:46:34 +0300

    test: test change hooks

--- e2e/config/TestHooks.ts
+++ e2e/config/TestHooks.ts
@@ -7,18 +7,24 @@ BeforeAll({ timeout: 600 * 1000 }, async () => {
     console.log("Detox test session started!")
 
     console.log("Launching app...")
-    await detox.device.launchApp()
+    await detox.device.launchApp({ delete: true })
     console.log("App launched!")
 })
 
 Before({ timeout: 600 * 1000 }, async () => {
-    console.log("Relaunching app before test...")
-    await detox.device.reloadReactNative()
-    console.log("App relaunched!")
+    // console.log("Relaunching app before test...")
+    // await detox.device.reloadReactNative()
+    // console.log("App relaunched!")
+
+    console.log("Launching app...")
+    await detox.device.launchApp({ delete: true })
+    console.log("App launched!")
 })
 
 AfterAll({ timeout: 600 * 1000 }, async () => {
     console.log("Starting cleanup Detox test session...")
+
     await detoxConfig.cleanup()
+
     console.log("Detox test session cleaned up!")
 })
