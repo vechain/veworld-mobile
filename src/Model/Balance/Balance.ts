3efc80b708e886fefcbb2e94909a5f33571a5154
ve-akanoce <erik.nucibella@vechain.org>
2023-04-18 22:12:26 +0200

    fix: typos

--- src/Model/Balance/Balance.ts
+++ src/Model/Balance/Balance.ts
@@ -6,6 +6,7 @@ import { FungibleToken } from "~Model"
  * @field `balance` - The current balance
  * @field `genesisId` - The ID of the genesis block. Used to identify the network
  * @field `timeUpdated` - The time the data was updated in seconds
+ * @field `position` - The position of the token in the account's token list
  */
 export interface Balance {
     accountAddress: string
