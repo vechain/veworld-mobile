7b845948822158564f456d3c9c93d9aae946528e
pierobassa <64158778+pierobassa@users.noreply.github.com>
2023-04-18 15:11:57 +0200

    feat: Style Toasts aligning with Figma design (#385)
    * chore: build files
    
    * feat: added toast colors
    
    * feat: added bodyBold typography
    
    * feat: added underline prop
    
    * feat: base toast styling and configuration
    
    * feat: toast content component
    
    * refactor: moved base toast to dedicated folder
    
    * refactor: indexing
    
    * test: base toast & styling unit tests
    
    * refactor: theme prop not needed
    
    * feat: added toast colors to light & dark colouring
    
    * refactor: removed iconcolor from toastconfig
    
    * refactor: removed iconcolor prop
    
    * feat: added iconColor ColorValue
    
    * test: fixed checking color in base toast which has been removed
    
    * refactor: correcting colors

---
+++ src/Components/Base/BaseToast/components/ToastContent.tsx
@@ -0,0 +1,62 @@
+import React, { useCallback } from "react"
+import { BaseIcon, BaseText, BaseView, hideToast } from "~Components"
+import { useTheme } from "~Common"
+import { ToastStyles } from "../util"
+
+type Props = {
+    styles: ToastStyles
+    icon: string
+    text1?: string
+    text2?: string
+    text3?: string
+    onPress?: () => void
+    hideToast?: () => void
+}
+
+export const ToastContent = ({
+    styles,
+    text1,
+    icon,
+    text2,
+    text3,
+    onPress,
+}: Props) => {
+    const theme = useTheme()
+
+    const handleOnPress = useCallback(() => {
+        if (onPress) {
+            onPress()
+            hideToast?.()
+        }
+    }, [onPress])
+
+    return (
+        <BaseView style={styles.container}>
+            <BaseView style={styles.contentContainer}>
+                <BaseIcon name={icon} size={20} color={styles.iconColor} />
+                <BaseView>
+                    <BaseView style={styles.textContainer} pl={12}>
+                        <BaseText
+                            color={theme.colors.textReversed}
+                            typographyFont="bodyBold">
+                            {text1}{" "}
+                        </BaseText>
+                        <BaseText
+                            color={theme.colors.textReversed}
+                            typographyFont="body">
+                            {text2}{" "}
+                        </BaseText>
+                        <BaseText
+                            disabled={!onPress}
+                            underline={onPress ? true : false}
+                            onPress={handleOnPress}
+                            color={theme.colors.textReversed}
+                            typographyFont="body">
+                            {text3}
+                        </BaseText>
+                    </BaseView>
+                </BaseView>
+            </BaseView>
+        </BaseView>
+    )
+}
