b4d9fc9b79644afa0e70551e8e9ecbccb064ed8a
ve-akanoce <erik.nucibella@vechain.org>
2023-04-20 10:35:54 +0200

    fix: remove unsued mocks

--- src/Test/helpers/index.ts
+++ src/Test/helpers/index.ts
@@ -1,10 +1,8 @@
 import { mockTokenRegistry } from "./stubs/TokenRegistryStubs"
-import { mockTransactionCall } from "./stubs/TransactionStubs"
 import ThorHelpers from "./stubs/Thor"
 import * as data from "./data"
 export default {
     data,
     mockTokenRegistry,
     thor: ThorHelpers,
-    mockTransactionCall,
 }
