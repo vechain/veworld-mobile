6a785d62cad3d1c4051ecfab164b693663e0dc85
Erik Nucibella <eriknucib@live.it>
2023-04-20 12:24:46 +0200

    feat(GasUtils): functions & tests  (#414)
    * feat(GasUtils): functions and utils around transactions gas
    
    * refactor(Balances): align names, model in ~Model
    
    * refactor(Balances): get -> select, remove denormalized naming
    
    * feat(Test): port test helpers with functions stubs and data
    
    * feat(GasUtils): exhaustive tests, fix imports and stubs
    
    * feat(GasUtils): exhaustive tests, fix imports and stubs
    
    * refactor(ThorStub): completely mock Connex.Thor in a systematic way
    
    * refactor: remove legacy coinbase stuff
    
    * fix: imports, gasUtils test
    
    * fix: typos, branch coverage
    
    * fix: remove unsued stubs
    
    * fix: test alias in tsconfig

--- src/Common/Utils/index.ts
+++ src/Common/Utils/index.ts
@@ -7,6 +7,7 @@ import BiometricsUtils from "./BiometricsUtils"
 import CryptoUtils from "./CryptoUtils"
 // import FormUtils from "./FormUtils"
 import FormattingUtils from "./FormattingUtils"
+import GasUtils from "./GasUtils"
 import HexUtils from "./HexUtils"
 // import MappingUtils from "./MappingUtils"
 import PasswordUtils from "./PasswordUtils"
@@ -34,6 +35,7 @@ export {
     CryptoUtils,
     // FormUtils,
     FormattingUtils,
+    GasUtils,
     HexUtils,
     // MappingUtils,
     PasswordUtils,
