f8a39bf8edbef726089ae18b2899a3f07ae7d21a
Piero Bassa <pierobassa222@gmail.com>
2023-04-21 12:19:37 +0200

    feat(test): added mnemonic and password consts

--- e2e/helpers/constants/Constants.ts
+++ e2e/helpers/constants/Constants.ts
@@ -1,3 +1,8 @@
 export const DEFAULT_TIMEOUT = 5_000
 export const SHORT_TIMEOUT = 1_000
 export const LONG_TIMEOUT = 10_000
+
+export const TEST_MNEMONIC =
+    "denial kitchen pet squirrel other broom bar gas better priority spoil cross"
+
+export const TEST_PASSWORD = "123456"
