e4b97c6658b249c1f6cff54ea4a294e7a078b16e
Piero Bassa <pierobassa222@gmail.com>
2023-04-21 12:20:50 +0200

    feat(test): contact screen utility functions

---
+++ e2e/helpers/screens/contacts/ContactsScreen.ts
@@ -0,0 +1,17 @@
+import { SHORT_TIMEOUT } from "../../constants"
+
+export const isActive = async (): Promise<boolean> => {
+    return await waitFor(element(by.id("contacts-screen-title")))
+        .toExist()
+        .withTimeout(SHORT_TIMEOUT)
+        .then(() => true)
+        .catch(() => false)
+}
+
+export const isContactsListEmpty = async (): Promise<boolean> => {
+    return await waitFor(element(by.text("Create contact")))
+        .toBeVisible()
+        .withTimeout(SHORT_TIMEOUT)
+        .then(() => true)
+        .catch(() => false)
+}
