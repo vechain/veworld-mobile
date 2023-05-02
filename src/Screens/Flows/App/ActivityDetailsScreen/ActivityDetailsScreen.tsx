de4be3027495d7094f557fc9b48a2d69e02199e7
Piero Bassa <pierobassa222@gmail.com>
2023-05-02 12:22:05 +0200

    feat: activity details screen

---
+++ src/Screens/Flows/App/ActivityDetailsScreen/ActivityDetailsScreen.tsx
@@ -0,0 +1,143 @@
+import React, { useCallback, useMemo } from "react"
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import {
+    BaseButton,
+    BaseIcon,
+    BaseSafeArea,
+    BaseScrollView,
+    BaseSpacer,
+    BaseText,
+    BaseView,
+    TransferCard,
+} from "~Components"
+import { RootStackParamListHome, Routes } from "~Navigation"
+import { useNavigation } from "@react-navigation/native"
+import { StyleSheet } from "react-native"
+import { DateUtils, useTheme } from "~Common"
+import { useI18nContext } from "~i18n"
+import { getActivityTitle } from "./util"
+import { getCalendars } from "expo-localization"
+import { ActivityType, FungibleTokenActivity } from "~Model"
+import { FungibleTokenTransferDetails } from "./Components"
+
+type Props = NativeStackScreenProps<
+    RootStackParamListHome,
+    Routes.ACTIVITY_DETAILS
+>
+
+export const ActivityDetailsScreen = ({ route }: Props) => {
+    const { activity, token } = route.params
+
+    const nav = useNavigation()
+
+    const theme = useTheme()
+
+    const { LL, locale } = useI18nContext()
+
+    const goBack = useCallback(() => nav.goBack(), [nav])
+
+    const dateTimeActivity = useMemo(() => {
+        return DateUtils.formatDateTime(
+            activity.timestamp ?? 0,
+            locale,
+            getCalendars()[0].timeZone ?? DateUtils.DEFAULT_TIMEZONE,
+        )
+    }, [activity.timestamp, locale])
+
+    const isFungibleTokenActivity = useMemo(() => {
+        return (
+            activity.type === ActivityType.FUNGIBLE_TOKEN ||
+            activity.type === ActivityType.VET_TRANSFER
+        )
+    }, [activity.type])
+
+    const renderActivityDetails = useMemo(() => {
+        switch (activity.type) {
+            case ActivityType.FUNGIBLE_TOKEN:
+            case ActivityType.VET_TRANSFER: {
+                return (
+                    <FungibleTokenTransferDetails
+                        activity={activity as FungibleTokenActivity}
+                        token={token}
+                    />
+                )
+            }
+            //TODO Other activity type details
+        }
+    }, [activity, token])
+
+    return (
+        <BaseSafeArea grow={1}>
+            <BaseView pb={6}>
+                <BaseIcon
+                    style={baseStyles.backIcon}
+                    size={36}
+                    name="chevron-left"
+                    color={theme.colors.text}
+                    action={goBack}
+                />
+            </BaseView>
+            <BaseScrollView
+                containerStyle={baseStyles.scrollViewContainer}
+                style={baseStyles.scrollView}>
+                <BaseView mx={20}>
+                    <BaseText typographyFont="title">
+                        {getActivityTitle(activity, LL)}
+                    </BaseText>
+
+                    <BaseSpacer height={16} />
+
+                    <BaseText typographyFont="subSubTitleLight">
+                        {dateTimeActivity}
+                    </BaseText>
+
+                    <BaseSpacer height={16} />
+
+                    {activity.to && isFungibleTokenActivity && (
+                        <TransferCard
+                            fromAddress={activity.from}
+                            toAddress={activity.to[0]}
+                        />
+                    )}
+
+                    <BaseSpacer height={20} />
+
+                    {/* TODO action click opens in-app browser or system browser. TBD with others */}
+                    <BaseButton
+                        action={() => {}}
+                        w={100}
+                        title={LL.VIEW_ON_EXPLORER().toUpperCase()}
+                        haptics="medium"
+                        typographyFont="buttonPrimary"
+                    />
+
+                    <BaseSpacer height={20} />
+
+                    <BaseText typographyFont="subTitleBold">
+                        {LL.DETAILS()}
+                    </BaseText>
+
+                    <BaseSpacer height={2} />
+
+                    {/* Render Activity Details */}
+                    {renderActivityDetails}
+                </BaseView>
+            </BaseScrollView>
+        </BaseSafeArea>
+    )
+}
+
+const baseStyles = StyleSheet.create({
+    backIcon: {
+        marginHorizontal: 8,
+        alignSelf: "flex-start",
+    },
+    scrollViewContainer: {
+        flex: 1,
+        width: "100%",
+        marginBottom: 60,
+    },
+    scrollView: {
+        width: "100%",
+    },
+})
