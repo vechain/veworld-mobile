5cc5e4f4387ed2862907c7a937b6ae71dc13dc93
ve-akanoce <erik.nucibella@vechain.org>
2023-05-03 17:25:58 +0200

    feat(Ledger): bluetooth device model

--- src/Model/Ledger/Ledger.ts
+++ src/Model/Ledger/Ledger.ts
@@ -1,7 +1,20 @@
-// export interface LedgerBalance extends Connex.Thor.Account {
-//     address: string
-// }
+import { DeviceModel } from "@ledgerhq/devices"
 
-// export interface LedgerAccounts {
-//     balances: LedgerBalance[]
-// }
+/**
+ * Represent a ledger device that is connected to the device via bluetooth
+ *  - id: The device id
+ * - isConnectable: Whether the device is connectable
+ * - localName: The local name of the device
+ * - productName: The product name of the device
+ * - rssi: The signal strength of the device
+ * - deviceModel: The model of the device
+ * @category Ledger
+ */
+export type LedgerDevice = {
+    id: string
+    isConnectable: boolean
+    localName: string
+    productName: string
+    rssi: number
+    deviceModel: DeviceModel
+}
