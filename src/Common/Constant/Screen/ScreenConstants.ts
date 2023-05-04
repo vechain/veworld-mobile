35a9c712a33b64089d3cb25423212910e9035c48
Piero Bassa <pierobassa222@gmail.com>
2023-05-04 15:03:25 +0200

    refactor: removed tab height

--- src/Common/Constant/Screen/ScreenConstants.ts
+++ src/Common/Constant/Screen/ScreenConstants.ts
@@ -1,11 +1,8 @@
 import { Dimensions } from "react-native"
-import { PlatformUtils } from "~Common/Utils"
 
 export const SCREEN_HEIGHT = Dimensions.get("window").height
 
 export const SCREEN_WIDTH = Dimensions.get("window").width
-
-export const TAB_HEIGHT = PlatformUtils.isIOS() ? 86 : 68
 
 // https://worship.agency/mobile-screen-sizes-for-2021
 export const isSmallScreen = SCREEN_HEIGHT <= 667
