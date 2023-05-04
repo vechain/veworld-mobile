65d15a21adf56d4956794cdf9d7235c153bff3a2
Davide Carpini <carpini.davide@gmail.com>
2023-05-04 17:40:12 +0200

    fix: coverage

--- jest.config.js
+++ jest.config.js
@@ -49,7 +49,6 @@ module.exports = {
         "src/Components/Base/**/*.{js,jsx,ts,tsx}",
     ],
     coveragePathIgnorePatterns: [
-        "index.ts",
         "<rootDir>/src/Common/Hooks/useAppReset/useAppReset.tsx",
         "<rootDir>/src/Common/Utils/ConnectionUtils/ConnectionUtils.ts",
     ],
