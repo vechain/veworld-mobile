84030462672dfde10d2538c64d158c1b681abbb1
grenos <vasilis.green@gmail.com>
2023-05-05 11:18:09 +0200

    chore: fix circleci config - run in parallel

--- jest.config.js
+++ jest.config.js
@@ -60,8 +60,5 @@ module.exports = {
             lines: 97.3,
         },
     },
-    coverageReporters: [
-        "default",
-        ["jest-junit", { addFileAttribute: "true" }],
-    ],
+    coverageReporters: ["default", ["jest-junit"]],
 }
