9ea38c4d5432de730937755d379283af4f8557e0
ve-akanoce <erik.nucibella@vechain.org>
2023-05-08 15:15:15 +0200

    feat(BalanceUtils): max coverage for file

--- src/Common/Utils/BalanceUtils/BalanceUtils.spec.ts
+++ src/Common/Utils/BalanceUtils/BalanceUtils.spec.ts
@@ -6,7 +6,11 @@ import axios from "axios"
 import MockAdapter from "axios-mock-adapter"
 
 const { account1D1, token1, token2 } = TestHelpers.data
+const thorAccountStub = TestHelpers.thor.stubs.account
 const thorClient = TestHelpers.thor.mockThorInstance({})
+const thorClientWithCallError = TestHelpers.thor.mockThorInstance({
+    account: thorAccountStub({ shouldCallError: true }),
+})
 const mainNetwork = defaultMainNetwork
 
 const mockedBalance = {
@@ -56,6 +60,25 @@ describe("BalanceUtils", () => {
 
         expect(balance.balance).toEqual(mockedBalance.energy)
     })
+
+    it("VTHO - should throw network error", async () => {
+        mock.onGet(
+            `${mainNetwork.currentUrl}/accounts/${account1D1.address}`,
+        ).networkError()
+
+        const balanceRequest = async () =>
+            await BalanceUtils.getBalanceFromBlockchain(
+                VTHO.address,
+                account1D1.address,
+                mainNetwork,
+                thorClient,
+            )
+
+        expect(balanceRequest).rejects.toThrowError(
+            "Failed to get balance from external service",
+        )
+    })
+
     it("PLA Token - should return the correct balance", async () => {
         const balance = await BalanceUtils.getBalanceFromBlockchain(
             token1.address,
@@ -75,5 +98,17 @@ describe("BalanceUtils", () => {
         )
 
         expect(balance.balance).toEqual(decodedTokenBalance)
+    })
+
+    it("SHA token - should throw an error", async () => {
+        const balanceRequest = async () =>
+            await BalanceUtils.getBalanceFromBlockchain(
+                token2.address,
+                account1D1.address,
+                mainNetwork,
+                thorClientWithCallError,
+            )
+
+        expect(balanceRequest).rejects.toThrowError()
     })
 })
