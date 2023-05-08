d7127491eb2b381929566d20d296768a0bf4b9c6
ve-akanoce <erik.nucibella@vechain.org>
2023-05-08 17:07:32 +0200

    fix(SelectLedgerAccounts): display ledger accounts using ledgerUtils

--- src/Screens/Flows/WalletCreation/ImportHardwareWallet/ImportLedgerWallet/02-SelectLedgerAccounts.tsx
+++ src/Screens/Flows/WalletCreation/ImportHardwareWallet/ImportLedgerWallet/02-SelectLedgerAccounts.tsx
@@ -1,16 +1,20 @@
-import React, { useCallback, useEffect } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import {
     BaseButton,
     BaseIcon,
     BaseSafeArea,
     BaseSpacer,
     BaseText,
+    BaseTouchableBox,
     BaseView,
     DismissKeyboardView,
     showWarningToast,
 } from "~Components"
 import { useI18nContext } from "~i18n"
-import { debug, useTheme } from "~Common"
+import { ColorThemeType, debug, useThemedStyles } from "~Common"
+import { VET } from "~Common/Constant/Token"
+import { FormattingUtils, LedgerUtils, AddressUtils } from "~Common/Utils"
+
 import { StyleSheet } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 
@@ -22,6 +26,10 @@ import {
 } from "~Navigation"
 
 import useLedger, { LedgerStatus } from "~Common/Hooks/useLedger"
+import { selectSelectedNetwork, useAppSelector } from "~Storage/Redux"
+import { LedgerAccount } from "~Common/Utils/LedgerUtils/LedgerUtils"
+import { humanAddress } from "~Common/Utils/FormattingUtils/FormattingUtils"
+import { FlatList } from "react-native-gesture-handler"
 
 type Props = {} & NativeStackScreenProps<
     RootStackParamListOnboarding & RootStackParamListCreateWalletApp,
@@ -32,41 +40,106 @@ export const SelectLedgerAccounts: React.FC<Props> = ({ route }) => {
     const { device } = route.params
     const { LL } = useI18nContext()
     const nav = useNavigation()
-    const theme = useTheme()
+    const { styles: themedStyles, theme } = useThemedStyles(styles)
+    const selectedNetwork = useAppSelector(selectSelectedNetwork)
 
     const onDisconnect = useCallback(() => {
         showWarningToast("Device disconnected")
     }, [])
 
     const { status, rootAccount, connect } = useLedger(device, onDisconnect)
+    const [ledgerAccounts, setLedgerAccounts] = useState<LedgerAccount[]>([])
+    const [selectedAccounts, setSelectedAccounts] = useState<string[]>([])
 
-    const goBack = () => nav.goBack()
+    /**
+     * When the root account changes, fetch the accounts and balances
+     */
+    useEffect(() => {
+        const getLedgerAccounts = async () => {
+            if (rootAccount) {
+                const accounts = await LedgerUtils.getAccountsWithBalances(
+                    rootAccount,
+                    selectedNetwork,
+                    10,
+                )
+                setLedgerAccounts(accounts)
+            }
+        }
+        getLedgerAccounts()
+    }, [rootAccount, selectedNetwork])
+
+    const goBack = useCallback(() => nav.goBack(), [nav])
 
     useEffect(() => {
         debug({ status, rootAccount })
     }, [status, rootAccount])
 
-    // const renderItem = useCallback(
-    //     ({ item }: { item: LedgerDevice }) => {
-    //         return (
-    //             <LedgerDeviceBox
-    //                 key={item.id}
-    //                 device={item}
-    //                 onPress={() => onDeviceSelect(item)}
-    //                 isSelected={selectedDevice?.id === item.id}
-    //             />
-    //         )
-    //     },
-    //     [onDeviceSelect, selectedDevice],
-    // )
+    const renderItem = useCallback(
+        ({ item, index }: { item: LedgerAccount; index: number }) => {
+            const isSelected = selectedAccounts.some(address =>
+                AddressUtils.compareAddresses(address, item.address),
+            )
 
-    // const renderSeparator = useCallback(() => <BaseSpacer height={16} />, [])
+            const style = isSelected
+                ? themedStyles.selected
+                : themedStyles.notSelected
+
+            const onAccountClick = () => {
+                setSelectedAccounts(prev => {
+                    const alreadySelected = prev.findIndex(address =>
+                        AddressUtils.compareAddresses(address, item.address),
+                    )
+                    if (alreadySelected >= 0) {
+                        return prev.filter(
+                            address =>
+                                !AddressUtils.compareAddresses(
+                                    address,
+                                    item.address,
+                                ),
+                        )
+                    } else {
+                        return [...prev, item.address]
+                    }
+                })
+            }
+
+            return (
+                <BaseTouchableBox
+                    key={item.address}
+                    action={onAccountClick}
+                    innerContainerStyle={style}
+                    flexDirection="row"
+                    justifyContent="space-between">
+                    <BaseView flexDirection="column" alignItems="flex-start">
+                        <BaseText typographyFont="subSubTitle">
+                            {LL.WALLET_LABEL_ACCOUNT()} {index + 1}
+                        </BaseText>
+                        <BaseSpacer height={6} />
+                        <BaseText typographyFont="captionRegular">
+                            {humanAddress(item.address)}
+                        </BaseText>
+                    </BaseView>
+                    <BaseText typographyFont="captionRegular">
+                        {item.balance &&
+                            FormattingUtils.humanNumber(
+                                item.balance?.balance,
+                                item.balance?.balance,
+                            )}{" "}
+                        {VET.symbol}
+                    </BaseText>
+                </BaseTouchableBox>
+            )
+        },
+        [themedStyles, selectedAccounts, LL],
+    )
+
+    const renderSeparator = useCallback(() => <BaseSpacer height={16} />, [])
 
     return (
         <DismissKeyboardView>
             <BaseSafeArea grow={1}>
                 <BaseIcon
-                    style={styles.backIcon}
+                    style={themedStyles.backIcon}
                     mx={8}
                     size={36}
                     name="chevron-left"
@@ -98,17 +171,17 @@ export const SelectLedgerAccounts: React.FC<Props> = ({ route }) => {
                                 <BaseButton title="Refresh" action={connect} />
                             )}
                         </BaseView>
-                        {/* <FlatList
-                            style={styles.container}
-                            data={availableDevices}
+                        <FlatList
+                            style={themedStyles.container}
+                            data={ledgerAccounts}
                             numColumns={1}
                             horizontal={false}
                             renderItem={renderItem}
                             nestedScrollEnabled={false}
                             showsVerticalScrollIndicator={false}
                             ItemSeparatorComponent={renderSeparator}
-                            keyExtractor={item => item.id}
-                        /> */}
+                            keyExtractor={item => item.address}
+                        />
                     </BaseView>
 
                     <BaseView w={100}>
@@ -127,9 +200,18 @@ export const SelectLedgerAccounts: React.FC<Props> = ({ route }) => {
     )
 }
 
-const styles = StyleSheet.create({
-    backIcon: { marginHorizontal: 20, alignSelf: "flex-start" },
-    container: {
-        width: "100%",
-    },
-})
+const styles = (theme: ColorThemeType) =>
+    StyleSheet.create({
+        backIcon: { marginHorizontal: 20, alignSelf: "flex-start" },
+        container: {
+            width: "100%",
+        },
+        selected: {
+            borderWidth: 1.5,
+            borderColor: theme.colors.text,
+        },
+        notSelected: {
+            borderWidth: 1.5,
+            borderColor: theme.colors.card,
+        },
+    })
