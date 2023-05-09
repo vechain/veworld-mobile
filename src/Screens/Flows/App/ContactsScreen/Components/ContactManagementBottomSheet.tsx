d1feadcee8f9928401c3e8fa42cd3d073f2a5946
Piero Bassa <pierobassa222@gmail.com>
2023-05-09 10:27:46 +0200

    feat: reusable contact management bottom sheet

---
+++ src/Screens/Flows/App/ContactsScreen/Components/ContactManagementBottomSheet.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useMemo, useState } from "react"
+import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types"
+import {
+    BaseBottomSheet,
+    BaseButton,
+    BaseSpacer,
+    BaseText,
+    BaseView,
+    DismissKeyboardView,
+} from "~Components"
+import { useI18nContext } from "~i18n"
+import { ContactForm } from "../../AddContactScreen/Components"
+import { useContactValidation } from "./Hooks"
+import { isSmallScreen, useTheme } from "~Common"
+import { Contact } from "~Model"
+
+type Props = {
+    contact?: Contact
+    isAddingContact?: boolean
+    checkTouched?: boolean
+    onClose: () => void
+    onSaveContact: (alias: string, address: string) => void
+}
+
+const snapPoints = [isSmallScreen ? "60%" : "52%"]
+
+export const ContactManagementBottomSheet = React.forwardRef<
+    BottomSheetModalMethods,
+    Props
+>(
+    (
+        {
+            contact,
+            isAddingContact = false,
+            checkTouched = true,
+            onSaveContact,
+        },
+        ref,
+    ) => {
+        const { LL } = useI18nContext()
+
+        const theme = useTheme()
+
+        const [alias, setAlias] = useState<string>(contact?.alias || "")
+        const [address, setAddress] = useState<string>(contact?.address || "")
+
+        useEffect(() => {
+            setAlias(contact?.alias || "")
+            setAddress(contact?.address || "")
+        }, [contact])
+
+        const { validateName, validateAddress } = useContactValidation(
+            false,
+            contact,
+        )
+
+        const { nameError, addressError } = {
+            nameError: validateName(alias),
+            addressError: validateAddress(address),
+        }
+
+        const isFormValid = useMemo(() => {
+            return addressError.length === 0 && nameError.length === 0
+        }, [addressError.length, nameError.length])
+
+        const title = isAddingContact
+            ? LL.BTN_CREATE_CONTACT()
+            : LL.SB_EDIT_CONTACT()
+
+        return (
+            <BaseBottomSheet snapPoints={snapPoints} ref={ref}>
+                <DismissKeyboardView>
+                    <BaseView
+                        h={100}
+                        alignItems="center"
+                        justifyContent="space-between"
+                        flexDirection="column">
+                        <BaseView alignSelf="flex-start">
+                            <BaseView
+                                flexDirection="row"
+                                justifyContent="space-between"
+                                w={100}
+                                alignItems="center">
+                                <BaseText typographyFont="subTitleBold">
+                                    {title}
+                                </BaseText>
+                            </BaseView>
+
+                            <BaseSpacer height={16} />
+
+                            <ContactForm
+                                titleName={LL.BD_CONTACT_NAME()}
+                                titleAddress={LL.BD_CONTACT_ADDRESS()}
+                                nameError={nameError}
+                                addressError={addressError}
+                                setName={setAlias}
+                                setAddress={setAddress}
+                                valueName={alias}
+                                valueAddress={address}
+                                addressFieldDisabled={isAddingContact}
+                                checkTouched={checkTouched}
+                            />
+
+                            <BaseSpacer height={16} />
+                        </BaseView>
+
+                        <BaseView flexDirection="row" pb={32}>
+                            <BaseButton
+                                action={() => onSaveContact(alias, address)}
+                                w={100}
+                                disabled={!isFormValid}
+                                title={LL.COMMON_BTN_SAVE().toUpperCase()}
+                                bgColor={theme.colors.primary}
+                            />
+                        </BaseView>
+                    </BaseView>
+                </DismissKeyboardView>
+            </BaseBottomSheet>
+        )
+    },
+)
