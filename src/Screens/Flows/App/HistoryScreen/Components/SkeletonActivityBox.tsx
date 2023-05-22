9961c714b1f790a59774045b04e8f0e6d66cf958
Piero Bassa <pierobassa222@gmail.com>
2023-05-22 14:44:13 +0200

    feat: skeleton activity box

---
+++ src/Screens/Flows/App/HistoryScreen/Components/SkeletonActivityBox.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+import SkeletonContent from "react-native-skeleton-content"
+import { useTheme } from "~Common"
+import { StyleSheet } from "react-native"
+
+export const SkeletonActivityBox = () => {
+    const theme = useTheme()
+
+    return (
+        <SkeletonContent
+            containerStyle={baseStyles.container}
+            animationDirection="horizontalLeft"
+            boneColor={theme.colors.skeletonBoneColor}
+            highlightColor={theme.colors.skeletonHighlightColor}
+            layout={[
+                // Here we create a row with a circle and a line
+                {
+                    flexDirection: "row",
+                    alignItems: "center",
+                    children: [
+                        // Circle
+                        { width: 40, height: 40, borderRadius: 20 },
+                        // Lines
+                        {
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                            width: "100%",
+                            children: [
+                                // Line
+                                {
+                                    width: "80%",
+                                    height: 20,
+                                    marginLeft: 10,
+                                },
+                                // Short line
+                                {
+                                    marginTop: 6,
+                                    width: "20%",
+                                    height: 20,
+                                    marginLeft: 10,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ]}
+            isLoading={true}
+        />
+    )
+}
+
+const baseStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+        flexDirection: "column",
+        marginLeft: -1,
+        paddingBottom: 18,
+    },
+})
