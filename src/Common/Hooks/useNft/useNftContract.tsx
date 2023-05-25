bcd6db498bea9dcf7334977303040d5391dd0eb5
Darren Kelly <darren.kelly@vechain.org>
2023-05-25 13:38:59 +0100

    refactor: use promise.all for useNftContract

--- src/Common/Hooks/useNft/useNftContract.tsx
+++ src/Common/Hooks/useNft/useNftContract.tsx
@@ -12,6 +12,7 @@ import {
     getOwnerOf,
     getName,
     getSymbol,
+    GithubCollectionResponse,
 } from "~Networking"
 import {
     selectSelectedAccount,
@@ -19,6 +20,74 @@ import {
     useAppSelector,
 } from "~Storage/Redux"
 const allSettled = require("promise.allsettled")
+
+const processCollection = async (
+    collectionInfo: GithubCollectionResponse[],
+    collection: NftForContractResponse[],
+    thor: Connex.Thor,
+    accAddress: string,
+): Promise<NonFungibleTokenCollection> => {
+    {
+        // find collection from GH registry
+        const foundCollection = collectionInfo.find(
+            col => col.address === collection[0].contractAddress,
+        )
+
+        if (!foundCollection) throw Error("Collection not found")
+
+        const {
+            address: contractAddress,
+            creator,
+            description,
+            icon,
+        } = foundCollection
+
+        const nftCollection: NonFungibleTokenCollection = {
+            address: contractAddress,
+            name: await getName(contractAddress, thor),
+            symbol: await getSymbol(contractAddress, thor),
+            creator: creator ?? "N/A",
+            description: description,
+            icon: icon
+                ? `https://vechain.github.io/nft-registry/${foundCollection?.icon}`
+                : "",
+            balanceOf: await getNftBalanceOf(
+                accAddress,
+                foundCollection.address,
+                thor,
+            ),
+            nfts: [],
+        }
+
+        await Promise.all(
+            collection.map(async item => {
+                //TODO: Do we need owner of if we're getting tokens from indexer?
+                const ownerOf = await getOwnerOf(
+                    item.tokenId,
+                    item.contractAddress,
+                    thor,
+                )
+
+                const tokenURI = await getTokenURI(
+                    item.tokenId,
+                    item.contractAddress,
+                    thor,
+                )
+
+                const nft = {
+                    tokenId: item.tokenId,
+                    owner: ownerOf,
+                    tokenURI,
+                    image: "",
+                }
+
+                nftCollection.nfts.push(nft)
+            }),
+        )
+
+        return nftCollection
+    }
+}
 
 export const useNftContract = () => {
     const thor = useThor()
@@ -61,89 +130,19 @@ export const useNftContract = () => {
                 },
             )
 
-            const _nftCollections: NonFungibleTokenCollection[] = []
-
-            // loop over the nnft collections
-            for (const collection of nftData) {
-                // find collection from GH registry
-                const foundCollection = collectionInfo.find(
-                    col => col.address === collection[0].contractAddress,
+            const _nftCollections2: NonFungibleTokenCollection[] =
+                await Promise.all(
+                    nftData.map(async collection => {
+                        return await processCollection(
+                            collectionInfo,
+                            collection,
+                            thor,
+                            selectedAccount.address,
+                        )
+                    }),
                 )
 
-                const nftCollection: NonFungibleTokenCollection = {
-                    address: "",
-                    name: "",
-                    symbol: "",
-                    creator: "",
-                    description: "",
-                    icon: "",
-                    balanceOf: 0,
-                    nfts: [],
-                }
-
-                // loop over the nfts in the collection to get nft details
-                for (const item of collection) {
-                    if (!nftCollection.address)
-                        nftCollection.address = item.contractAddress
-
-                    if (!nftCollection.icon)
-                        nftCollection.icon = foundCollection?.icon
-                            ? `https://vechain.github.io/nft-registry/${foundCollection?.icon}`
-                            : ""
-
-                    if (!nftCollection.symbol)
-                        nftCollection.symbol = await getSymbol(
-                            item.contractAddress,
-                            thor,
-                        )
-
-                    if (!nftCollection.creator)
-                        nftCollection.creator =
-                            foundCollection?.creator ?? "N/A"
-
-                    if (!nftCollection.description)
-                        nftCollection.description =
-                            foundCollection?.description ?? ""
-
-                    if (!nftCollection.name)
-                        nftCollection.name = await getName(
-                            item.contractAddress,
-                            thor,
-                        )
-
-                    if (!nftCollection.balanceOf)
-                        nftCollection.balanceOf = await getNftBalanceOf(
-                            selectedAccount?.address!,
-                            item.contractAddress,
-                            thor,
-                        )
-
-                    const ownerOf = await getOwnerOf(
-                        item.tokenId,
-                        item.contractAddress,
-                        thor,
-                    )
-
-                    const tokenURI = await getTokenURI(
-                        item.tokenId,
-                        item.contractAddress,
-                        thor,
-                    )
-
-                    const nft = {
-                        tokenId: item.tokenId,
-                        owner: ownerOf,
-                        tokenURI,
-                        image: "",
-                    }
-
-                    nftCollection.nfts.push(nft)
-                }
-
-                _nftCollections.push(nftCollection)
-            }
-
-            setNftCollections(_nftCollections)
+            setNftCollections(_nftCollections2)
         } catch (e) {
             error(e)
             throw e
