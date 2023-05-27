be6f483c73ca5b65d8f5dd27d1e823aea3542306
ve-akanoce <erik.nucibella@vechain.org>
2023-05-27 16:37:47 +0200

    feat(tests): toastContent tests, missing timeout in waitForNextUpdate

--- src/Test/helpers/data/ledger.ts
+++ src/Test/helpers/data/ledger.ts
@@ -3,7 +3,8 @@ import { Device } from "react-native-ble-plx"
 import { StatusCodes, VETLedgerAccount, VETLedgerApp } from "~Common/Ledger"
 import { AddressUtils, CryptoUtils } from "~Utils"
 import { hdnode1 } from "./wallets"
-
+import { Characteristic } from "@ledgerhq/react-native-hw-transport-ble/lib/types"
+import { DeviceModel, DeviceModelId } from "@ledgerhq/devices"
 /*eslint-disable no-console*/
 const publicKey =
     "042e7f024c8af943a41af6b74a8be59c57daf978282fb0118674cba85cac0fe68eeca595a4a84f93f76ab8d648e40e5ec880691787cbfe6607de578a4217d4c15c"
@@ -19,10 +20,15 @@ export const mockLedgerAccount = {
     chainCode,
 }
 
-export const mockDeviceModel = {
-    id: "nanoX",
+export const mockDeviceModel: DeviceModel = {
+    id: DeviceModelId.nanoX,
     productName: "Ledger Nano X",
     usbOnly: false,
+    productIdMM: 0x0004,
+    legacyUsbProductId: 0x0001,
+    memorySize: 0x100000,
+    masks: [0x0000, 0x0000],
+    getBlockSize: () => 0x1000,
 }
 
 export const mockedDevice = {
@@ -50,35 +56,22 @@ export const mockTransport = {
     },
 }
 
-// @ts-ignore
-export const mockedTransport: BleTransport = {
-    // @ts-ignore
-    deviceModel: mockDeviceModel, // @ts-ignore
-    device: mockedDevice,
-    close(): Promise<void> {
-        console.log("Closed transport")
-        return Promise.resolve()
-    },
-    open(_deviceOrId: string | Device): Promise<BleTransport> {
-        return Promise.resolve(mockedTransport)
-    },
-
-    on(eventName: string) {
-        console.log("Created event for: " + eventName)
-    },
-    exchange(): Promise<any> {
-        return Promise.resolve()
-    },
-    exchangeAtomicImpl(): Promise<any> {
-        return Promise.resolve()
-    },
-    exchangeBusyPromise: null,
-    exchangeTimeout: 30000,
-    id: "4418A927-A5E6-5743-8F64-895250CC29F9",
-    isConnected: true,
-    mtuSize: 153,
-    notYetDisconnected: true,
+const characteristics: Characteristic = {
+    uuid: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
+    isWritableWithResponse: true,
+    isWritableWithoutResponse: true,
+    isNotifiable: false,
+    isNotifying: false,
+    value: null,
 }
+
+export const mockedTransport = new BleTransport(
+    mockedDevice as Device,
+    characteristics,
+    characteristics,
+    null as any,
+    mockDeviceModel,
+)
 
 export const mockLedgerApp: VETLedgerApp = {
     // @ts-ignore
