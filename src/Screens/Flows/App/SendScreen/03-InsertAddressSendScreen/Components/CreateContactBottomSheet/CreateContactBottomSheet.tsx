42e5235ec67f26c812cb73105d99eef4962cef22
Erik Nucibella <eriknucib@live.it>
2023-06-07 12:51:29 +0200

    fix: polish send flow and minors  (#611)
    * fix(TransactionSummary): alias of selected account not displayed if receiver
    
    * fix(CreateCOntactSheet): add address validation, better keyboard support
    
    * fix: polish, add haptics, missing awaits
    
    * fix: add haptics
    
    * fix: await BaseButton test

--- src/Screens/Flows/App/SendScreen/03-InsertAddressSendScreen/Components/CreateContactBottomSheet/CreateContactBottomSheet.tsx
+++ src/Screens/Flows/App/SendScreen/03-InsertAddressSendScreen/Components/CreateContactBottomSheet/CreateContactBottomSheet.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useCallback, useMemo, useState } from "react"
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types"
 import {
     BaseBottomSheet,
@@ -11,57 +11,78 @@ import {
 } from "~Components"
 import { useI18nContext } from "~i18n"
 import { insertContact, useAppDispatch } from "~Storage/Redux"
-import { ContactType } from "~Model"
+import { Contact, ContactType } from "~Model"
+import { useContactValidation } from "~Screens/Flows/App/ContactsScreen"
 
 type Props = {
-    handleClose: () => void
-    goToResumeStep: () => void
+    onClose: () => void
+    onSubmit: (address: string) => void
     address: string
 }
 
 export const CreateContactBottomSheet = React.forwardRef<
     BottomSheetModalMethods,
     Props
->(({ goToResumeStep, address, handleClose }, ref) => {
+>(({ onSubmit, address, onClose }, ref) => {
     const { LL } = useI18nContext()
     const [creationMode, setCreationMode] = useState(false)
-    const [name, setName] = useState("")
+    const [alias, setAlias] = useState<string>("")
+
+    const contact: Contact = useMemo(() => {
+        return {
+            alias,
+            address,
+            type: ContactType.KNOWN,
+        }
+    }, [alias, address])
+
+    const { validateName, validateAddress } = useContactValidation(
+        false,
+        contact,
+    )
+
+    const { nameError, addressError } = {
+        nameError: validateName(alias),
+        addressError: validateAddress(address),
+    }
+
+    const isFormValid = useMemo(() => {
+        return addressError.length === 0 && nameError.length === 0
+    }, [addressError.length, nameError.length])
+
     const dispatch = useAppDispatch()
     const snapPoints = creationMode ? ["60%"] : ["40%"]
 
-    const closeAndGotoResumeStep = () => {
-        setName("")
+    const closeAndSubmit = useCallback(() => {
+        setAlias("")
         setCreationMode(false)
-        handleClose()
-        goToResumeStep()
-    }
+        onClose()
+        onSubmit(address)
+    }, [onClose, onSubmit, address])
 
-    const handleSaveButton = () => {
-        dispatch(
-            insertContact({
-                alias: name,
-                address,
-                type: ContactType.KNOWN,
-            }),
-        )
-        closeAndGotoResumeStep()
-    }
+    const handleSaveButton = useCallback(() => {
+        if (!isFormValid) return
+        dispatch(insertContact(contact))
+        closeAndSubmit()
+    }, [isFormValid, dispatch, contact, closeAndSubmit])
 
     return (
-        <BaseBottomSheet snapPoints={snapPoints} ref={ref}>
+        <BaseBottomSheet ref={ref} snapPoints={snapPoints}>
             {creationMode ? (
                 <ScrollViewWithFooter
                     footer={
                         <BaseView>
                             <BaseButton
                                 w={100}
+                                haptics="light"
                                 title={LL.COMMON_BTN_SAVE()}
                                 action={handleSaveButton}
-                                disabled={!name}
+                                disabled={!isFormValid}
                             />
                             <BaseSpacer height={16} />
                             <BaseButton
                                 w={100}
+                                haptics="light"
                                 variant="outline"
                                 title={LL.COMMON_BTN_CANCEL()}
                                 action={() => setCreationMode(false)}
@@ -78,8 +99,8 @@ export const CreateContactBottomSheet = React.forwardRef<
                         </BaseText>
                         <BaseSpacer height={16} />
                         <BaseBottomSheetTextInput
-                            value={name}
-                            setValue={setName}
+                            value={alias}
+                            setValue={setAlias}
                             placeholder={LL.SEND_CREATE_CONTACT_NAME()}
                         />
                         <BaseSpacer height={24} />
@@ -96,15 +117,17 @@ export const CreateContactBottomSheet = React.forwardRef<
                         <BaseView>
                             <BaseButton
                                 w={100}
+                                haptics="light"
                                 title={LL.SEND_CREATE_CONTACT_CREATE_BUTTON()}
                                 action={() => setCreationMode(true)}
                             />
                             <BaseSpacer height={16} />
                             <BaseButton
                                 w={100}
+                                haptics="light"
                                 variant="outline"
                                 title={LL.SEND_CREATE_CONTACT_PROCEED_ANYWAY_BUTTON()}
-                                action={closeAndGotoResumeStep}
+                                action={closeAndSubmit}
                             />
                         </BaseView>
                     }>
