7d714c2f67422aa8e6e49ee8ee4dadc1a790677d
grenos <vasilis.green@gmail.com>
2023-06-08 18:17:59 +0200

    fix: rename file

--- src/Screens/Flows/App/NFT/NFTScreen/NFTScreen.tsx
+++ src/Screens/Flows/App/NFT/NFTScreen/NFTScreen.tsx
@@ -10,14 +10,14 @@ import {
     FastActionsBar,
     SelectAccountBottomSheet,
 } from "~Components"
-import { NftScreenHeader } from "./Component"
+import { NftScreenHeader } from "./Components"
 import {
     AccountWithDevice,
     FastAction,
     NonFungibleTokenCollection,
 } from "~Model"
 import { isEmpty } from "lodash"
-import { NftSkeleton } from "./Component/NftSkeleton"
+import { NftSkeleton } from "./Components/NftSkeleton"
 import { StyleSheet, FlatList, ActivityIndicator } from "react-native"
 import {
     useBottomSheetModal,
