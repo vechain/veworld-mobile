d7cf3aba21ed281f525cfa1b49241c5d95f18254
Piero Bassa <pierobassa222@gmail.com>
2023-06-09 11:55:50 +0200

    feat: dispatching incoming transfer on beat

--- src/BlockListener.tsx
+++ src/BlockListener.tsx
@@ -22,6 +22,7 @@ import {
     useAppDispatch,
     useAppSelector,
     validateAndUpsertActivity,
+    addIncomingTransfer,
 } from "~Storage/Redux"
 // import { useI18nContext } from "~i18n"
 
@@ -214,7 +215,20 @@ const BlockListener: React.FC = () => {
             .apply(0, 5)
 
         // send toast notification for each transfer
-        transfers.forEach(tran => showFoundTokenTransfer(VET, tran.amount))
+        transfers.forEach(tran => {
+            showFoundTokenTransfer(VET, tran.amount)
+
+            dispatch(
+                addIncomingTransfer(
+                    tran.meta,
+                    tran.amount,
+                    tran.recipient,
+                    tran.sender,
+                    VET.address,
+                    thor,
+                ),
+            )
+        })
     }
 
     const [processedTransactionIds, setProcessedTransactionIds] = useState<
@@ -251,6 +265,17 @@ const BlockListener: React.FC = () => {
                     showFoundTokenTransfer(token, evt.amount)
                     setProcessedTransactionIds(
                         prev => new Set([...prev, evt.transactionId]),
+                    )
+
+                    dispatch(
+                        addIncomingTransfer(
+                            evt.meta,
+                            evt.amount,
+                            evt.recipient,
+                            evt.sender,
+                            evt.token.address,
+                            thor,
+                        ),
                     )
                 }
             })
