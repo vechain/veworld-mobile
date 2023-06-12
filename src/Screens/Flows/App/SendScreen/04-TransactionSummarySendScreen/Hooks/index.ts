c90c06dcecaf1b3408f86b0cb5cf01138bce0511
ve-akanoce <erik.nucibella@vechain.org>
2023-06-12 16:08:07 +0200

    feat(useSendTransaction): dedicated hooks to send tx and perform updates

--- src/Screens/Flows/App/SendScreen/04-TransactionSummarySendScreen/Hooks/index.ts
+++ src/Screens/Flows/App/SendScreen/04-TransactionSummarySendScreen/Hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./useSignTransaction"
 export * from "./useTransaction"
 export * from "./useDelegation"
+export * from "./useSendTransaction"
---
+++ src/Screens/Flows/App/SendScreen/04-TransactionSummarySendScreen/Hooks/useSendTransaction.ts
@@ -0,0 +1,50 @@
+import { Linking } from "react-native"
+import { Transaction } from "thor-devkit"
+import { showSuccessToast, useThor } from "~Components"
+import { defaultMainNetwork } from "~Constants"
+import { Network, WalletAccount } from "~Model"
+import {
+    addPendingTransferTransactionActivity,
+    updateAccountBalances,
+    useAppDispatch,
+} from "~Storage/Redux"
+import { sendTransaction } from "~Networking"
+import { useI18nContext } from "~i18n"
+
+/**
+ * Hooks that expose a function to send a transaction and perform updates, showing a toast on success
+ * @param network the network to send the transaction to
+ * @returns
+ */
+export const useSendTransaction = (
+    network: Network,
+    account: WalletAccount,
+) => {
+    const dispatch = useAppDispatch()
+    const { LL } = useI18nContext()
+    const thorClient = useThor()
+
+    const sendTransactionAndPerformUpdates = async (tx: Transaction) => {
+        const id = await sendTransaction(tx, network.currentUrl)
+
+        // Add pending transaction activity
+        dispatch(addPendingTransferTransactionActivity(tx, thorClient))
+
+        showSuccessToast(
+            LL.SUCCESS_GENERIC(),
+            LL.SUCCESS_GENERIC_OPERATION(),
+            LL.SUCCESS_GENERIC_VIEW_DETAIL_LINK(),
+            async () => {
+                await Linking.openURL(
+                    `${
+                        network.explorerUrl || defaultMainNetwork.explorerUrl
+                    }/transactions/${id}`,
+                )
+            },
+            "transactionSuccessToast",
+        )
+        await dispatch(updateAccountBalances(thorClient, account.address))
+    }
+
+    return { sendTransactionAndPerformUpdates }
+}
