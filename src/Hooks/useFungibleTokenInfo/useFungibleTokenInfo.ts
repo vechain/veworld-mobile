07ee5daccff2446c2ad3254d96e7c70402368bdd
Piero Bassa <pierobassa222@gmail.com>
2023-06-30 14:54:32 +0200

    feat: added error state to hook

--- src/Hooks/useFungibleTokenInfo/useFungibleTokenInfo.ts
+++ src/Hooks/useFungibleTokenInfo/useFungibleTokenInfo.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useThor } from "~Components"
 import { VET } from "~Constants"
 import { getTokenDecimals, getTokenSymbol } from "~Networking"
-import { error } from "~Utils"
+import { error as loggerError } from "~Utils"
 
 /**
  * React hook to fetch basic information about fungible tokens.
@@ -15,36 +15,32 @@ import { error } from "~Utils"
  */
 export const useFungibleTokenInfo = (tokenAddress: string) => {
     const [symbol, setSymbol] = useState<string>()
-
     const [decimals, setDecimals] = useState<number>()
+    const [error, setError] = useState<Error | null>(null)
 
     const thor = useThor()
-    /**
-     * Fetches the token symbol and decimals
-     * for the selected account
-     */
+
     useEffect(() => {
         if (tokenAddress === VET.address) return
 
-        getTokenDecimals(tokenAddress, thor)
-            .then(res => {
-                setDecimals(res)
-            })
-            .catch(err => {
-                error("Error getting token decimals", err)
-            })
+        const fetchData = async () => {
+            try {
+                const resDecimals = await getTokenDecimals(tokenAddress, thor)
+                setDecimals(resDecimals)
+                const resSymbol = await getTokenSymbol(tokenAddress, thor)
+                setSymbol(resSymbol)
+            } catch (err) {
+                setError(err as Error)
+                loggerError("Error getting token info", err)
+            }
+        }
 
-        getTokenSymbol(tokenAddress, thor)
-            .then(res => {
-                setSymbol(res)
-            })
-            .catch(err => {
-                error("Error getting token symbol", err)
-            })
-    }, [thor, tokenAddress])
+        fetchData()
+    }, [tokenAddress, thor])
 
     return {
         symbol,
         decimals,
+        error,
     }
 }
