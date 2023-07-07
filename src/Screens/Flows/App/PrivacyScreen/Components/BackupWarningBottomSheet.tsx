5d6d7bc1d2af8b4aff476fd823df14d7d04812b4
Piero Bassa <64158778+pierobassa@users.noreply.github.com>
2023-07-07 16:16:02 +0200

    feat(Android): add warning on android when upgrading security & minor security fixes (#794)
    * chore: build files
    
    * refactor: recursive biometrics unlock rename
    
    * feat: added extra warning for android users upgrading security
    
    * fix: error thrown on device encryption key not being set
    
    * refactor: translation for show cancelled biometrics alert
    
    * chore: translations
    
    * test: import fix

--- src/Screens/Flows/App/PrivacyScreen/Components/BackupWarningBottomSheet.tsx
+++ src/Screens/Flows/App/PrivacyScreen/Components/BackupWarningBottomSheet.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useMemo } from "react"
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types"
 import {
     BaseBottomSheet,
@@ -13,18 +13,18 @@ import {
 import { useI18nContext } from "~i18n"
 import { COLORS } from "~Constants"
 import { StyleSheet } from "react-native"
+import { PlatformUtils } from "~Utils"
 
 type Props = {
     onClose: () => void
     onConfirm: () => void
+    isUpgradeSecurity?: boolean
 }
-
-const snapPoints = ["57%"]
 
 export const BackupWarningBottomSheet = React.forwardRef<
     BottomSheetModalMethods,
     Props
->(({ onClose, onConfirm }, ref) => {
+>(({ onClose, onConfirm, isUpgradeSecurity = false }, ref) => {
     const { LL } = useI18nContext()
 
     // Holds the state of the user's acknowledgement of the warning
@@ -35,6 +35,13 @@ export const BackupWarningBottomSheet = React.forwardRef<
         onClose()
         setChecked(false)
     }, [onClose, onConfirm])
+
+    const snapPoints = useMemo(() => {
+        if (PlatformUtils.isAndroid() && isUpgradeSecurity) {
+            return ["70%"]
+        }
+        return ["60%"]
+    }, [isUpgradeSecurity])
 
     return (
         <BaseBottomSheet ref={ref} snapPoints={snapPoints}>
@@ -68,7 +75,7 @@ export const BackupWarningBottomSheet = React.forwardRef<
                     <BaseText typographyFont="subSubTitleLight">
                         {LL.SB_BACKUP_YOUR_PHRASE()}
                     </BaseText>
-                    <BaseText typographyFont="subSubTitle" pt={4}>
+                    <BaseText typographyFont="subSubTitle">
                         {LL.SB_BACKUP_YOUR_PHRASE_2()}
                     </BaseText>
 
@@ -89,7 +96,13 @@ export const BackupWarningBottomSheet = React.forwardRef<
                         </BaseView>
                     </BaseView>
 
-                    <BaseSpacer height={16} />
+                    <BaseSpacer height={24} />
+
+                    {PlatformUtils.isAndroid() && isUpgradeSecurity && (
+                        <BaseText typographyFont="subSubTitle" pt={4}>
+                            {LL.SB_UPGRADE_SECURITY_WARNING_ANDROID()}
+                        </BaseText>
+                    )}
                 </BaseView>
             </ScrollViewWithFooter>
         </BaseBottomSheet>
