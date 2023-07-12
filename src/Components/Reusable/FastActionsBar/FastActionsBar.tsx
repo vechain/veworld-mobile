361c4a763a5c5258476801339f877c0b65ff22c4
grenos <vasilis.green@gmail.com>
2023-07-12 16:56:09 +0200

    fix: fix navigation and ui

--- src/Components/Reusable/FastActionsBar/FastActionsBar.tsx
+++ src/Components/Reusable/FastActionsBar/FastActionsBar.tsx
@@ -1,51 +1,66 @@
 import React, { memo, useCallback } from "react"
 import { StyleSheet } from "react-native"
-import { useTheme } from "~Hooks"
+import { useTheme, useThemedStyles } from "~Hooks"
 import { BaseSpacer, BaseText, BaseTouchable, BaseView } from "~Components"
 import { FastAction } from "~Model"
 
-export const FastActionsBar = memo(({ actions }: { actions: FastAction[] }) => {
-    const theme = useTheme()
-    const renderAction = useCallback((action: FastAction) => {
-        return (
-            <BaseTouchable
-                key={action.name}
-                action={action.action}
-                testID={action.testID}
-                style={styles.action}>
-                <BaseView flexDirection="column" alignItems="center">
-                    {action.icon}
-                    <BaseSpacer height={6} />
-                    <BaseText typographyFont="smallButtonPrimary">
-                        {action.name}
-                    </BaseText>
-                </BaseView>
-            </BaseTouchable>
+export const FastActionsBar = memo(
+    ({
+        actions,
+        paddingHorizontal = 24,
+    }: {
+        actions: FastAction[]
+        paddingHorizontal?: number
+    }) => {
+        const theme = useTheme()
+
+        const { styles } = useThemedStyles(baseStyles(paddingHorizontal))
+
+        const renderAction = useCallback(
+            (action: FastAction) => {
+                return (
+                    <BaseTouchable
+                        key={action.name}
+                        action={action.action}
+                        testID={action.testID}
+                        style={styles.action}>
+                        <BaseView flexDirection="column" alignItems="center">
+                            {action.icon}
+                            <BaseSpacer height={6} />
+                            <BaseText typographyFont="smallButtonPrimary">
+                                {action.name}
+                            </BaseText>
+                        </BaseView>
+                    </BaseTouchable>
+                )
+            },
+            [styles.action],
         )
-    }, [])
 
-    return (
-        <BaseView style={styles.container}>
-            <BaseView
-                flexDirection="row"
-                alignItems="center"
-                justifyContent="center"
-                bg={theme.colors.card}
-                borderRadius={34}>
-                {actions.map(renderAction)}
+        return (
+            <BaseView style={styles.container}>
+                <BaseView
+                    flexDirection="row"
+                    alignItems="center"
+                    justifyContent="center"
+                    bg={theme.colors.card}
+                    borderRadius={34}>
+                    {actions.map(renderAction)}
+                </BaseView>
             </BaseView>
-        </BaseView>
-    )
-})
+        )
+    },
+)
 
-const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 20,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    action: {
-        paddingHorizontal: 24,
-        paddingVertical: 12,
-    },
-})
+const baseStyles = (paddingHorizontal: number) => () =>
+    StyleSheet.create({
+        container: {
+            paddingHorizontal: 20,
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        action: {
+            paddingHorizontal,
+            paddingVertical: 12,
+        },
+    })
