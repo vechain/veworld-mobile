ee91a3f70b75e0b8e5ce7c7878fc8e1b24f7fc20
grenos <vasilis.green@gmail.com>
2023-07-13 18:10:09 +0200

    fix: app crash on 0 balance

--- src/Screens/Flows/App/AssetDetailScreen/AssetDetailScreen.tsx
+++ src/Screens/Flows/App/AssetDetailScreen/AssetDetailScreen.tsx
@@ -10,6 +10,7 @@ import {
     BaseText,
     BaseView,
     FastActionsBar,
+    showWarningToast,
 } from "~Components"
 import { RootStackParamListDiscover, Routes } from "~Navigation"
 import {
@@ -61,18 +62,27 @@ export const AssetDetailScreen = ({ route }: Props) => {
         () => [
             {
                 name: LL.BTN_SEND(),
-                action: () =>
-                    nav.navigate(Routes.SELECT_AMOUNT_SEND, {
-                        token: foundToken[0],
-                        initialRoute: Routes.HOME,
-                    }),
+                action: () => {
+                    if (foundToken.length) {
+                        nav.navigate(Routes.SELECT_AMOUNT_SEND, {
+                            token: foundToken[0],
+                            initialRoute: Routes.HOME,
+                        })
+                    } else {
+                        showWarningToast(
+                            LL.SEND_ERROR_TOKEN_NOT_FOUND({
+                                tokenName: token.symbol,
+                            }),
+                        )
+                    }
+                },
                 icon: (
                     <BaseIcon color={theme.colors.text} name="send-outline" />
                 ),
                 testID: "sendButton",
             },
         ],
-        [LL, foundToken, nav, theme.colors.text],
+        [LL, foundToken, nav, theme.colors.text, token.symbol],
     )
 
     return (
