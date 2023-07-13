3cfdeb3a879a30606f4d0a3bf3577416a0ef9647
Piero Bassa <pierobassa222@gmail.com>
2023-07-13 13:00:46 +0200

    feat: calling add sign certificate activity

--- src/Screens/Flows/App/WalletConnect/SignMessageScreen/SignMessageScreen.tsx
+++ src/Screens/Flows/App/WalletConnect/SignMessageScreen/SignMessageScreen.tsx
@@ -11,7 +11,12 @@ import {
     BaseSafeArea,
 } from "~Components"
 import { Certificate, blake2b256 } from "thor-devkit"
-import { selectSelectedAccount, useAppSelector } from "~Storage/Redux"
+import {
+    addSignCertificateActivity,
+    selectSelectedAccount,
+    useAppDispatch,
+    useAppSelector,
+} from "~Storage/Redux"
 import { WalletConnectUtils, WalletConnectResponseUtils, error } from "~Utils"
 import { useCheckIdentity, useSignMessage } from "~Hooks"
 import { AccountWithDevice } from "~Model"
@@ -36,6 +41,8 @@ export const SignMessageScreen: FC<Props> = ({ route }: Props) => {
     const selectedAccount: AccountWithDevice = useAppSelector(
         selectSelectedAccount,
     )
+
+    const dispatch = useAppDispatch()
 
     // Request values
     const { params } =
@@ -85,6 +92,15 @@ export const SignMessageScreen: FC<Props> = ({ route }: Props) => {
                     signature,
                     cert,
                 )
+
+                dispatch(
+                    addSignCertificateActivity(
+                        sessionRequest.peer.metadata.name,
+                        cert.domain,
+                        cert.payload.content,
+                        cert.purpose,
+                    ),
+                )
             } catch (err: unknown) {
                 error(err)
                 await WalletConnectResponseUtils.signMessageRequestErrorResponse(
@@ -98,7 +114,16 @@ export const SignMessageScreen: FC<Props> = ({ route }: Props) => {
 
             onClose()
         },
-        [signMessage, requestEvent, web3Wallet, LL, cert, onClose],
+        [
+            onClose,
+            signMessage,
+            requestEvent,
+            web3Wallet,
+            LL,
+            cert,
+            dispatch,
+            sessionRequest.peer.metadata.name,
+        ],
     )
 
     const onReject = useCallback(async () => {
