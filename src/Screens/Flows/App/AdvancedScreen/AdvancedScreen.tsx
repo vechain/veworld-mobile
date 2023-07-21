b4e8343a3db81bb36c7de9f394366e14dec43f25
grenos <vasilis.green@gmail.com>
2023-07-21 15:00:13 +0200

    fix: remove log button

--- src/Screens/Flows/App/AdvancedScreen/AdvancedScreen.tsx
+++ src/Screens/Flows/App/AdvancedScreen/AdvancedScreen.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback } from "react"
 import {
     BackButtonHeader,
-    BaseButton,
-    BaseIcon,
     BaseSafeArea,
     BaseSpacer,
     BaseText,
@@ -11,9 +9,6 @@ import {
 } from "~Components"
 import { Reset } from "./Components/Reset"
 import { useI18nContext } from "~i18n"
-import { info } from "~Utils"
-import { useTheme } from "~Hooks"
-import { StyleSheet } from "react-native"
 import {
     selectSentryTrackingEnabled,
     setSentryTrackingEnabled,
@@ -22,14 +17,8 @@ import {
 } from "~Storage/Redux"
 
 export const AdvancedScreen = () => {
-    const theme = useTheme()
     const { LL } = useI18nContext()
     const dispatch = useAppDispatch()
-
-    const onDownloadLogs = useCallback(() => {
-        // TODO (Vas) (https://github.com/vechainfoundation/veworld-mobile/issues/756) implement download logs
-        info("Download logs")
-    }, [])
 
     const sentryTrackingEnabled = useAppSelector(selectSentryTrackingEnabled)
     const toggleSentryTrackingSwitch = useCallback(
@@ -47,30 +36,6 @@ export const AdvancedScreen = () => {
                 <BaseText typographyFont="title">
                     {LL.TITLE_ADVANCED()}
                 </BaseText>
-                <BaseSpacer height={24} />
-
-                <BaseText typographyFont="bodyMedium" my={8}>
-                    {LL.BD_STATE_LOGS()}
-                </BaseText>
-                <BaseText typographyFont="caption">
-                    {LL.BD_STATE_LOGS_DISCLAIMER()}
-                </BaseText>
-                <BaseSpacer height={16} />
-                <BaseButton
-                    style={baseStyles.btnWidth}
-                    size="md"
-                    radius={12}
-                    action={onDownloadLogs}
-                    title={LL.BTN_DOWNLOAD_LOGS()}
-                    rightIcon={
-                        <BaseIcon
-                            name="tray-arrow-down"
-                            size={20}
-                            color={theme.colors.textReversed}
-                            style={baseStyles.iconMargin}
-                        />
-                    }
-                />
 
                 <BaseSpacer height={24} />
                 <BaseText typographyFont="bodyMedium" my={8}>
@@ -93,18 +58,3 @@ export const AdvancedScreen = () => {
         </BaseSafeArea>
     )
 }
-
-const baseStyles = StyleSheet.create({
-    backIcon: {
-        marginHorizontal: 8,
-        alignSelf: "flex-start",
-    },
-
-    btnWidth: {
-        width: 172,
-    },
-
-    iconMargin: {
-        marginHorizontal: 12,
-    },
-})
