a345d006aaa78f86949166f6b7a755306b211cdd
Daithi Hearn <daithi.hearn@gmail.com>
2023-07-25 19:44:45 +0200

    fix: unit tests

--- src/Test/helpers/data/nft.ts
+++ src/Test/helpers/data/nft.ts
@@ -35,7 +35,7 @@ export const nftItemsPerContract = [
     {
         id: "65c4ddddb1ff6e339bcb6cc841681025b1889fe0",
         tokenId: 1,
-        contractAddress: "0x2a5c0083937fe7efa9c9c3f099d6e73efe2a6c35",
+        address: "0x2a5c0083937fe7efa9c9c3f099d6e73efe2a6c35",
         owner: "0xf077b491b355e64048ce21e3a6fc4751eeea77fa",
         txId: "0x0f882e24d1e24b3f5811d7b2c6caceca700129ef1b9128aa3ab1975778751b26",
         blockNumber: 15389140,
@@ -46,7 +46,7 @@ export const nftItemsPerContract = [
     {
         id: "8089c75b1c2e5d02bb57700d5513c31e0b6714f4",
         tokenId: 3,
-        contractAddress: "0x2a5c0083937fe7efa9c9c3f099d6e73efe2a6c35",
+        address: "0x2a5c0083937fe7efa9c9c3f099d6e73efe2a6c35",
         owner: "0xf077b491b355e64048ce21e3a6fc4751eeea77fa",
         txId: "0xdfc6d84591c37005ab75823eda8d911f023eb4750521071dba3b9d24814d87af",
         blockNumber: 15389181,
@@ -57,7 +57,7 @@ export const nftItemsPerContract = [
     {
         id: "f67e58d41d0c9f11f8987ea94f1c8d5026216b5a",
         tokenId: 2,
-        contractAddress: "0x2a5c0083937fe7efa9c9c3f099d6e73efe2a6c35",
+        address: "0x2a5c0083937fe7efa9c9c3f099d6e73efe2a6c35",
         owner: "0xf077b491b355e64048ce21e3a6fc4751eeea77fa",
         txId: "0xdfc6d84591c37005ab75823eda8d911f023eb4750521071dba3b9d24814d87af",
         blockNumber: 15389181,
