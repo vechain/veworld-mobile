c5407d77ef9776931233ad9a5f4b72215f092ff0
Davide Carpini <carpini.davide@gmail.com>
2023-07-25 18:28:49 +0300

    fix: android api version and homescreen ui bug (#950)

--- src/Hooks/usePlatformBottomInsets/usePlatformBottomInsets.ts
+++ src/Hooks/usePlatformBottomInsets/usePlatformBottomInsets.ts
@@ -4,24 +4,24 @@ import { PlatformUtils } from "~Utils"
 
 export enum BottomInsetsEXtraPadding {
     StaticButton = 80,
+    TabBar = 24,
 }
 
 export const usePlatformBottomInsets = (
     extraPadding?: BottomInsetsEXtraPadding,
 ) => {
     const paddingBottom = useBottomTabBarHeight()
+    let staticExtraPadding = extraPadding ?? 0
 
     const calculateBottomInsets: number = useMemo(() => {
-        let staticButtonExtraPadding = extraPadding ?? 0
-
-        return PlatformUtils.isIOS()
-            ? paddingBottom + staticButtonExtraPadding
-            : 0
-    }, [paddingBottom, extraPadding])
+        return PlatformUtils.isIOS() ? paddingBottom + staticExtraPadding : 0
+    }, [paddingBottom, staticExtraPadding])
 
     const tabBarAndroidBottomInsets: number = useMemo(() => {
-        return PlatformUtils.isAndroid() ? paddingBottom : 0
-    }, [paddingBottom])
+        return PlatformUtils.isAndroid()
+            ? paddingBottom + staticExtraPadding
+            : staticExtraPadding
+    }, [paddingBottom, staticExtraPadding])
 
     return { calculateBottomInsets, tabBarAndroidBottomInsets }
 }
