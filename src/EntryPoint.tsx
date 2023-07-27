9a8436fab0e4fcb2912311210a4e962271aa8a30
Piero Bassa <pierobassa222@gmail.com>
2023-07-27 22:24:03 +0200

    feat: added app loader usage

--- src/EntryPoint.tsx
+++ src/EntryPoint.tsx
@@ -3,16 +3,17 @@ import { BaseStatusBar, SecurityProvider } from "~Components"
 import { SwitchStack } from "~Navigation"
 import ErrorBoundary from "~Components/Providers/ErrorBoundary"
 import { PinCodeProvider } from "~Components/Providers/PinCodeProvider/PinCodeProvider"
+import { AppLoader } from "./AppLoader"
 
 export const EntryPoint = () => {
     return (
         <ErrorBoundary>
             <PinCodeProvider>
                 <SecurityProvider>
-                    <>
+                    <AppLoader>
                         <BaseStatusBar />
                         <SwitchStack />
-                    </>
+                    </AppLoader>
                 </SecurityProvider>
             </PinCodeProvider>
         </ErrorBoundary>
