696b7c99a013005388d21f8c36f26a5c69146c71
Piero Bassa <pierobassa222@gmail.com>
2023-07-28 15:00:22 +0200

    refactor: rollback translations

--- src/Screens/Flows/App/LedgerScreen/LedgerSignCertificate.tsx
+++ src/Screens/Flows/App/LedgerScreen/LedgerSignCertificate.tsx
@@ -23,13 +23,7 @@ import {
     RootStackParamListSwitch,
     Routes,
 } from "~Navigation"
-import {
-    debug,
-    error,
-    LedgerUtils,
-    MinimizerUtils,
-    WalletConnectResponseUtils,
-} from "~Utils"
+import { debug, error, LedgerUtils, WalletConnectResponseUtils } from "~Utils"
 import { useI18nContext } from "~i18n"
 import { useNavigation } from "@react-navigation/native"
 import * as Haptics from "expo-haptics"
@@ -87,17 +81,17 @@ export const LedgerSignCertificate: React.FC<Props> = ({ route }) => {
     const Steps: Step[] = useMemo(
         () => [
             {
-                isActiveText: "Connecting",
-                isNextText: "Connect",
-                isDoneText: "Connected",
+                isActiveText: LL.LEDGER_CONNECTING(),
+                isNextText: LL.LEDGER_CONNECT(),
+                isDoneText: LL.LEDGER_CONNECTED(),
                 progressPercentage: 25,
                 title: LL.SEND_LEDGER_CHECK_CONNECTION(),
                 subtitle: LL.SEND_LEDGER_CHECK_CONNECTION_SB(),
             },
             {
-                isActiveText: "Signing",
-                isNextText: "Sign data",
-                isDoneText: "Data Signed",
+                isActiveText: LL.LEDGER_SIGNING(),
+                isNextText: LL.LEDGER_SIGN_DATA(),
+                isDoneText: LL.LEDGER_DATA_SIGNED(),
                 progressPercentage: 75,
                 title: LL.CERT_LEDGER_SIGN_DATA(),
                 subtitle: LL.CERT_LEDGER_SIGN_DATA_SB(),
@@ -194,7 +188,8 @@ export const LedgerSignCertificate: React.FC<Props> = ({ route }) => {
 
             await removeLedger()
 
-            MinimizerUtils.goBack()
+            // refactor(Minimizer): issues with iOS 17 & Android when connecting to desktop DApp (https://github.com/vechainfoundation/veworld-mobile/issues/951)
+            // MinimizerUtils.goBack()
 
             navigateOnFinish()
         } catch (e) {
