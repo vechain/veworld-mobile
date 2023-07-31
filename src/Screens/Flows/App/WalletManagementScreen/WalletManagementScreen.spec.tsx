b345fd459f37a96623b10063d6671ef3312674a1
Darren Kelly <darren.kelly@vechain.org>
2023-07-31 08:54:17 +0100

    feat(wallets): allow wallet deletaion

--- src/Screens/Flows/App/WalletManagementScreen/WalletManagementScreen.spec.tsx
+++ src/Screens/Flows/App/WalletManagementScreen/WalletManagementScreen.spec.tsx
@@ -2,10 +2,17 @@ import React from "react"
 import { TestWrapper } from "~Test"
 import { WalletManagementScreen } from "./WalletManagementScreen"
 import { render, screen } from "@testing-library/react-native"
+import { useCheckIdentity } from "~Hooks"
 
 const findElement = async () =>
     await screen.findByText("Wallet management", {}, { timeout: 5000 })
 
+jest.mock("~Hooks/useCheckIdentity", () => ({
+    useCheckIdentity: jest.fn(),
+}))
+;(useCheckIdentity as jest.Mock).mockReturnValue({
+    ConfirmIdentityBottomSheet: () => <></>,
+})
 describe("WalletManagementScreen", () => {
     it("should render correctly", async () => {
         render(<WalletManagementScreen />, {
