e8424beedd62f26343b59a34aef56b50fb908baa
Darren Kelly <darren.kelly@vechain.org>
2023-08-02 20:17:57 +0100

    refactor(transactions): update coverage

--- src/Hooks/useDelegation/useDelegation.spec.ts
+++ src/Hooks/useDelegation/useDelegation.spec.ts
@@ -2,11 +2,35 @@ import { renderHook } from "@testing-library/react-hooks"
 import { useDelegation } from "~Hooks"
 import { TestHelpers, TestWrapper } from "~Test"
 import { DelegationType } from "~Model/Delegation"
+import {
+    getDefaultDelegationAccount,
+    getDefaultDelegationOption,
+    getDefaultDelegationUrl,
+} from "~Storage/Redux"
 
 const { account1D1, device1 } = TestHelpers.data
 const setGasPayer = jest.fn()
 
+jest.mock("~Storage/Redux/Selectors", () => ({
+    ...jest.requireActual("~Storage/Redux/Selectors"),
+    getDefaultDelegationOption: jest.fn(),
+    getDefaultDelegationAccount: jest.fn(),
+    getDefaultDelegationUrl: jest.fn(),
+}))
 describe("useDelegation", () => {
+    beforeEach(() => {
+        jest.resetAllMocks()
+        // @ts-ignore
+        ;(getDefaultDelegationOption as jest.Mock).mockReturnValue(
+            DelegationType.NONE,
+        )
+        // @ts-ignore
+        ;(getDefaultDelegationAccount as jest.Mock).mockReturnValue(undefined)
+
+        // @ts-ignore
+        ;(getDefaultDelegationUrl as jest.Mock).mockReturnValue(undefined)
+    })
+
     it("should render", async () => {
         const { result, waitForNextUpdate } = renderHook(
             () => useDelegation({ setGasPayer }),
@@ -108,6 +132,86 @@ describe("useDelegation", () => {
 
         const { result, waitForNextUpdate } = renderHook(
             () => useDelegation({ setGasPayer, providedUrl: url }),
+            {
+                wrapper: TestWrapper,
+            },
+        )
+
+        await waitForNextUpdate()
+
+        expect(result.current.isDelegated).toBeTruthy()
+        expect(result.current.selectedDelegationOption).toEqual(
+            DelegationType.URL,
+        )
+        expect(result.current.selectedDelegationUrl).toEqual(url)
+    })
+
+    it("should reset all delegation", async () => {
+        const { result, waitForNextUpdate } = renderHook(
+            () => useDelegation({ setGasPayer }),
+            {
+                wrapper: TestWrapper,
+            },
+        )
+
+        await waitForNextUpdate()
+
+        result.current.setSelectedDelegationUrl("https://test.com")
+
+        expect(result.current.isDelegated).toBeTruthy()
+        expect(result.current.selectedDelegationUrl).toEqual("https://test.com")
+
+        result.current.setNoDelegation()
+
+        expect(result.current.isDelegated).toBeFalsy()
+        expect(result.current.selectedDelegationOption).toEqual(
+            DelegationType.NONE,
+        )
+        expect(result.current.selectedDelegationAccount).toBeUndefined()
+        expect(result.current.selectedDelegationUrl).toBeUndefined()
+    })
+
+    it("getDefaultDelegationAccount should auto set delegation account", async () => {
+        const account = {
+            ...account1D1,
+            device: device1,
+        }
+
+        // @ts-ignore
+        ;(getDefaultDelegationAccount as jest.Mock).mockReturnValue(account)
+        // @ts-ignore
+        ;(getDefaultDelegationOption as jest.Mock).mockReturnValue(
+            DelegationType.ACCOUNT,
+        )
+
+        const { result, waitForNextUpdate } = renderHook(
+            () => useDelegation({ setGasPayer }),
+            {
+                wrapper: TestWrapper,
+            },
+        )
+
+        await waitForNextUpdate()
+
+        expect(result.current.isDelegated).toBeTruthy()
+        expect(result.current.selectedDelegationOption).toEqual(
+            DelegationType.ACCOUNT,
+        )
+        expect(result.current.selectedDelegationAccount).toEqual(account)
+    })
+
+    it("getDefaultDelegationUrl should auto set delegation url", async () => {
+        const url = "https://test.com"
+
+        // @ts-ignore
+        ;(getDefaultDelegationUrl as jest.Mock).mockReturnValue(url)
+        // @ts-ignore
+        ;(getDefaultDelegationOption as jest.Mock).mockReturnValue(
+            DelegationType.URL,
+        )
+
+        const { result, waitForNextUpdate } = renderHook(
+            () => useDelegation({ setGasPayer }),
             {
                 wrapper: TestWrapper,
             },
