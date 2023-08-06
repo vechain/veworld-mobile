c9e71b8c39469a337bd40358a2b6e960e7cd04ac
Piero Bassa <pierobassa222@gmail.com>
2023-08-06 18:29:21 +0200

    feat: added name to useFungibleTokenInfo return object

--- src/Hooks/useFungibleTokenInfo/useFungibleTokenInfo.ts
+++ src/Hooks/useFungibleTokenInfo/useFungibleTokenInfo.ts
@@ -1,21 +1,34 @@
 import { useCallback, useEffect, useState } from "react"
 import { useThor } from "~Components"
 import { VET } from "~Constants"
-import { getTokenDecimals, getTokenSymbol } from "~Networking"
+import { getTokenDecimals, getTokenName, getTokenSymbol } from "~Networking"
 import { error as loggerError } from "~Utils"
 
 /**
- * React hook to fetch basic information about fungible tokens.
+ * A custom hook that fetches basic information about fungible tokens.
  *
- * @param {string} tokenAddress The address of the deployed token.
- * @returns An object containing the token's symbol and decimals.
+ * @param {string} [tokenAddress] - The address of the deployed token. If not provided, the hook won't fetch.
+ * @returns {object} An object containing:
+ *  - `name`: The name of the token (string | undefined).
+ *  - `symbol`: The symbol of the token (string | undefined).
+ *  - `decimals`: The decimals of the token (number | undefined).
+ *  - `error`: Any error that might occur during fetching (Error | null).
+ *  - `fetchData`: A function to fetch data given a token address. Returns an object with the same structure as the hook's return value.
  *
  * @example
- * const { symbol, decimals } = useFungibleTokenInfo(tokenAddress);
+ * ```typescript
+ * const { symbol, decimals, fetchData } = useFungibleTokenInfo(tokenAddress);
+ * fetchData(someOtherAddress).then(data => console.log(data.name, data.symbol, data.decimals));
+ * ```
+ *
+ * Notes:
+ * - If the provided `tokenAddress` is the same as `VET.address`, the hook won't make any fetching.
+ * - The `fetchData` function is memoized with useCallback and is dependent on the `thor` context.
  */
 export const useFungibleTokenInfo = (tokenAddress?: string) => {
     const [symbol, setSymbol] = useState<string>()
     const [decimals, setDecimals] = useState<number>()
+    const [name, setName] = useState<string>()
     const [error, setError] = useState<Error | null>(null)
 
     const thor = useThor()
@@ -29,6 +42,8 @@ export const useFungibleTokenInfo = (tokenAddress?: string) => {
                 setDecimals(resDecimals)
                 const resSymbol = await getTokenSymbol(tokenAddress!, thor)
                 setSymbol(resSymbol)
+                const resName = await getTokenName(tokenAddress!, thor)
+                setName(resName)
             } catch (err) {
                 setError(err as Error)
                 loggerError("Error getting token info", err)
@@ -44,7 +59,10 @@ export const useFungibleTokenInfo = (tokenAddress?: string) => {
 
             const resSymbol = await getTokenSymbol(_tokenAddress, thor)
 
+            const resName = await getTokenName(_tokenAddress, thor)
+
             return {
+                name: resName,
                 symbol: resSymbol,
                 decimals: resDecimals,
             }
@@ -53,6 +71,7 @@ export const useFungibleTokenInfo = (tokenAddress?: string) => {
     )
 
     return {
+        name,
         symbol,
         decimals,
         error,
