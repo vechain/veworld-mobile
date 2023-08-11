38c69ab9cb6082d0d6abf390c201f39014e0d2c5
Valazan <al-fede@hotmail.it>
2023-08-11 16:11:20 +0200

    refactor: use network type enum in hook

--- src/Hooks/useBlockchainNetwork/useBlockchainNetwork.ts
+++ src/Hooks/useBlockchainNetwork/useBlockchainNetwork.ts
@@ -1,10 +1,11 @@
 // Hook that allows you to access the current blockchain network properties
 
+import { NETWORK_TYPE } from "~Model"
 import { selectSelectedNetwork, useAppSelector } from "~Storage/Redux"
 
 export const useBlockchainNetwork = () => {
     const network = useAppSelector(selectSelectedNetwork)
-    const isMainnet = network.type === "mainnet"
+    const isMainnet = network.type === NETWORK_TYPE.MAIN
 
     return {
         network,
