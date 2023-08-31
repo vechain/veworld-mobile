e5c012f21fb89d9ce942469706c225be4cf9cf91
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2023-08-31 15:42:40 +0100

    fix(wallet-connect): handling linking when app is unlocked (#1247)

--- src/Hooks/useSetSelectedAccount/useSetSelectedAccount.ts
+++ src/Hooks/useSetSelectedAccount/useSetSelectedAccount.ts
@@ -1,11 +1,22 @@
-import { setSelectedAccount, useAppDispatch } from "~Storage/Redux"
+import {
+    selectSelectedAccount,
+    setSelectedAccount,
+    useAppDispatch,
+    useAppSelector,
+} from "~Storage/Redux"
 import { useResetStacks } from "./useResetStacks"
+import { AddressUtils } from "~Utils"
 
 export const useSetSelectedAccount = () => {
+    const selectedAccount = useAppSelector(selectSelectedAccount)
     const { resetStacks } = useResetStacks()
     const dispatch = useAppDispatch()
 
     const onSetSelectedAccount = ({ address }: { address?: string }) => {
+        if (AddressUtils.compareAddresses(selectedAccount?.address, address)) {
+            return
+        }
+
         resetStacks()
         address && dispatch(setSelectedAccount({ address }))
     }
