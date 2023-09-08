c25c373ac1effba793693e3d85ec13973e0784b4
Daithi Hearn <37153003+daithihearn@users.noreply.github.com>
2023-09-08 16:16:05 +0800

    refactor: extracting redux from persistent cache (#1297)
    * fix(wallet-connect): change URL
    
    * refactor: caching priority
    
    * style: minor changes
    
    * refactor: flashlist usage
    
    * refactor: not showing video preview on android
    
    * chore: build files
    
    * refactor: extracting redux from persistent cache
    
    * chore: build files
    
    * feat: showMargin prop
    
    * refactor: removing cache code from redux
    refactor: using a single cache for token metadata
    
    * fix: updating unit tests
    
    * refactor: cleaning up collection metadata
    
    * fix: updating unit tests
    
    * refactor: renaming cache to persisted cache
    
    * feat: supporting http for metadata and images
    
    ---------
    
    Co-authored-by: Darren Kelly <darren.kelly@vechain.org>
    Co-authored-by: Piero Bassa <pierobassa222@gmail.com>

--- src/Storage/PersistedCache/constants.ts
+++ src/Storage/PersistedCache/constants.ts
@@ -1,2 +1,2 @@
-export const CACHE_NFT_METADATA_KEY = "VeWorld_Cache__NFT_Metadata_key"
+export const CACHE_NFT_METADATA_KEY = "VeWorld_Cache_NFT_Metadata_key"
 export const CACHE_NFT_MEDIA_KEY = "VeWorld_Cache_NFT_Media_key"
