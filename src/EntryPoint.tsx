6f52671833da72cbf124c2f580c34e3ecdcfddc0
Piero Bassa <pierobassa222@gmail.com>
2023-09-12 14:22:41 +0200

    feat: app ready on entrypoint mount

--- src/EntryPoint.tsx
+++ src/EntryPoint.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react"
-import { BaseStatusBar } from "~Components"
+import { BaseStatusBar, useEncryptedStorage } from "~Components"
 import { SwitchStack } from "~Navigation"
 import ErrorBoundary from "~Components/Providers/ErrorBoundary"
 import { PinCodeProvider } from "~Components/Providers/PinCodeProvider/PinCodeProvider"
 import { AppLoader } from "./AppLoader"
-import RNBootSplash from "react-native-bootsplash"
 import { AutoLogoutProvider } from "~Components/Providers/AutoLogoutProvider"
-import { AnimatedSplashScreen } from "./AnimatedSplashScreen"
+import { AnimatedSplashScreen } from "../src/AnimatedSplashScreen"
+import RNBootSplash from "react-native-bootsplash"
 
 export const EntryPoint = () => {
+    const { setIsAppReady } = useEncryptedStorage()
+
     useEffect(() => {
-        RNBootSplash.hide({ fade: true, duration: 500 })
+        RNBootSplash.hide({ fade: false })
+        setIsAppReady(true)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
