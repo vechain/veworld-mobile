3c53c40f48cbf0659c02c747417b00720ca68911
Valazan <al-fede@hotmail.it>
2023-09-22 16:47:50 +0200

    feat: error message util tests

--- src/Utils/ErrorMessageUtils/ErrorMessageUtils.spec.ts
+++ src/Utils/ErrorMessageUtils/ErrorMessageUtils.spec.ts
@@ -1,9 +1,16 @@
 import ErrorUtils from "."
 
 describe("ErrorUtils", () => {
-    it("should read correctly the error message", () => {
+    it("should read correctly the error message of type unknown", () => {
         try {
             throw new Error("test")
+        } catch (e) {
+            expect(ErrorUtils.getErrorMessage(e)).toBe("test")
+        }
+    })
+    it("should read correctly the error message of type string", () => {
+        try {
+            throw "test"
         } catch (e) {
             expect(ErrorUtils.getErrorMessage(e)).toBe("test")
         }
