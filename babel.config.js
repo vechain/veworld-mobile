7161eddec20198511719681c4b7870e5df8d5ec4
grenos <vasilis.green@gmail.com>
2023-10-02 13:09:11 +0200

    feat: add bable plugin to remove any forgotten logs in production

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     presets: ["module:metro-react-native-babel-preset"],
 
+    env: {
+        production: {
+            plugins: ["transform-remove-console"],
+        },
+    },
+
     plugins: [
         ["module:react-native-dotenv"],
         [
