7a9dcedc8426f0d097aa990bcad175afce6292c9
Vasilis Green <Vasilis.green@gmail.com>
2023-10-06 16:58:46 +0200

    fix: fix bugs and improves performance on save nft and others (#1493)

--- src/Components/Reusable/NFTVideo/NFTVideo.tsx
+++ src/Components/Reusable/NFTVideo/NFTVideo.tsx
@@ -1,5 +1,12 @@
-import React, { memo, useRef } from "react"
-import { Video, ResizeMode } from "expo-av"
+import React, { memo, useEffect, useRef, useState } from "react"
+import {
+    Audio,
+    Video,
+    ResizeMode,
+    AVPlaybackStatus,
+    InterruptionModeAndroid,
+    InterruptionModeIOS,
+} from "expo-av"
 import { BaseView } from "~Components/Base"
 import { useTheme } from "~Hooks"
 
@@ -10,19 +17,41 @@ type Props = {
     useNativeControls: boolean
 }
 
+// Workaround to play audio on iOS
+// https://stackoverflow.com/a/74493514/7977491
+const triggerAudio = async (ref: any) => {
+    await Audio.setAudioModeAsync({
+        playsInSilentModeIOS: true, // play audio even when phone is on silent mode
+        interruptionModeAndroid: InterruptionModeAndroid.DoNotMix, // interrupt other apps' audio
+        interruptionModeIOS: InterruptionModeIOS.DoNotMix, // interrupt other apps' audio
+    })
+
+    ref.current.playAsync()
+}
+
 export const NFTVideo = memo((props: Props) => {
-    const { uri, style } = props
+    const { uri, style, isPlayAudio } = props
+    const ref = useRef(null)
+    const [status, setStatus] = useState<AVPlaybackStatus | undefined>()
+
+    useEffect(() => {
+        if (!status) return
+        if (status.isLoaded && isPlayAudio) triggerAudio(ref)
+
+        return () => {
+            ref.current = null
+        }
+    }, [ref, status, isPlayAudio])
 
     const theme = useTheme()
-
-    const video = useRef(null)
 
     return (
         <BaseView>
             <Video
-                volume={props.isPlayAudio ? 1 : 0}
+                volume={isPlayAudio ? 1 : 0}
                 usePoster
-                ref={video}
+                ref={ref}
+                onPlaybackStatusUpdate={setStatus}
                 shouldPlay
                 useNativeControls={props.useNativeControls}
                 style={[
