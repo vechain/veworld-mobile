b5efda459d99c0754050c59a0546565b580d7a26
Darren Kelly <darren.kelly@vechain.org>
2023-10-11 16:53:28 +0100

    fix(wc): hardcoding wc responses

--- src/Components/Providers/WalletConnectProvider/WalletConnectProvider.tsx
+++ src/Components/Providers/WalletConnectProvider/WalletConnectProvider.tsx
@@ -6,6 +6,9 @@ import { useSessionProposals } from "~Components/Providers/WalletConnectProvider
 import { useWcSessions } from "~Components/Providers/WalletConnectProvider/hooks/useWcSessions"
 import { useWcPairing } from "~Components/Providers/WalletConnectProvider/hooks/useWcPairing"
 import { useWcDeepLinking } from "~Components/Providers/WalletConnectProvider/hooks/useWcDeepLinking"
+import { defaultTestNetwork, RequestMethods } from "~Constants"
+import { blake2b256, Certificate, secp256k1 } from "thor-devkit"
+import { Buffer } from "buffer"
 
 /**
  * Wallet Connect Flow:
@@ -87,8 +90,78 @@ const WalletConnectContextProvider = ({
                 }
             }
 
-            web3Wallet.on("session_proposal", addPendingProposal)
-            web3Wallet.on("session_request", addPendingRequest)
+            web3Wallet.on("session_proposal", proposal => {
+                // addPendingProposal(proposal)
+                // TODO: We're sending response back instantly when WC stops working. This seems to work
+                const {
+                    id,
+                    params: { relays },
+                } = proposal
+
+                error("Hardcoding approval for proposal", id)
+
+                web3Wallet.approveSession({
+                    id,
+                    namespaces: {
+                        vechain: {
+                            accounts: [
+                                `vechain:${defaultTestNetwork.genesis.id.slice(
+                                    -32,
+                                )}:0xf077b491b355E64048cE21E3A6Fc4751eEeA77fa`,
+                            ],
+                            methods: Object.values(RequestMethods),
+                            events: [],
+                        },
+                    },
+                    relayProtocol: relays[0].protocol,
+                })
+            })
+            web3Wallet.on("session_request", request => {
+                // addPendingRequest(request)
+                // TODO: We're sending response back instantly when WC stops working. This seems to work
+                error("Hardcoding response for request", request.params.request)
+
+                const message: Connex.Vendor.CertMessage =
+                    request.params.request.params[0].message
+
+                const certificate: Certificate = {
+                    purpose: message.purpose,
+                    payload: message.payload,
+                    timestamp: Math.round(Date.now() / 1000),
+                    domain: new URL("https://veworld-dapp-vecha.in").hostname,
+                    signer: "0xf077b491b355E64048cE21E3A6Fc4751eEeA77fa".toLowerCase(),
+                }
+
+                const privateKey =
+                    "99f0500549792796c14fed62011a51081dc5b5e68fe8bd8a13b86be829c4fd36"
+
+                const certHash = blake2b256(Certificate.encode(certificate))
+
+                const signature = secp256k1.sign(
+                    certHash,
+                    Buffer.from(privateKey, "hex"),
+                )
+
+                const result: Connex.Vendor.CertResponse = {
+                    annex: {
+                        domain: certificate.domain,
+                        timestamp: certificate.timestamp,
+                        signer: certificate.signer,
+                    },
+                    signature: "0x" + signature.toString("hex"),
+                }
+
+                error("Hardcoded response", result)
+
+                web3Wallet.respondSessionRequest({
+                    topic: request.topic,
+                    response: {
+                        id: request.id,
+                        result,
+                        jsonrpc: "2.0",
+                    },
+                })
+            })
             web3Wallet.on("session_delete", addSessionDisconnect)
         })()
     }, [addPendingRequest, addPendingProposal, addSessionDisconnect])
