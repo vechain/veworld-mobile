2780897a6ef7dd1e1dd142ab8cf10f79ae8fb0d7
Davide Carpini <carpini.davide@gmail.com>
2023-10-19 17:02:58 +0200

    fix: remove useless test.setup (#1563)

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,7 @@
 module.exports = {
     default: {
         paths: ["e2e/features/**/*.feature"],
-        require: ["e2e/**/*.ts", "tests.setup.js"],
+        require: ["e2e/**/*.ts"],
         requireModule: ["ts-node/register"],
         formatOptions: {
             snippetInterface: "synchronous",
