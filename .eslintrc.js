ab4998fa891c0bca105305180fc33b482e7f2611
grenos <vasilis.green@gmail.com>
2023-11-22 12:17:43 +0100

    chore: change lint and prettier line length rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
                 semi: "off",
                 curly: "off",
                 quotes: [2, "double", { avoidEscape: true }],
+                "max-len": ["error", { code: 120, comments: 220 }],
                 "i18next/no-literal-string": "error",
                 "no-console": "error",
                 "no-duplicate-imports": "error",
