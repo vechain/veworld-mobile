f2702dda217972f26a1af7f4d00d74b7a3019c28
Darren Kelly <darren.kelly@vechain.org>
2023-11-29 09:20:42 +0000

    feat: tracking bookmarked dapps

--- src/Screens/Flows/App/InAppBrowser/Components/BrowserBottomBar.tsx
+++ src/Screens/Flows/App/InAppBrowser/Components/BrowserBottomBar.tsx
@@ -1,18 +1,9 @@
-import React, { useCallback, useMemo } from "react"
+import React, { useMemo } from "react"
 import { StyleSheet } from "react-native"
 import { BaseIcon, BaseView } from "~Components"
-import { useTheme } from "~Hooks"
+import { useDappBookmarking, useTheme } from "~Hooks"
 import { useInAppBrowser } from "~Components/Providers/InAppBrowserProvider"
-import { ColorThemeType, DiscoveryDApp } from "~Constants"
-import {
-    addBookmark,
-    removeBookmark,
-    selectAllDapps,
-    selectBookmarkedDapps,
-    useAppDispatch,
-    useAppSelector,
-} from "~Storage/Redux"
-import { URIUtils } from "~Utils"
+import { ColorThemeType } from "~Constants"
 
 type IconProps = {
     name: string
@@ -24,55 +15,8 @@ export const BrowserBottomBar: React.FC = () => {
     const { canGoBack, canGoForward, goBack, goForward, navigationState, webviewRef } = useInAppBrowser()
     const theme = useTheme()
     const styles = createStyles(theme)
-    const dispatch = useAppDispatch()
 
-    const bookmarkedDapps: DiscoveryDApp[] = useAppSelector(selectBookmarkedDapps)
-    const allDApps = useAppSelector(selectAllDapps)
-
-    const existingBookmark = useMemo(() => {
-        if (!navigationState?.url) return undefined
-
-        try {
-            return bookmarkedDapps.find(bookmark =>
-                URIUtils.compareURLs(URIUtils.clean(bookmark.href), URIUtils.clean(navigationState.url)),
-            )
-        } catch {
-            return undefined
-        }
-    }, [bookmarkedDapps, navigationState])
-
-    const isBookMarked = useMemo(() => {
-        return !!existingBookmark
-    }, [existingBookmark])
-
-    const toggleBookmark = useCallback(async () => {
-        if (!navigationState?.url) return
-
-        if (existingBookmark) {
-            dispatch(removeBookmark(existingBookmark))
-        } else {
-            const existingDApp = allDApps.find(dapp =>
-                URIUtils.compareURLs(URIUtils.clean(dapp.href), URIUtils.clean(navigationState.url)),
-            )
-
-            if (existingDApp) {
-                return dispatch(addBookmark(existingDApp))
-            } else {
-                const url = new URL(navigationState.url)
-
-                const bookmark: DiscoveryDApp = {
-                    name: navigationState.title,
-                    href: URIUtils.clean(url.href),
-                    desc: "",
-                    isCustom: true,
-                    createAt: new Date().getTime(),
-                    amountOfNavigations: 1,
-                }
-
-                dispatch(addBookmark(bookmark))
-            }
-        }
-    }, [dispatch, existingBookmark, navigationState, allDApps])
+    const { isBookMarked, toggleBookmark } = useDappBookmarking(navigationState?.url, navigationState?.title)
 
     const IconConfig: IconProps[] = useMemo(() => {
         return [
