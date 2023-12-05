09a9ab68e21ed131d930136619eadbb63361d7c1
grenos <vasilis.green@gmail.com>
2023-12-05 14:40:29 +0100

    fix: fix sort mnemonic edge case

--- src/Screens/Flows/WalletCreation/NewMnemonicScreen/useGenerateMnemonic.tsx
+++ src/Screens/Flows/WalletCreation/NewMnemonicScreen/useGenerateMnemonic.tsx
@@ -1,16 +1,29 @@
 import { useEffect, useState } from "react"
 import { InteractionManager } from "react-native"
 import { mnemonic as thorMnemonic } from "thor-devkit"
+import { setIsAppLoading, useAppDispatch } from "~Storage/Redux"
 
 export const useGenerateMnemonic = () => {
     const [mnemonic, setMnemonic] = useState<string[]>(Array.from({ length: 12 }))
+    const dispatch = useAppDispatch()
 
     useEffect(() => {
         InteractionManager.runAfterInteractions(() => {
-            const seed = thorMnemonic.generate()
-            setMnemonic(seed)
+            function init() {
+                dispatch(setIsAppLoading(true))
+                const seed = thorMnemonic.generate()
+
+                if (seed.length === 12) {
+                    setMnemonic(seed)
+                    dispatch(setIsAppLoading(false))
+                } else {
+                    init()
+                }
+            }
+
+            init()
         })
-    }, [])
+    }, [dispatch])
 
     return { mnemonic }
 }
