62e9cdc12a89b00fa18ba191d07605b7f64a1694
ve-akanoce <erik.nucibella@vechain.org>
2023-12-18 16:21:22 +0100

    feat(coingecko): add endpoints and react-query hooks for chartData, marketInfo, exchangeRate

--- src/Api/Coingecko/endpoints.ts
+++ src/Api/Coingecko/endpoints.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { VET, VTHO } from "~Constants"
 import { CurrencyExchangeRate } from "~Model"
+import { error } from "~Utils"
 
 export const COINGECKO_URL = process.env.REACT_APP_COINGECKO_URL
 const timeout = Number(
@@ -21,12 +22,80 @@ export const getCoinGeckoIdBySymbol = {
 }
 
 export const getCoingeckoCurrencies = async (): Promise<string[]> => {
-    const currencies = await axiosInstance.get<string[]>(
-        "/simple/supported_vs_currencies",
-    )
+    try {
+        const currencies = await axiosInstance.get<string[]>(
+            "/simple/supported_vs_currencies",
+        )
 
-    //Sort alphabetically
-    return currencies.data.sort((a, b) => a.localeCompare(b))
+        //Sort alphabetically
+        return currencies.data.sort((a, b) => a.localeCompare(b))
+    } catch (e) {
+        error("getCoingeckoCurrencies", e)
+        throw e
+    }
+}
+
+export interface MarketInfoResponse {
+    id: string
+    symbol: string
+    name: string
+    image: string
+    current_price: number
+    market_cap: number
+    market_cap_rank: number
+    fully_diluted_valuation: number
+    total_volume: number
+    high_24h: number
+    low_24h: number
+    price_change_24h: number
+    price_change_percentage_24h: number
+    market_cap_change_24h: number
+    market_cap_change_percentage_24h: number
+    circulating_supply: number
+    total_supply: number
+    max_supply: number
+    ath: number
+    ath_change_percentage: number
+    ath_date: string
+    atl: number
+    atl_change_percentage: number
+    atl_date: string
+    roi: {
+        times: number
+        currency: string
+        percentage: number
+    }
+    last_updated: string
+}
+
+export const getMarketInfo = async ({
+    coinGeckoIds,
+    vs_currency,
+}: {
+    coinGeckoIds?: string[]
+    vs_currency: string
+}) => {
+    try {
+        const pricesResponse = await axios.get<MarketInfoResponse[]>(
+            "/coins/markets",
+            {
+                params: {
+                    ids: coinGeckoIds,
+                    vs_currency,
+                    order: "market_cap_desc",
+                    per_page: 100,
+                    page: 1,
+                    sparkline: false,
+                    locale: "en",
+                },
+            },
+        )
+
+        return pricesResponse.data
+    } catch (e) {
+        error("fetchVechainMarketInfo", e)
+        throw e
+    }
 }
 
 interface PriceChangeResponse {
@@ -40,38 +109,87 @@ export const getExchangeRate = async ({
     coinGeckoId?: string
     vs_currency: string
 }): Promise<CurrencyExchangeRate> => {
-    // Just for better react-query support. We'll never reach this point if used via react-query hooks
-    if (!coinGeckoId) throw new Error("CoinGecko ID is not defined")
+    try {
+        // Just for better react-query support. We'll never reach this point if used via react-query hooks
+        if (!coinGeckoId) throw new Error("CoinGecko ID is not defined")
 
-    const pricesResponse = await axiosInstance.get<PriceChangeResponse>(
-        `/coins/${coinGeckoId}/market_chart`,
-        {
-            params: {
-                days: 1,
-                vs_currency: vs_currency,
+        const pricesResponse = await axiosInstance.get<PriceChangeResponse>(
+            `/coins/${coinGeckoId}/market_chart`,
+            {
+                params: {
+                    days: 1,
+                    vs_currency: vs_currency,
+                },
             },
-        },
-    )
+        )
 
-    const prices = pricesResponse.data.prices
+        const prices = pricesResponse.data.prices
 
-    //The current exchange rate - the last entry
-    const currentExchangeRate = prices[prices.length - 1][1]
+        //The current exchange rate - the last entry
+        const currentExchangeRate = prices[prices.length - 1][1]
 
-    let percentChange
+        let percentChange
 
-    //First entry is 24 hours ago
-    const yesterdaysExchangeRate = prices[0][1]
-    //Difference between today and yesterday
-    const priceChange = currentExchangeRate - yesterdaysExchangeRate
-    //Percent of yesterdays price
-    const change = (priceChange / yesterdaysExchangeRate) * 100
-    //Rounded to 2 digits
-    percentChange = +change.toFixed(2)
+        //First entry is 24 hours ago
+        const yesterdaysExchangeRate = prices[0][1]
+        //Difference between today and yesterday
+        const priceChange = currentExchangeRate - yesterdaysExchangeRate
+        //Percent of yesterdays price
+        const change = (priceChange / yesterdaysExchangeRate) * 100
+        //Rounded to 2 digits
+        percentChange = +change.toFixed(2)
 
-    return {
-        rate: currentExchangeRate,
-        change: percentChange,
-        coinGeckoId,
+        return {
+            rate: currentExchangeRate,
+            change: percentChange,
+            coinGeckoId,
+        }
+    } catch (e) {
+        error("getExchangeRate", e)
+        throw e
+    }
+}
+
+export type MarketChartResponse = {
+    timestamp: number
+    value: number
+}[]
+/**
+ * Get the market chart of a coin for a given number of days and currency
+ * @param coinGeckoId - The CoinGecko ID of the coin
+ * @param vs_currency - The currency to compare
+ * @param days - The number of days to get the market chart for
+ * @returns  the market chart array of arrays of [timestamp, price]
+ */
+export const getMarketChart = async ({
+    coinGeckoId,
+    vs_currency,
+    days,
+}: {
+    coinGeckoId?: string
+    vs_currency: string
+    days: number
+}): Promise<MarketChartResponse> => {
+    try {
+        // Just for better react-query support. We'll never reach this point if used via react-query hooks
+        if (!coinGeckoId) throw new Error("CoinGecko ID is not defined")
+
+        const pricesResponse = await axiosInstance.get<PriceChangeResponse>(
+            `/coins/${coinGeckoId}/market_chart`,
+            {
+                params: {
+                    days,
+                    vs_currency,
+                },
+            },
+        )
+
+        return pricesResponse.data.prices.map(entry => ({
+            timestamp: entry[0],
+            value: entry[1],
+        }))
+    } catch (e) {
+        error("getMarketChart", e)
+        throw e
     }
 }
--- src/Api/Coingecko/hooks.ts
+++ src/Api/Coingecko/hooks.ts
@@ -1,8 +1,12 @@
 import { useQuery } from "@tanstack/react-query"
 import {
+    MarketChartResponse,
     VETHOR_COINGECKO_ID,
     VET_COINGECKO_ID,
+    getCoinGeckoIdBySymbol,
     getExchangeRate,
+    getMarketChart,
+    getMarketInfo,
 } from "./endpoints"
 
 const getExchangeRateQueryKey = ({
@@ -29,6 +33,113 @@ export const useExchangeRate = ({
         queryKey: getExchangeRateQueryKey({ id, vs_currency }),
         queryFn: () => getExchangeRate({ coinGeckoId: id, vs_currency }),
         enabled: !!id,
+    })
+}
+
+const getMarketsInfoQueryKey = ({ vs_currency }: { vs_currency: string }) => [
+    "MARKET_INFO",
+    vs_currency,
+]
+
+/**
+ * Get the market info of all supported coins for a given currency (getCoinGeckoIdBySymbol)
+ * @param vs_currency  the currency to compare
+ * @returns  the market info
+ */
+export const useMarketsInfo = ({ vs_currency }: { vs_currency: string }) => {
+    const coinGeckoIds = Object.values(getCoinGeckoIdBySymbol)
+    return useQuery({
+        queryKey: getMarketsInfoQueryKey({ vs_currency }),
+        queryFn: () => getMarketInfo({ coinGeckoIds, vs_currency }),
+        staleTime: Infinity,
+    })
+}
+
+const getMarketInfoQueryKey = ({
+    id,
+    vs_currency,
+}: {
+    id: string | number
+    vs_currency: string
+}) => ["MARKET_INFO", vs_currency, id]
+
+/**
+ * Get the market info of a coin for a given currency
+ * @param id  the id of the coin
+ * @param vs_currency  the currency to compare
+ * @returns  the market info
+ */
+export const useMarketInfo = ({
+    id,
+    vs_currency,
+}: {
+    id: string | number
+    vs_currency: string
+}) => {
+    const { data: marketsInfo } = useMarketsInfo({ vs_currency })
+    return useQuery({
+        queryKey: getMarketInfoQueryKey({ id, vs_currency }),
+        queryFn: () => {
+            return marketsInfo?.find(market => market.id === id)
+        },
+        staleTime: Infinity,
+        enabled: !!marketsInfo,
+    })
+}
+
+const getMarketChartQueryKey = ({
+    id,
+    vs_currency,
+    days,
+}: {
+    id?: string
+    vs_currency: string
+    days: number
+}) => ["MARKET_CHART", id, vs_currency, days]
+
+/**
+ *  Get the market chart of a coin for a given number of days and currency
+ * @param id  the id of the coin
+ * @param vs_currency  the currency to compare
+ * @param days  the number of days to get the market chart for
+ * @returns  the market chart array of arrays of [timestamp, price]
+ */
+
+export const DEFAULT_CHART_DATA = [
+    { timestamp: 0, value: 1 },
+    { timestamp: 1, value: 1 },
+    { timestamp: 2, value: 1 },
+    { timestamp: 3, value: 1 },
+    { timestamp: 4, value: 1 },
+    { timestamp: 5, value: 1 },
+    { timestamp: 6, value: 1 },
+    { timestamp: 7, value: 1 },
+    { timestamp: 8, value: 1 },
+    { timestamp: 9, value: 1 },
+    { timestamp: 10, value: 1 },
+    { timestamp: 11, value: 1 },
+    { timestamp: 12, value: 1 },
+]
+
+export const useMarketChart = ({
+    id,
+    vs_currency,
+    days,
+    initialData,
+}: {
+    id?: string
+    vs_currency: string
+    days: number
+    initialData?: MarketChartResponse
+}) => {
+    return useQuery({
+        queryKey: getMarketChartQueryKey({ id, vs_currency, days }),
+        queryFn: () => getMarketChart({ coinGeckoId: id, vs_currency, days }),
+        enabled: !!id,
+        initialData,
+        staleTime: Number(
+            process.env.REACT_APP_CHART_DATA_SYNC_PERIOD ?? 1000 * 60 * 5,
+        ),
     })
 }
 
