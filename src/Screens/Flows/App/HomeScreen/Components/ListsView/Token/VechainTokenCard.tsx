667fc3dc6c39883e8cd456af2aa2040882efd458
ve-akanoce <erik.nucibella@vechain.org>
2023-12-20 11:46:14 +0100

    fix: remove duplicate code from merge

--- src/Screens/Flows/App/HomeScreen/Components/ListsView/Token/VechainTokenCard.tsx
+++ src/Screens/Flows/App/HomeScreen/Components/ListsView/Token/VechainTokenCard.tsx
@@ -32,10 +32,6 @@ export const VechainTokenCard = memo(({ tokenWithInfo, isAnimation, isBalanceVis
             tokenInfo?.market_data.price_change_percentage_24h ?? 0,
             tokenInfo?.market_data.price_change_percentage_24h ?? 0,
         ) +
-        FormattingUtils.humanNumber(
-            tokenInfo?.market_data.price_change_percentage_24h ?? 0,
-            tokenInfo?.market_data.price_change_percentage_24h ?? 0,
-        ) +
         "%"
 
     const animatedOpacityReverse = useAnimatedStyle(() => {
