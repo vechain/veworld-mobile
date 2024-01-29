16ec5c1eeed159e02d397dde22322dd6d5e19d19
grenos <vasilis.green@gmail.com>
2024-01-29 15:28:10 +0100

    fix: Sentry pod issue, set cb flag to off for ios, add error boundary log

--- src/Constants/Constants/feature.ts
+++ src/Constants/Constants/feature.ts
@@ -1,1 +1,6 @@
-export const BUY_FEATURE_ENABLED = true
+import { PlatformUtils } from "~Utils"
+
+// Feature flags
+
+// TODO - Temporary flag to disable the buy feature for iOS
+export const BUY_FEATURE_ENABLED = PlatformUtils.isAndroid()
