cb6ed51336a451586ef76933c5ba6e4ae1b7d0ae
Valazan <al-fede@hotmail.it>
2024-01-31 17:45:54 +0100

    feat: adding validation on generated words

--- src/Screens/Flows/WalletCreation/NewMnemonicScreen/useGenerateMnemonic.tsx
+++ src/Screens/Flows/WalletCreation/NewMnemonicScreen/useGenerateMnemonic.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import { InteractionManager } from "react-native"
 import { mnemonic as thorMnemonic } from "thor-devkit"
+import { ERROR_EVENTS } from "~Constants"
 import { setIsAppLoading, useAppDispatch } from "~Storage/Redux"
+import { error } from "~Utils"
 
 export const useGenerateMnemonic = () => {
     const [mnemonic, setMnemonic] = useState<string[]>(Array.from({ length: 12 }))
@@ -13,10 +15,11 @@ export const useGenerateMnemonic = () => {
                 dispatch(setIsAppLoading(true))
                 const seed = thorMnemonic.generate()
 
-                if (seed.length === 12) {
+                if (seed.length === 12 && seed.every(word => word.length > 0)) {
                     setMnemonic(seed)
                     dispatch(setIsAppLoading(false))
                 } else {
+                    error(ERROR_EVENTS.WALLET_CREATION, "Generated mnemonic has missing words")
                     init()
                 }
             }
