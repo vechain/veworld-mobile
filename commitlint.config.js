68fdda69c709ec34550793c8e16e3abb950a58fd
grenos <vasilis.green@gmail.com>
2024-02-13 18:06:02 +0100

    fix: config commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,1 @@
-module.exports = { extends: ["@commitlint/config-conventional"] }
+module.exports = { extends: ["@commitlint/config-conventional"], rules: { "subject-min-length": [2, "always", 4] } }
