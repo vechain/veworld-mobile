c7157c79b9dc217585fee63683d05c185b0f0272
grenos <vasilis.green@gmail.com>
2024-02-16 20:15:30 +0100

    build: init

--- src/Screens/Flows/Onboarding/WelcomeScreen/WelcomeScreen.tsx
+++ src/Screens/Flows/Onboarding/WelcomeScreen/WelcomeScreen.tsx
@@ -58,6 +58,8 @@ export const WelcomeScreen = () => {
     }, [createWallet, dispatch, migrateOnboarding])
     // end dev button
 
+    const isShowDevButton = process.env.IS_CI_BUILD_ENABLED || devFeaturesEnabled
+
     return (
         <Layout
             noBackButton
@@ -111,9 +113,9 @@ export const WelcomeScreen = () => {
                         testID="GET_STARTED_BTN"
                         haptics="Medium"
                     />
-                    {(devFeaturesEnabled || process.env.IS_CI_BUILD_ENABLED) && (
+                    {isShowDevButton ? (
                         <BaseButton size="md" variant="link" action={onDemoOnboarding} title="DEV:DEMO" />
-                    )}
+                    ) : null}
                 </BaseView>
             }
         />
