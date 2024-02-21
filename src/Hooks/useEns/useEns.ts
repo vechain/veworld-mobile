e8f68bfaf8ae2d3eb6e83e3e22fab94bb42ab366
grenos <vasilis.green@gmail.com>
2024-02-21 17:33:29 +0100

    fix: ueEns hook, remove external dep

--- src/Hooks/useEns/useEns.ts
+++ src/Hooks/useEns/useEns.ts
@@ -1,41 +1,97 @@
 import { useThor } from "~Components"
 import { useCallback, useEffect, useState } from "react"
-import { vendor } from "./MockVendor"
-import { getAddress, getName } from "@vechain.energy/dapp-kit-hooks"
 
 export const useEns = ({ name, address }: { name?: string; address?: string }) => {
     const thor = useThor()
-    const [_name, setName] = useState("")
-    const [_address, setAddres] = useState("")
+    const [stateName, setName] = useState("")
+    const [stateAddress, setAddres] = useState("")
 
-    const _getAddress = useCallback(async () => {
-        const connex: Connex = {
-            thor: thor,
-            vendor: vendor,
-        }
+    const _getAddress = useCallback(
+        async (_name: string) => {
+            const {
+                decoded: { addresses },
+            } = await thor.account(VET_RESOLVER_UTILS).method(ABI.getAddresses).call([_name])
 
-        setAddres(await getAddress(_address, connex))
-    }, [thor, _address])
+            if (Array.isArray(addresses) && addresses[0]) {
+                setAddres(addresses[0])
+                return
+            }
 
-    const _getName = useCallback(async () => {
-        const connex: Connex = {
-            thor: thor,
-            vendor: vendor,
-        }
+            setAddres(addresses[0])
+        },
+        [thor],
+    )
 
-        setName(await getName(_name, connex))
-    }, [_name, thor])
+    const _getName = useCallback(
+        async (_address: string) => {
+            const {
+                decoded: { names },
+            } = await thor.account(VET_RESOLVER_UTILS).method(ABI.getNames).call([_address])
+
+            if (Array.isArray(names) && names[0]) {
+                setName(names[0])
+                return
+            }
+
+            setName(names[0])
+        },
+        [thor],
+    )
 
     useEffect(() => {
         if (name && !address) {
-            _getAddress()
+            _getAddress(name)
         } else if (!name && address) {
-            _getName()
-        } else {
-            _getAddress()
-            _getName()
+            _getName(address)
+        } else if (name && address) {
+            _getAddress(name)
+            _getName(address)
         }
     }, [address, _getAddress, _getName, name])
 
-    return { name, address }
+    return { name: stateName, address: stateAddress }
+}
+
+const VET_RESOLVER_UTILS = "0xA11413086e163e41901bb81fdc5617c975Fa5a1A"
+
+const ABI = {
+    getAddresses: {
+        inputs: [
+            {
+                internalType: "string[]",
+                name: "names",
+                type: "string[]",
+            },
+        ],
+        name: "getAddresses",
+        outputs: [
+            {
+                internalType: "address[]",
+                name: "addresses",
+                type: "address[]",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+
+    getNames: {
+        inputs: [
+            {
+                internalType: "address[]",
+                name: "addresses",
+                type: "address[]",
+            },
+        ],
+        name: "getNames",
+        outputs: [
+            {
+                internalType: "string[]",
+                name: "names",
+                type: "string[]",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
 }
