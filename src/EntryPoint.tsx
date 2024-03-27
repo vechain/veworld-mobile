731d0e56d5ab80508eddd832d27690157eba8298
ShaunKav <shaun.kavanagh@vechain.org>
2024-03-27 16:05:29 +0000

    feat: datadog crash testing

--- src/EntryPoint.tsx
+++ src/EntryPoint.tsx
@@ -1,4 +1,11 @@
 import React, { useEffect } from "react"
+// Import Datadog SDK dependencies
+import { Button, View } from "react-native"
+
+import { DatadogProvider, DatadogProviderConfiguration } from "@datadog/mobile-react-native"
+import { crashJavascriptThread, crashNativeMainThread } from "react-native-performance-limiter"
+
+// Existing imports
 import { AutoLockProvider, BaseStatusBar, ErrorBoundary, useApplicationSecurity } from "~Components"
 import { SwitchStack } from "~Navigation"
 import { AppLoader } from "./AppLoader"
@@ -7,29 +14,55 @@ import RNBootSplash from "react-native-bootsplash"
 import { SecurityLevelType } from "~Model"
 import { PlatformUtils } from "~Utils"
 
+// Datadog SDK configuration
+const config = new DatadogProviderConfiguration(
+    "pubfcce59cd78594f40fb3dba62fc34280b", // Replace with your actual client token
+    "dev-test", // Environment name
+    "4b98025d-dc62-423c-8ee3-4e9002448328", // Application ID
+    true, // Track user interactions
+    true, // Track XHR Resources
+    true, // Track Errors
+)
+config.site = "EU1" // Set the Datadog site
+// Additional optional configurations...
+config.nativeCrashReportEnabled = true // enable native crash reporting
+
+const crashApp = () => {
+    crashNativeMainThread("Test crash here on entry Native ")
+}
+const crashAppJS = () => {
+    crashJavascriptThread("custom error message Javascript")
+}
+
 export const EntryPoint = () => {
     const { setIsAppReady, securityType } = useApplicationSecurity()
 
     useEffect(() => {
         RNBootSplash.hide({ fade: false })
         setIsAppReady(true)
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [setIsAppReady])
 
     return (
-        <ErrorBoundary>
-            <PlatformAutolock>
-                <AnimatedSplashScreen
-                    playAnimation={true}
-                    useFadeOutAnimation={securityType === SecurityLevelType.SECRET}>
-                    <AppLoader>
-                        <BaseStatusBar />
-                        <SwitchStack />
-                    </AppLoader>
-                </AnimatedSplashScreen>
-            </PlatformAutolock>
-        </ErrorBoundary>
+        <DatadogProvider configuration={config}>
+            <ErrorBoundary>
+                <PlatformAutolock>
+                    <AnimatedSplashScreen
+                        playAnimation={true}
+                        useFadeOutAnimation={securityType === SecurityLevelType.SECRET}>
+                        <AppLoader>
+                            <BaseStatusBar />
+                            {/* Centered Buttons */}
+                            {/*<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>*/}
+                            <View>
+                                <Button title="Crash Native Thread" onPress={() => crashApp()} />
+                                <Button title="Crash JS Thread" onPress={() => crashAppJS()} />
+                            </View>
+                            <SwitchStack />
+                        </AppLoader>
+                    </AnimatedSplashScreen>
+                </PlatformAutolock>
+            </ErrorBoundary>
+        </DatadogProvider>
     )
 }
 
