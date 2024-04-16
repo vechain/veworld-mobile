4ed7dbaf1093350706d8587ba0c9114117f2e44f
ShaunKav <shaun.kavanagh@vechain.org>
2024-04-16 17:12:07 +0100

    refactor: log action changes

--- src/Logging/RumManager/RumManager.ts
+++ src/Logging/RumManager/RumManager.ts
@@ -1,16 +1,9 @@
 import { DdRum, RumActionType } from "@datadog/mobile-react-native"
 class RumManager {
-    private viewName: string
-
-    constructor() {
-        this.viewName = ""
-    }
-
     public logAction(viewName: string, actionName: string): void {
-        this.viewName = viewName
         DdRum.startView(viewName, viewName, {}, Date.now())
         DdRum.addAction(RumActionType.TAP, actionName)
-        DdRum.stopView(this.viewName)
+        DdRum.stopView(viewName)
     }
 }
 
