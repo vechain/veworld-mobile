749a14ee954a88ae6f87c0baec03aff961b73d5e
grenos <vasilis.green@gmail.com>
2024-05-22 15:29:21 +0200

    feat: add warning modal

--- src/Screens/Flows/App/SecurityUpgrade_V2/SecurityUpgrade_V2.tsx
+++ src/Screens/Flows/App/SecurityUpgrade_V2/SecurityUpgrade_V2.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useEffect } from "react"
 import {
     BaseButton,
     BaseSafeArea,
@@ -13,14 +13,29 @@ import { useBackupMnemonic } from "../PrivacyScreen/Hooks/useBackupMnemonic"
 import { LocalDevice } from "~Model"
 import { selectLocalDevices, useAppSelector } from "~Storage/Redux"
 import { BackupMnemonicBottomSheet } from "../PrivacyScreen/Components"
+import { BackHandler } from "react-native"
+import { BackupWarningBottomSheet } from "../PrivacyScreen/Components/BackupWarningBottomSheet"
 
 export const SecurityUpgrade_V2 = () => {
+    useEffect(() => {
+        // Remove hardware back button press for this screen on Android
+        const backHandler = BackHandler.addEventListener("hardwareBackPress", () => false)
+        return () => backHandler.remove()
+    }, [])
+
     const { isWalletSecurityBiometrics } = useWalletSecurity()
     const devices = useAppSelector(selectLocalDevices) as LocalDevice[]
     const { isOpen: isPasswordPromptOpen, onOpen: openPasswordPrompt, onClose: closePasswordPrompt } = useDisclosure()
 
     const onRunV2Upgrade = useCallback(() => {
-        // run state and wallet upgrade
+        /*
+            TODO
+
+            1. get all wallets and decrypt them using the old flow
+            2. encrypt them using the new flow
+            3. save them back to the storage
+            4. remove the old keys from the keychain
+        */
     }, [])
 
     const {
@@ -33,6 +48,12 @@ export const SecurityUpgrade_V2 = () => {
         ref: walletMgmtBottomSheetRef,
         openWithDelay: openWalletMgmtSheetWithDelay,
         onClose: closeWalletMgmtSheet,
+    } = useBottomSheetModal()
+
+    const {
+        ref: backupWarningSheetRef,
+        onOpen: openBackupWarningSheet,
+        onClose: closeBackupWarningSheet,
     } = useBottomSheetModal()
 
     const { onPasswordSuccess, checkSecurityBeforeOpening, handleOnSelectedWallet, mnemonicArray } = useBackupMnemonic({
@@ -61,7 +82,7 @@ export const SecurityUpgrade_V2 = () => {
                 </BaseView>
 
                 <BaseView pb={62}>
-                    <BaseButton title="DONE" action={onRunV2Upgrade} />
+                    <BaseButton title="Upgrade Security" action={openBackupWarningSheet} />
                 </BaseView>
             </BaseView>
 
@@ -81,6 +102,13 @@ export const SecurityUpgrade_V2 = () => {
                 isOpen={isPasswordPromptOpen}
                 onClose={closePasswordPrompt}
                 onSuccess={onPasswordSuccess}
+            />
+
+            <BackupWarningBottomSheet
+                ref={backupWarningSheetRef}
+                onConfirm={onRunV2Upgrade}
+                onClose={closeBackupWarningSheet}
+                isUpgradeSecurity={true}
             />
         </BaseSafeArea>
     )
