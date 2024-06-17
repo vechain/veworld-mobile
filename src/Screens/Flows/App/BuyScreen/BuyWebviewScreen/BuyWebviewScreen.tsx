698284cf1b0a8007e85f2654509d0e27242c61cd
Kristian Barrese <kristian.webdeveloper@gmail.com>
2024-06-17 16:48:28 +0200

    2055 transak onramp (#2059)
    * feat: buy integrate transak, add new track events
    
    * style: backbuttonheader supports text
    
    * fix: vas comments + fix Transak color management
    
    * fix: backbuttonheader embedded styles
    
    * feat: add apple pay antitlement
    
    * chore: removes apple pay entitlements
    
    * fix: transak config with working apple pay
    
    * feat: transak event tracking update
    
    * fix: temp remove google_pay
    
    * feat: custom back to app button at end of transak buy
    
    * fix: warning on immutable middleware timings
    
    * fix: possible for jest broken build on transak sdk
    
    * fix: possible for jest broken build on transak sdk 2
    
    * fix: possible for jest broken build on transak sdk 3
    
    * fix: possible for jest broken build uuid
    
    * fix: possible for jest broken build uuid 2
    
    * fix: possible for jest broken build netinfo
    
    * fix: temp remove google_pay
    
    * fix: possible for jest broken build react-native-device-info
    
    * fix: jest getuniqueidsync
    
    * fix: resolve small issues
    
    ---------
    
    Co-authored-by: grenos <vasilis.green@gmail.com>

--- src/Screens/Flows/App/BuyScreen/BuyWebviewScreen/BuyWebviewScreen.tsx
+++ src/Screens/Flows/App/BuyScreen/BuyWebviewScreen/BuyWebviewScreen.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import React, { useMemo } from "react"
-import { BackButtonHeader, BaseSpacer, CoinbasePayWebView, Layout } from "~Components"
+import { BackButtonHeader, BaseSpacer, CoinbasePayWebView, Layout, TransakPayWebView } from "~Components"
 import { RootStackParamListBuy, Routes } from "~Navigation"
 import { selectSelectedAccountAddress, useAppSelector } from "~Storage/Redux"
 import { PlatformUtils } from "~Utils"
@@ -35,6 +35,9 @@ export const BuyWebviewScreen: React.FC<Props> = ({ route }) => {
                 <>
                     {provider === PaymentProvidersEnum.CoinbasePay && (
                         <CoinbasePayWebView currentAmount={0} destinationAddress={selectedAccountAddress} />
+                    )}
+                    {provider === PaymentProvidersEnum.Transak && (
+                        <TransakPayWebView currentAmount={0} destinationAddress={selectedAccountAddress} />
                     )}
                 </>
             }
---
+++ src/Test/mocks/react-native-device-info.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getUniqueIdSync: jest.fn(() => "mocked-unique-id"),
+    getVersion: jest.fn(() => "mocked-version"),
+    // Add other mocks as needed
+}
