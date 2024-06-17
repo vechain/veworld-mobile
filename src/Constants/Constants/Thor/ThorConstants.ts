1da4b66218535265eb5714bc503ed0ff39ba2ac9
Piero Bassa <pierobassa222@gmail.com>
2024-06-17 14:41:29 +0200

    feat: solo genesis block & id

--- src/Constants/Constants/Thor/ThorConstants.ts
+++ src/Constants/Constants/Thor/ThorConstants.ts
@@ -47,6 +47,9 @@ export const genesisesId = {
     get test(): string {
         return "0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127"
     },
+    get solo(): string {
+        return "0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6"
+    },
 }
 
 export const genesises = {
@@ -90,12 +93,34 @@ export const genesises = {
             transactions: [],
         }
     },
+    get solo(): Connex.Thor.Block {
+        return {
+            number: 0,
+            id: "0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6",
+            size: 170,
+            parentID: "0xffffffff00000000000000000000000000000000000000000000000000000000",
+            timestamp: 1526400000,
+            gasLimit: 10000000,
+            beneficiary: "0x0000000000000000000000000000000000000000",
+            gasUsed: 0,
+            totalScore: 0,
+            txsRoot: "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
+            txsFeatures: 0,
+            stateRoot: "0x93de0ffb1f33bc0af053abc2a87c4af44594f5dcb1cb879dd823686a15d68550",
+            receiptsRoot: "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
+            signer: "0x0000000000000000000000000000000000000000",
+            isTrunk: true,
+            transactions: [],
+        }
+    },
     which(gid: string) {
         switch (gid) {
             case this.main.id:
                 return NETWORK_TYPE.MAIN
             case this.test.id:
                 return NETWORK_TYPE.TEST
+            case this.solo.id:
+                return NETWORK_TYPE.SOLO
             default:
                 return NETWORK_TYPE.OTHER
         }
