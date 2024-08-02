b75b919dfb70fa360a8d127e2392913b739e4797
Kristian Barrese <kristian.webdeveloper@gmail.com>
2024-08-02 18:42:12 +0200

    fix: correct swap dApps filtering

--- src/Storage/Redux/Selectors/Discovery.ts
+++ src/Storage/Redux/Selectors/Discovery.ts
@@ -75,11 +75,6 @@ export const selectFeaturedImages = createSelector(getDiscoveryState, discovery 
     return images
 })
 
-export const selectSwapFeaturedDapps = createSelector(selectFeaturedDapps, dapps => {
-    return dapps.filter(
-        dapp =>
-            dapp?.name?.toLowerCase()?.includes("swap") ||
-            dapp?.desc?.toLowerCase()?.includes("swap") ||
-            dapp?.tags?.map(t => t.toLowerCase())?.includes("swap"),
-    )
-})
+export const selectSwapFeaturedDapps = createSelector(selectFeaturedDapps, dapps =>
+    dapps.filter(dapp => dapp?.tags?.map(t => t.toLowerCase())?.includes("swap")),
+)
