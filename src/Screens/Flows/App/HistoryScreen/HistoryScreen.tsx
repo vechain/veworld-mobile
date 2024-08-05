34589a00f3c6ee668f02e5956af5500670de42d0
Kristian Barrese <kristian.webdeveloper@gmail.com>
2024-08-05 11:11:04 +0200

    fix: remove useless fragment

--- src/Screens/Flows/App/HistoryScreen/HistoryScreen.tsx
+++ src/Screens/Flows/App/HistoryScreen/HistoryScreen.tsx
@@ -248,17 +248,15 @@ export const HistoryScreen = () => {
         <Layout
             safeAreaTestID="History_Screen"
             fixedHeader={
-                <>
-                    <BaseView flexDirection="row" justifyContent="space-between">
-                        <BaseText typographyFont="title">{LL.BTN_HISTORY()}</BaseText>
+                <BaseView flexDirection="row" justifyContent="space-between">
+                    <BaseText typographyFont="title">{LL.BTN_HISTORY()}</BaseText>
 
-                        <ChangeAccountButtonPill
-                            title={selectedAccount.alias ?? LL.WALLET_LABEL_ACCOUNT()}
-                            text={selectedAccount.address}
-                            action={openSelectAccountBottomSheet}
-                        />
-                    </BaseView>
-                </>
+                    <ChangeAccountButtonPill
+                        title={selectedAccount.alias ?? LL.WALLET_LABEL_ACCOUNT()}
+                        text={selectedAccount.address}
+                        action={openSelectAccountBottomSheet}
+                    />
+                </BaseView>
             }
             fixedBody={
                 <>
