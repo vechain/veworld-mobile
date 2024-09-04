2c0f783a5ddedb5620ee784c62e5ced540abbdce
Michael Martin <michael.martin1997@gmail.com>
2024-09-04 17:52:31 +0200

    feat: implemented mixpanel events, removed backup banner on imported wallet

--- src/Utils/DeviceUtils/DeviceUtils.ts
+++ src/Utils/DeviceUtils/DeviceUtils.ts
@@ -17,6 +17,7 @@ export const generateDeviceForMnemonic = (
     deviceIndex: number,
     alias: string,
     path: DerivationPath = DerivationPath.VET,
+    isImported: boolean,
 ): WalletAndDevice => {
     const hdNode = HDNode.fromMnemonic(mnemonic, path)
 
@@ -35,7 +36,7 @@ export const generateDeviceForMnemonic = (
         index: deviceIndex,
         position: 0, // this will be updated when the device is added to the redux store
         derivationPath: path,
-        isBuckedUp: false,
+        isBuckedUp: isImported,
     }
 
     return { wallet, device }
@@ -67,7 +68,7 @@ export const generateDeviceForPrivateKey = (
         type: DEVICE_TYPE.LOCAL_PRIVATE_KEY,
         index: deviceIndex,
         position: 0, // this will be updated when the device is added to the redux store
-        isBuckedUp: false,
+        isBuckedUp: true,
     }
 
     return { wallet, device }
