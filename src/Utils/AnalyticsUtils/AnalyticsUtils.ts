2cb20b225b887c7e55540e98d3568727b9d148f1
ShaunKav <shaun.kavanagh@vechain.org>
2024-09-18 13:04:37 +0100

    fix: tech debt found by sonar

--- src/Utils/AnalyticsUtils/AnalyticsUtils.ts
+++ src/Utils/AnalyticsUtils/AnalyticsUtils.ts
@@ -1,4 +1,3 @@
-//import { DdRum, RumActionType } from "@datadog/mobile-react-native"
 import { Mixpanel } from "mixpanel-react-native"
 import { ERROR_EVENTS } from "~Constants"
 import { selectAnalyticsTrackingEnabled } from "~Storage/Redux"
@@ -33,14 +32,6 @@ const trackEvent =
             const isAnalyticsEnabled = selectAnalyticsTrackingEnabled(getState())
             if (mixpanel && isInitialized && isAnalyticsEnabled) {
                 mixpanel.track(event, properties)
-                /*
-                if (properties) {
-                    const context = properties.context
-                    DdRum.addAction(RumActionType.TAP, event, { context }, Date.now())
-                } else {
-                    DdRum.addAction(RumActionType.TAP, event, {}, Date.now())
-                }
-                    */
             }
         } catch (e) {
             warn(ERROR_EVENTS.APP, "Error tracking event", e)
