5fa56d9d1b51d1c273ad7b90588897f17c2363ff
mfrezzati <marco.frezzati@vechain.org>
2024-09-24 09:42:23 +0200

    feat: add floating buttons to sign certs

---
+++ src/Components/Reusable/SignAndReject/SignAndReject.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from "react"
+import { StyleSheet } from "react-native"
+import { BaseButton, BaseSpacer, BaseView } from "~Components/Base"
+import { ColorThemeType } from "~Constants"
+import { useThemedStyles } from "~Hooks"
+
+type Props = {
+    onConfirmTitle: string
+    onRejectTitle: string
+    onConfirm: () => void
+    onReject: () => void
+    confirmButtonDisabled?: boolean
+    isConfirmLoading?: boolean
+    rejectButtonDisabled?: boolean
+    isRejectLoading?: boolean
+}
+
+export const SignAndReject: FC<Props> = React.memo(
+    ({
+        onConfirmTitle,
+        onRejectTitle,
+        onConfirm,
+        onReject,
+        confirmButtonDisabled = false,
+        isConfirmLoading = false,
+        rejectButtonDisabled = false,
+        isRejectLoading = false,
+    }: Props) => {
+        const { styles } = useThemedStyles(baseStyles)
+
+        return (
+            <BaseView style={styles.rootContainer}>
+                <BaseButton
+                    w={100}
+                    haptics="Light"
+                    title={onConfirmTitle}
+                    action={onConfirm}
+                    isLoading={isConfirmLoading}
+                    disabled={confirmButtonDisabled}
+                />
+                <BaseSpacer height={16} />
+                <BaseButton
+                    w={100}
+                    haptics="Light"
+                    variant="outline"
+                    title={onRejectTitle}
+                    action={onReject}
+                    isLoading={isRejectLoading}
+                    disabled={rejectButtonDisabled}
+                />
+            </BaseView>
+        )
+    },
+)
+
+const baseStyles = (theme: ColorThemeType) =>
+    StyleSheet.create({
+        rootContainer: {
+            height: 194,
+            width: "100%",
+            padding: 24,
+            alignItems: "center",
+            backgroundColor: theme.colors.card,
+        },
+    })
