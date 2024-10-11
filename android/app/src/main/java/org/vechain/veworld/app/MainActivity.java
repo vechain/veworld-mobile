8283e87d674cde12b6dcd6de6c01ca9a01c430da
Michael Martin <michael.martin1997@gmail.com>
2024-10-11 15:38:44 +0200

    feat: blackscreen when the app goes in background

--- android/app/src/main/java/org/vechain/veworld/app/MainActivity.java
+++ android/app/src/main/java/org/vechain/veworld/app/MainActivity.java
@@ -4,16 +4,27 @@ import expo.modules.ReactActivityDelegateWrapper;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 import com.facebook.react.ReactActivity;
-
+import com.facebook.react.ReactRootView;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
+
+  // private LinearLayout mainLayout;
+  // private ReactRootView reactRootView;
+  // private LinearLayout secureLayout;
+  // private FrameLayout rootLayout;
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -28,10 +39,14 @@ public class MainActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     RNBootSplash.init(this, R.style.BootTheme);
     super.onCreate(null);
+
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
     // ATTENTION: This was auto-generated to handle app links.
     Intent appLinkIntent = getIntent();
     String appLinkAction = appLinkIntent.getAction();
     Uri appLinkData = appLinkIntent.getData();
+
   }
 
   /**
