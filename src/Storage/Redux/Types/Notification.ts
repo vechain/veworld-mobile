773107eae355f8c03581cc5583a7450d80a339f4
mfrezzati <marco.frezzati@vechain.org>
2024-11-07 15:53:37 +0100

    feat: add custom data type for push

--- src/Storage/Redux/Types/Notification.ts
+++ src/Storage/Redux/Types/Notification.ts
@@ -1,4 +1,12 @@
+import { Routes } from "~Navigation"
+
+export type NotificationDeepLink = {
+    route: (typeof Routes)[keyof typeof Routes]
+    params: Record<string, any>
+}
+
 export interface NotificationState {
     permissionEnabled: boolean
     optedIn: boolean
+    deeplink: NotificationDeepLink | null
 }
