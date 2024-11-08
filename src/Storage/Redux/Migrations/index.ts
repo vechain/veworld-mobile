5a7fbb8618799ff94a0f0c532d5d61e3814fa740
Michael Martin <michael.martin1997@gmail.com>
2024-11-08 12:29:45 +0100

    fix: add redux migration fro new locales

--- src/Storage/Redux/Migrations/index.ts
+++ src/Storage/Redux/Migrations/index.ts
@@ -4,6 +4,7 @@ import { Migration4 } from "./Migration4"
 import { Migration5 } from "./Migration5"
 import { MigrationManifest } from "redux-persist/es/types"
 import { Migration6 } from "./Migration6"
+import { Migration7 } from "./Migration7"
 
 export const migrationUpdates: MigrationManifest = {
     2: state => Migration2(state),
@@ -11,4 +12,5 @@ export const migrationUpdates: MigrationManifest = {
     4: state => Migration4(state),
     5: state => Migration5(state),
     6: state => Migration6(state),
+    7: state => Migration7(state),
 }
