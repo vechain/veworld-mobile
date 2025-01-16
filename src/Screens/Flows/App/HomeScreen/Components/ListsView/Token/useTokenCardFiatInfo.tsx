6b217d31d2319213a53629031f3d4f5732f33783
Hugh Livingstone <hugh.livingstone@vechain.org>
2025-01-16 12:01:50 +0000

    fix: token percent change match on homescreen and asset chart

--- src/Screens/Flows/App/HomeScreen/Components/ListsView/Token/useTokenCardFiatInfo.tsx
+++ src/Screens/Flows/App/HomeScreen/Components/ListsView/Token/useTokenCardFiatInfo.tsx
@@ -1,21 +1,29 @@
 import { TokenWithCompleteInfo } from "~Hooks"
 import { useAppSelector, selectIsTokensOwnedLoading } from "~Storage/Redux"
-import { BigNutils } from "~Utils"
+import { ReanimatedUtils } from "~Utils"
 
 export const useTokenCardFiatInfo = (tokenWithInfo: TokenWithCompleteInfo) => {
     const isTokensOwnedLoading = useAppSelector(selectIsTokensOwnedLoading)
 
-    const { tokenInfo, tokenInfoLoading, fiatBalance, exchangeRate } = tokenWithInfo
+    const { chartData, tokenInfo, tokenInfoLoading, fiatBalance, exchangeRate } = tokenWithInfo
+    let priceChange = 0
+    if (chartData?.length) {
+        const openPrice = chartData[0]?.value
+        const closePrice = chartData[chartData.length - 1]?.value
 
-    const isPositive24hChange = (tokenInfo?.market_data?.price_change_percentage_24h ?? 0) >= 0
+        if (openPrice && closePrice) {
+            priceChange = ((closePrice - openPrice) / openPrice) * 100
+        }
+    } else {
+        priceChange = tokenInfo?.market_data?.price_change_percentage_24h ?? 0
+    }
 
-    const change24h =
-        (isPositive24hChange ? "+" : "") +
-        BigNutils(tokenInfo?.market_data?.price_change_percentage_24h ?? 0)
-            .toHuman(0)
-            .decimals(2).toString +
-        "%"
+    const isPositive24hChange = priceChange >= 0
 
+    const change24h = ReanimatedUtils.numberToPercentWorklet(priceChange, {
+        precision: 2,
+        absolute: true,
+    })
     const isLoading = tokenInfoLoading || isTokensOwnedLoading
 
     return {
