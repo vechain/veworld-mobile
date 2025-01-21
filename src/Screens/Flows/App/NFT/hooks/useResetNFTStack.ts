45827f16addc27606f98b254ac2c7322d405708d
mfrezzati <marco.frezzati@vechain.org>
2025-01-21 15:30:44 +0100

    fix: reset nft stack when account is changed

---
+++ src/Screens/Flows/App/NFT/hooks/useResetNFTStack.ts
@@ -0,0 +1,27 @@
+import { CommonActions, useFocusEffect, useNavigation } from "@react-navigation/native"
+import { useCallback, useRef } from "react"
+import { Routes } from "~Navigation"
+import { selectSelectedAccount, useAppSelector } from "~Storage/Redux"
+
+export const useResetNFTStack = () => {
+    const navigation = useNavigation()
+    const selectedAccount = useAppSelector(selectSelectedAccount)
+    const selectedAccoountAddress = useRef(selectedAccount?.address)
+
+    useFocusEffect(
+        useCallback(() => {
+            if (selectedAccount.address !== selectedAccoountAddress.current) {
+                navigation.dispatch(state => {
+                    const index = state.routes.findIndex(r => r.name === Routes.NFTS)
+                    const routes = state.routes.slice(0, index + 1)
+
+                    return CommonActions.reset({
+                        ...state,
+                        routes,
+                        index: routes.length - 1,
+                    })
+                })
+            }
+        }, [navigation, selectedAccount.address]),
+    )
+}
