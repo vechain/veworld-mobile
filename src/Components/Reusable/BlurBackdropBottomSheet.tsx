18ec59da3d33e9514ce0ec5e8c1308db137136b5
Michael Martin <52627078+Doublemme@users.noreply.github.com>
2025-02-11 22:11:48 +0100

    2617 Change account is slow (#2654)
    * feat: added animation to backdrop blur
    
    * fix: fiat balance for account refresh every account change
    
    * feat: add back fiat balance in account card
    
    * fix: performace while calculating available usernames
    
    * fix: claimable usernames not updating while adding account
    
    ---------
    
    Co-authored-by: Michael Martin <michael.martin1997@gmail.com>

---
+++ src/Components/Reusable/BlurBackdropBottomSheet.tsx
@@ -0,0 +1,42 @@
+import React, { useMemo } from "react"
+import { Gesture, GestureDetector } from "react-native-gesture-handler"
+import { BlurView } from "./BlurView"
+import Animated, { Extrapolation, interpolate, runOnJS, SharedValue, useAnimatedStyle } from "react-native-reanimated"
+import { useBottomSheet } from "@gorhom/bottom-sheet"
+import { StyleSheet } from "react-native"
+import { COLORS } from "~Constants"
+
+type Props = {
+    animatedIndex: SharedValue<number>
+}
+
+export const BlurBackdropBottomSheet = ({ animatedIndex }: Props) => {
+    const { close } = useBottomSheet()
+
+    const containerAnimatedStyle = useAnimatedStyle(
+        () => ({
+            opacity: interpolate(animatedIndex.value, [-1, 0, 1], [0, 1, 1], Extrapolation.CLAMP),
+            flex: 1,
+        }),
+        [animatedIndex],
+    )
+
+    const tapHandler = useMemo(() => {
+        const gesture = Gesture.Tap().onEnd(() => {
+            runOnJS(close)()
+        })
+        return gesture
+    }, [close])
+
+    return (
+        <GestureDetector gesture={tapHandler}>
+            <Animated.View style={[containerAnimatedStyle]}>
+                <BlurView
+                    style={[StyleSheet.absoluteFill, { backgroundColor: COLORS.PURPLE_BLUR_TRANSPARENT }]}
+                    blurAmount={4}
+                    blurType={"dark"}
+                />
+            </Animated.View>
+        </GestureDetector>
+    )
+}
