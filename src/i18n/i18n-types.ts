6ba5cc494c0f3438d171baa0c5f50ac6cead92a6
Michael Martin <michael.martin1997@gmail.com>
2025-02-26 12:16:52 +0100

    feat: added translations

--- src/i18n/i18n-types.ts
+++ src/i18n/i18n-types.ts
@@ -668,7 +668,7 @@ type RootTranslation = {
 	 */
 	BD_SELECT_NETWORK_DESC: string
 	/**
-	 * C​h​o​o​s​e​ ​a​ ​p​r​o​v​i​d​e​r​:
+	 * C​h​o​o​s​e​ ​a​ ​p​a​y​m​e​n​t​ ​o​p​t​i​o​n​ ​t​o​ ​b​u​y​:
 	 */
 	BD_SELECT_PAYMENT_PROVIDER: string
 	/**
@@ -2865,6 +2865,10 @@ type RootTranslation = {
 	 */
 	SB_ENTER_LANGUAGE: string
 	/**
+	 * F​e​e​s
+	 */
+	SB_FEES: string
+	/**
 	 * W​h​i​c​h​ ​k​i​n​d​ ​o​f​ ​w​a​l​l​e​t​ ​d​o​ ​y​o​u​ ​w​a​n​t​ ​t​o​ ​i​m​p​o​r​t​?
 	 */
 	SB_IMPORT_WALLET_TYPE: string
@@ -2892,6 +2896,10 @@ type RootTranslation = {
 	 * P​a​s​s​w​o​r​d​ ​a​u​t​h​o​r​i​z​a​t​i​o​n
 	 */
 	SB_PASSWORD_AUTH: string
+	/**
+	 * P​a​y​ ​w​i​t​h
+	 */
+	SB_PAY_WITH: string
 	/**
 	 * P​r​i​v​a​t​e​ ​k​e​y
 	 */
@@ -3495,7 +3503,7 @@ type RootTranslation = {
 	 */
 	TITLE_BACKUP_YOUR_PHRASE: string
 	/**
-	 * B​u​y​ ​V​e​c​h​a​i​n​T​h​o​r​ ​t​o​k​e​n​s
+	 * B​u​y
 	 */
 	TITLE_BUY: string
 	/**
@@ -4586,7 +4594,7 @@ Try reconnect your device to the internet.
 	 */
 	BD_SELECT_NETWORK_DESC: () => LocalizedString
 	/**
-	 * Choose a provider:
+	 * Choose a payment option to buy:
 	 */
 	BD_SELECT_PAYMENT_PROVIDER: () => LocalizedString
 	/**
@@ -6751,6 +6759,10 @@ Try reconnect your device to the internet.
 	 */
 	SB_ENTER_LANGUAGE: () => LocalizedString
 	/**
+	 * Fees
+	 */
+	SB_FEES: () => LocalizedString
+	/**
 	 * Which kind of wallet do you want to import?
 	 */
 	SB_IMPORT_WALLET_TYPE: () => LocalizedString
@@ -6778,6 +6790,10 @@ Try reconnect your device to the internet.
 	 * Password authorization
 	 */
 	SB_PASSWORD_AUTH: () => LocalizedString
+	/**
+	 * Pay with
+	 */
+	SB_PAY_WITH: () => LocalizedString
 	/**
 	 * Private key
 	 */
@@ -7375,7 +7391,7 @@ Try reconnect your device to the internet.
 	 */
 	TITLE_BACKUP_YOUR_PHRASE: () => LocalizedString
 	/**
-	 * Buy VechainThor tokens
+	 * Buy
 	 */
 	TITLE_BUY: () => LocalizedString
 	/**
