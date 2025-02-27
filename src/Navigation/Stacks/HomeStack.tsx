0115f32c686ba99aaac377c27e2f8fdac7ba0af0
Michael Martin <michael.martin1997@gmail.com>
2025-02-27 15:58:44 +0100

    fix: navigation error in custom network screens

--- src/Navigation/Stacks/HomeStack.tsx
+++ src/Navigation/Stacks/HomeStack.tsx
@@ -37,6 +37,8 @@ import {
     ClaimUsername,
     UsernameClaimed,
     ConvertTransactionScreen,
+    AddCustomNodeScreen,
+    ManageCustomNodesScreen,
 } from "~Screens"
 
 export type RootStackParamListHome = {
@@ -108,6 +110,8 @@ export type RootStackParamListHome = {
         ul?: boolean
     }
     [Routes.SETTINGS_NETWORK]: undefined
+    [Routes.SETTINGS_ADD_CUSTOM_NODE]: undefined
+    [Routes.SETTINGS_MANAGE_CUSTOM_NODES]: undefined
     [Routes.CLAIM_USERNAME]: undefined
     [Routes.USERNAME_CLAIMED]: {
         username: string
@@ -203,6 +207,17 @@ export const HomeStack = () => {
                 <Screen
                     name={Routes.SETTINGS_NETWORK}
                     component={ChangeNetworkScreen}
+                    options={{ headerShown: false }}
+                />
+                <Screen
+                    name={Routes.SETTINGS_ADD_CUSTOM_NODE}
+                    component={AddCustomNodeScreen}
+                    options={{ headerShown: false }}
+                />
+
+                <Screen
+                    name={Routes.SETTINGS_MANAGE_CUSTOM_NODES}
+                    component={ManageCustomNodesScreen}
                     options={{ headerShown: false }}
                 />
             </Group>
