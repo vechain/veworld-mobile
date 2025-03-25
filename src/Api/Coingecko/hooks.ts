5613500564b2aba350bd1ad7e516454ab512ea2c
Michael Martin <52627078+Doublemme@users.noreply.github.com>
2025-03-25 10:34:43 +0100

    fix: useExchangeRate hook availability (#2806)
    Co-authored-by: Michael Martin <michael.martin1997@gmail.com>

--- src/Api/Coingecko/hooks.ts
+++ src/Api/Coingecko/hooks.ts
@@ -185,10 +185,12 @@ export const useExchangeRate = ({ id, vs_currency }: { id?: string; vs_currency:
     const { data: b3trInfo } = useVechainStatsTokenInfo("b3tr")
     const currency = vs_currency.toLowerCase()
 
+    const isEnabled = id === "vebetterdao" ? !!b3trInfo : !!tokenInfo
+
     return useQuery({
         queryKey: getExchangeRateQueryKey({ id, vs_currency }),
         queryFn: () => (id === "vebetterdao" ? Number(b3trInfo) : tokenInfo?.market_data.current_price[currency]),
-        enabled: Boolean(!!tokenInfo && !!b3trInfo),
+        enabled: isEnabled,
         staleTime: getQueryCacheTime(),
         refetchInterval: getRefetchIntevalTime(),
     })
