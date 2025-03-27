7aa7f4b0634a0672d5c562dacc9e2d96c698a2dd
Michael Martin <52627078+Doublemme@users.noreply.github.com>
2025-03-27 17:30:34 +0100

    fix: reverted all the changes that hides the 24h change for better tokens (#2814)
    Co-authored-by: Michael Martin <michael.martin1997@gmail.com>

--- src/Components/Reusable/Tokens/OfficialTokenCard.tsx
+++ src/Components/Reusable/Tokens/OfficialTokenCard.tsx
@@ -2,7 +2,7 @@ import { Dimensions, StyleSheet, ViewProps } from "react-native"
 import React, { memo, useMemo } from "react"
 import { BaseCard, BaseText, BaseView, FiatBalance } from "~Components"
 import { useTheme, useThemedStyles, TokenWithCompleteInfo, useBalances } from "~Hooks"
-import { B3TR, ColorThemeType, VOT3 } from "~Constants"
+import { ColorThemeType } from "~Constants"
 import { TokenImage } from "../TokenImage"
 import { useI18nContext } from "~i18n"
 import { selectBalanceVisible, useAppSelector } from "~Storage/Redux"
@@ -51,10 +51,6 @@ export const OfficialTokenCard = memo(
         )
         const symbol = useMemo(() => tokenWithInfo.symbol ?? token?.symbol, [tokenWithInfo.symbol, token?.symbol])
 
-        const isBetterToken = useMemo(() => {
-            return token.symbol === B3TR.symbol || token.symbol === VOT3.symbol
-        }, [token.symbol])
-
         return (
             <BaseCard onPress={action} containerStyle={[styles.container, style]} testID={symbol}>
                 <BaseView flexDirection="row" justifyContent="space-between">
@@ -66,7 +62,7 @@ export const OfficialTokenCard = memo(
                             {token.name}
                         </BaseText>
 
-                        {tokenWithInfo.fiatBalance && !isBetterToken && (
+                        {tokenWithInfo.fiatBalance && (
                             <BaseView flexDirection="row">
                                 <BaseText
                                     typographyFont="captionBold"
