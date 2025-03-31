4dcd0cbfa239f2510c03a75105c6b98067ae4adb
Michael Martin <michael.martin1997@gmail.com>
2025-03-31 18:32:21 +0200

    feat: enable navigation to DApp browser on card press in NewDapps component

--- src/Screens/Flows/App/DiscoverScreen/Components/NewDapps.tsx
+++ src/Screens/Flows/App/DiscoverScreen/Components/NewDapps.tsx
@@ -5,23 +5,30 @@ import { useNewDApps } from "~Hooks"
 import { DAppCard } from "./DAppCard"
 import { FlatList, ListRenderItemInfo } from "react-native"
 import { DiscoveryDApp } from "~Constants"
+import { Routes } from "~Navigation"
+import { useNavigation } from "@react-navigation/native"
 
 export const NewDapps = () => {
     const { LL } = useI18nContext()
     const newDapps = useNewDApps()
+    const nav = useNavigation()
 
     const renderItem = useCallback(
         ({ item, index }: ListRenderItemInfo<DiscoveryDApp>) => {
             const isLast = index === newDapps.length - 1
             const columnsGap = 16
 
+            const onPress = () => {
+                nav.navigate(Routes.BROWSER, { url: item.href })
+            }
+
             return (
                 <BaseView pl={columnsGap} pr={isLast ? columnsGap : 0} justifyContent="center" alignItems="center">
-                    <DAppCard dapp={item} onPress={() => {}} />
+                    <DAppCard dapp={item} onPress={onPress} />
                 </BaseView>
             )
         },
-        [newDapps.length],
+        [nav, newDapps.length],
     )
 
     return (
