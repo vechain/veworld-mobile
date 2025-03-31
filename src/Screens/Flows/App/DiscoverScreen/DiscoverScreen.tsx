981291b6ac2d044c914609458ad87ca2c75e5756
HiiiiD <salomone.marco@outlook.it>
2025-03-31 12:25:10 +0200

    fix: cleanup DiscoverScreen

--- src/Screens/Flows/App/DiscoverScreen/DiscoverScreen.tsx
+++ src/Screens/Flows/App/DiscoverScreen/DiscoverScreen.tsx
@@ -1,28 +1,20 @@
 import { useNavigation, useScrollToTop } from "@react-navigation/native"
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
-import { Keyboard, StyleSheet } from "react-native"
+import React, { useCallback, useEffect, useMemo, useRef } from "react"
+import { StyleSheet } from "react-native"
 import Animated, { useAnimatedRef } from "react-native-reanimated"
-import {
-    AnimatedFloatingButton,
-    // AnimatedSearchBar,
-    BaseSpacer,
-    BaseView,
-    Layout,
-} from "~Components"
+import { BaseSpacer, BaseView, Layout } from "~Components"
 import { AnalyticsEvent } from "~Constants"
-import { useAnalyticTracking, useBrowserSearch, useFetchFeaturedDApps, useThemedStyles, useVisitedUrls } from "~Hooks"
+import { useAnalyticTracking, useFetchFeaturedDApps, useThemedStyles, useVisitedUrls } from "~Hooks"
 import { Routes } from "~Navigation"
 import {
     addNavigationToDApp,
     selectBookmarkedDapps,
     selectFeaturedDapps,
     selectHasUserOpenedDiscovery,
-    // selectVisitedUrls,
     setDiscoverySectionOpened,
     useAppDispatch,
     useAppSelector,
 } from "~Storage/Redux"
-import { URIUtils } from "~Utils"
 import { useI18nContext } from "~i18n"
 import { Ecosystem, Favourites, Header, NewDapps } from "./Components"
 import { VeBetterDAOCarousel } from "./Components/VeBetterDAOCarousel"
@@ -38,7 +30,6 @@ export const DiscoverScreen: React.FC = () => {
     const { addVisitedUrl } = useVisitedUrls()
 
     useFetchFeaturedDApps()
-    const { navigateToBrowser } = useBrowserSearch()
 
     const animatedRef = useAnimatedRef<Animated.ScrollView>()
     // const offset = useScrollViewOffset(animatedRef)
@@ -51,7 +42,6 @@ export const DiscoverScreen: React.FC = () => {
     const bookmarkedDApps = useAppSelector(selectBookmarkedDapps)
     const dapps = useAppSelector(selectFeaturedDapps)
 
-    const [filteredSearch, setFilteredSearch] = useState("")
     const groupedbookmarkedDApps = useMemo(() => groupFavoritesByBaseUrl(bookmarkedDApps), [bookmarkedDApps])
     const showFavorites = groupedbookmarkedDApps.length > 0
 
@@ -78,38 +68,6 @@ export const DiscoverScreen: React.FC = () => {
         },
         [nav, addVisitedUrl, track, dispatch],
     )
-
-    // const onTextChange = useCallback((_text: string) => {
-    //     setFilteredSearch(_text)
-    // }, [])
-
-    const isWebSearchFloatingButtonVisible = useMemo(() => {
-        return !!filteredSearch.length
-    }, [filteredSearch.length])
-
-    const onSearch = useCallback(() => {
-        if (!filteredSearch.length) return
-
-        Keyboard.dismiss()
-
-        const dapp = dapps.find(item => {
-            return URIUtils.getHostName(item.href)?.toLowerCase() === filteredSearch.toLowerCase()
-        })
-
-        navigateToBrowser(dapp?.href ?? filteredSearch)
-        setFilteredSearch("")
-    }, [dapps, filteredSearch, navigateToBrowser])
-
-    // const onMakeYourOwnDAppPress = useCallback(async () => {
-    //     const url = process.env.REACT_APP_CREATE_YOUR_VECHAIN_DAPP_URL
-    //     if (url && (await Linking.canOpenURL(url))) {
-    //         Linking.openURL(url)
-    //     }
-    // }, [])
-
-    // const onNavigateToBrowserHistory = useCallback(() => {
-    //     nav.navigate(Routes.DISCOVER_BROWSER_HISTORY)
-    // }, [nav])
 
     const onSeeAllPress = useCallback(() => nav.navigate(Routes.DISCOVER_FAVOURITES), [nav])
 
@@ -174,13 +132,7 @@ export const DiscoverScreen: React.FC = () => {
                         {/* Trending & Popular */}
                         <BaseSpacer height={12} />
                         <Ecosystem title={LL.DISCOVER_ECOSYSTEM()} dapps={dapps} onDAppPress={onDAppPress} />
-                        {isWebSearchFloatingButtonVisible && <BaseSpacer height={70} />}
                     </Animated.ScrollView>
-                    <AnimatedFloatingButton
-                        title={LL.DISCOVER_WEB_SEARCH_FLOATING_BUTTON_LABEL()}
-                        isVisible={isWebSearchFloatingButtonVisible}
-                        onPress={onSearch}
-                    />
                 </BaseView>
             }
         />
