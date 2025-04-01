5904d0142af5d41b68094e5304ed1927bd5cfcf9
HiiiiD <salomone.marco@outlook.it>
2025-04-01 09:03:47 +0200

    fix: fix SC issue

--- src/Utils/URIUtils/URIUtils.ts
+++ src/Utils/URIUtils/URIUtils.ts
@@ -113,7 +113,7 @@ const convertUriToUrl = (uri: string) => {
 async function isValidBrowserUrl(url: string): Promise<boolean> {
     let navInput: string | undefined
     const regexWww = new RegExp("^www\\.[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,}$")
-    const regexWithoutWww = new RegExp("^[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,}$")
+    const regexWithoutWww = /^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/
 
     try {
         if (isHttps(url)) {
