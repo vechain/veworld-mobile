2c7e5d0e4fdd525981487eae5321198cae335f4b
Michael Martin <michael.martin1997@gmail.com>
2025-04-07 09:24:42 +0200

    refactor: update VeBetterDAO banner imports and enhance carousel functionality

--- src/Screens/Flows/App/DiscoverScreen/Components/VeBetterDAOCarousel.tsx
+++ src/Screens/Flows/App/DiscoverScreen/Components/VeBetterDAOCarousel.tsx
@@ -3,10 +3,8 @@ import {
     VeBetterDaoLightBanner,
     VeBetterDaoDarkBanner,
     VeBetterDaoStellaBanner,
-    VeBetterDaoVoteDarkBanner,
-    VeBetterDaoVoteLightBanner,
-    VeBetterDaoMakeDappDarkBanner,
-    VeBetterDaoMakeDappLightBanner,
+    VeBetterDaoVoteBanner,
+    VeBetterDaoMakeDappBanner,
 } from "~Assets/Banners"
 import { BaseCarousel, CarouselSlideItem, useFeatureFlags } from "~Components"
 import { useCurrentAllocationsRoundId, useTheme } from "~Hooks"
@@ -31,12 +29,12 @@ export const VeBetterDAOCarousel = () => {
             },
             {
                 testID: "VeBetterDao_vote_banner",
-                source: theme.isDark ? VeBetterDaoVoteDarkBanner : VeBetterDaoVoteLightBanner,
+                source: VeBetterDaoVoteBanner,
                 href: `${DAO_VOTE_URL}${data ?? ""}`,
             },
             {
                 testID: "VeBetterDao_make_app_banner",
-                source: theme.isDark ? VeBetterDaoMakeDappDarkBanner : VeBetterDaoMakeDappLightBanner,
+                source: VeBetterDaoMakeDappBanner,
                 href: DAO_MAKE_APP_URL,
                 isExternalLink: true,
             },
