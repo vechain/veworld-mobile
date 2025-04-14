ab7b3a2d62aab451d78638b643a038c15127d651
Michael Martin <michael.martin1997@gmail.com>
2025-04-14 12:55:31 +0200

    refactor: remove App component and associated test file for code cleanup

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
-// module.exports = {
-//     presets: ["module:@react-native/babel-preset"],
-//     plugins: ["react-native-reanimated/plugin"],
-// }
-
 module.exports = {
     presets: ["module:@react-native/babel-preset"],
 
