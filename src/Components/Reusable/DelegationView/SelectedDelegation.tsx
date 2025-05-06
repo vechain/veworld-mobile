5917cf344c66bfb0de23aebcaedaefdfa5831aff
HiiiiD <salomone.marco@outlook.it>
2025-05-06 11:03:04 +0200

    feat: update selected delegation ui

--- src/Components/Reusable/DelegationView/SelectedDelegation.tsx
+++ src/Components/Reusable/DelegationView/SelectedDelegation.tsx
@@ -1,11 +1,12 @@
-import React from "react"
-import { StyleSheet } from "react-native"
-import { BaseButton, BaseCard, BaseSpacer, BaseText, BaseView } from "~Components/Base"
-import { ColorThemeType } from "~Constants"
-import { useThemedStyles } from "~Hooks"
+/* eslint-disable react-native/no-inline-styles */
+import React, { useMemo } from "react"
+import { StyleSheet, Text } from "react-native"
+import { BaseButton, BaseText, BaseView } from "~Components/Base"
+import { ColorThemeType, typography } from "~Constants"
+import { useThemedStyles, useVns } from "~Hooks"
 import { useI18nContext } from "~i18n"
 import { LocalAccountWithDevice } from "~Model"
-import { DelegateAccountCard } from "../DelegateAccountCard"
+import { AddressUtils } from "~Utils"
 
 type Props = {
     selectedDelegationAccount?: LocalAccountWithDevice
@@ -17,23 +18,79 @@ export const SelectedDelegation = ({ selectedDelegationAccount, selectedDelegati
     const { theme, styles } = useThemedStyles(baseStyles)
     const { LL } = useI18nContext()
 
-    if (selectedDelegationAccount)
-        return (
-            <>
-                <BaseSpacer height={16} />
-                <DelegateAccountCard account={selectedDelegationAccount} selected />
-            </>
-        )
+    const { name } = useVns({
+        address: selectedDelegationAccount?.address,
+        name: "",
+    })
 
-    if (selectedDelegationUrl)
+    const delegationUrlParsed = useMemo(() => {
+        if (!selectedDelegationUrl) return
+        return new URL(selectedDelegationUrl)
+    }, [selectedDelegationUrl])
+
+    const renderOption = useMemo(() => {
+        if (selectedDelegationAccount)
+            return (
+                <BaseView flexDirection="column" gap={4}>
+                    <BaseText typographyFont="captionMedium" color={theme.colors.textLight}>
+                        {LL.DELEGATION_FEE()}
+                    </BaseText>
+                    <BaseText typographyFont="bodySemiBold" color={theme.colors.subtitle} numberOfLines={1}>
+                        {name || AddressUtils.humanAddress(selectedDelegationAccount.address)}
+                    </BaseText>
+                </BaseView>
+            )
+
+        if (selectedDelegationUrl)
+            return (
+                <BaseView flexDirection="column" gap={4} flex={1}>
+                    <BaseText typographyFont="captionMedium" color={theme.colors.textLight}>
+                        {LL.DELEGATION_FEE()}
+                    </BaseText>
+                    <BaseView flexWrap="nowrap" flexDirection="row" alignItems="center">
+                        <Text
+                            style={{
+                                color: theme.colors.passwordPlaceholder,
+                                fontSize: typography.defaults.captionMedium.fontSize,
+                                fontFamily: typography.defaults.captionMedium.fontFamily || "Inter-Regular",
+                                fontWeight: typography.defaults.captionMedium
+                                    .fontWeight as keyof typeof typography.fontWeight,
+                                lineHeight: typography.defaults.captionMedium.lineHeight,
+                            }}>
+                            {delegationUrlParsed?.protocol}
+                            {"//"}
+                        </Text>
+                        <Text
+                            numberOfLines={1}
+                            style={{
+                                color: theme.colors.subtitle,
+                                fontSize: typography.defaults.bodySemiBold.fontSize,
+                                fontFamily: typography.defaults.bodySemiBold.fontFamily || "Inter-Regular",
+                                fontWeight: typography.defaults.bodySemiBold
+                                    .fontWeight as keyof typeof typography.fontWeight,
+                                lineHeight: typography.defaults.bodySemiBold.lineHeight,
+                                flexShrink: 1,
+                            }}>{`${delegationUrlParsed?.hostname}${delegationUrlParsed?.pathname}`}</Text>
+                    </BaseView>
+                </BaseView>
+            )
         return (
-            <>
-                <BaseSpacer height={16} />
-                <BaseCard>
-                    <BaseText py={8}>{selectedDelegationUrl}</BaseText>
-                </BaseCard>
-            </>
+            <BaseText typographyFont="bodyMedium" color={theme.colors.textLight}>
+                {LL.DELEGATION_NO_FEE()}
+            </BaseText>
         )
+    }, [
+        LL,
+        delegationUrlParsed?.hostname,
+        delegationUrlParsed?.pathname,
+        delegationUrlParsed?.protocol,
+        name,
+        selectedDelegationAccount,
+        selectedDelegationUrl,
+        theme.colors.passwordPlaceholder,
+        theme.colors.subtitle,
+        theme.colors.textLight,
+    ])
 
     return (
         <BaseView w={100} px={16} pb={16}>
@@ -45,12 +102,10 @@ export const SelectedDelegation = ({ selectedDelegationAccount, selectedDelegati
                 pb={8}
                 pr={8}
                 pl={12}
-                w={100}
                 justifyContent="space-between"
-                borderRadius={8}>
-                <BaseText typographyFont="bodyMedium" color={theme.colors.textLight}>
-                    {LL.DELEGATION_NO_FEE()}
-                </BaseText>
+                borderRadius={8}
+                gap={12}>
+                {renderOption}
                 <BaseButton
                     variant="solid"
                     bgColor={theme.colors.cardButton.background}
