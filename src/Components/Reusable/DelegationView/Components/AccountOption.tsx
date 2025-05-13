50ddc928d77a14aaeb5c2d2da66e22c311021711
HiiiiD <salomone.marco@outlook.it>
2025-05-13 09:10:23 +0200

    fix: fix tests

--- src/Components/Reusable/DelegationView/Components/AccountOption.tsx
+++ src/Components/Reusable/DelegationView/Components/AccountOption.tsx
@@ -41,7 +41,7 @@ export const AccountOption = ({ selectedDelegationAccount, flatListProps, childr
                     renderItem={({ item }) => {
                         return (
                             <DelegateAccountCardRadio
-                                testID={`DELEGATE_ACCOUNT_CARD_RADIO_${item.address}`}
+                                testID={"DELEGATE_ACCOUNT_CARD_RADIO"}
                                 account={item}
                                 onPress={handlePress}
                                 selected={item.address === selectedAccount?.address}
