d12f46f37b771a63f4746499a270b4f4d378316b
Michael Martin <michael.martin1997@gmail.com>
2025-05-14 19:10:07 +0200

    chore: upgrade react-native-safe-area-context to 5.4.0 and refactor modal to utilize SafeAreaView

--- src/Components/Base/BaseModal.tsx
+++ src/Components/Base/BaseModal.tsx
@@ -1,33 +1,39 @@
 import React from "react"
-import { Modal, ModalProps } from "react-native"
-import { BaseSafeArea } from "./BaseSafeArea/BaseSafeArea"
+import { Modal, ModalProps, StyleSheet } from "react-native"
 import { BaseView } from "./BaseView"
-import { SafeAreaProvider } from "react-native-safe-area-context"
-import { useTheme } from "~Hooks"
+import { useThemedStyles } from "~Hooks"
+import { SafeAreaView } from "react-native-safe-area-context"
+
 export interface IBaseModal extends ModalProps {
     isOpen: boolean
     onClose: () => void
     children: React.ReactNode
 }
 export const BaseModal: React.FC<IBaseModal> = ({ isOpen, onClose, children, transparent = false, ...otherProps }) => {
-    const theme = useTheme()
+    const { styles, theme } = useThemedStyles(baseStyles(transparent))
     return (
-        <SafeAreaProvider>
-            <BaseSafeArea bg={transparent ? "transparent" : undefined}>
-                <Modal
-                    visible={isOpen}
-                    animationType="slide"
-                    transparent={transparent}
-                    hardwareAccelerated
-                    presentationStyle="fullScreen"
-                    onDismiss={onClose}
-                    onRequestClose={onClose}
-                    {...otherProps}>
-                    <BaseView alignItems="center" justifyContent="flex-start" flex={1} bg={theme.colors.background}>
-                        {children}
-                    </BaseView>
-                </Modal>
-            </BaseSafeArea>
-        </SafeAreaProvider>
+        <Modal
+            visible={isOpen}
+            animationType="slide"
+            hardwareAccelerated
+            presentationStyle="fullScreen"
+            onDismiss={onClose}
+            transparent={transparent}
+            onRequestClose={onClose}
+            {...otherProps}>
+            <SafeAreaView style={styles.container}>
+                <BaseView alignItems="center" justifyContent="flex-start" flex={1} bg={theme.colors.background}>
+                    {children}
+                </BaseView>
+            </SafeAreaView>
+        </Modal>
     )
 }
+
+const baseStyles = (transparent: boolean) => () =>
+    StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: transparent ? "transparent" : undefined,
+        },
+    })
