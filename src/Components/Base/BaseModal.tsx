2d908833893c7100c4a7437c1373feba35419202
Michael Martin <michael.martin1997@gmail.com>
2025-05-14 15:28:31 +0200

    refactor: fixed modal to work with RN 0.76

--- src/Components/Base/BaseModal.tsx
+++ src/Components/Base/BaseModal.tsx
@@ -3,30 +3,31 @@ import { Modal, ModalProps } from "react-native"
 import { BaseSafeArea } from "./BaseSafeArea/BaseSafeArea"
 import { BaseView } from "./BaseView"
 import { SafeAreaProvider } from "react-native-safe-area-context"
-
+import { useTheme } from "~Hooks"
 export interface IBaseModal extends ModalProps {
     isOpen: boolean
     onClose: () => void
     children: React.ReactNode
 }
 export const BaseModal: React.FC<IBaseModal> = ({ isOpen, onClose, children, transparent = false, ...otherProps }) => {
+    const theme = useTheme()
     return (
-        <Modal
-            visible={isOpen}
-            animationType="slide"
-            transparent={transparent}
-            hardwareAccelerated
-            presentationStyle="fullScreen"
-            onDismiss={onClose}
-            onRequestClose={onClose}
-            {...otherProps}>
-            <SafeAreaProvider>
-                <BaseSafeArea bg={transparent ? "transparent" : undefined} grow={1}>
-                    <BaseView alignItems="center" justifyContent="flex-start" flexGrow={1}>
+        <SafeAreaProvider>
+            <BaseSafeArea bg={transparent ? "transparent" : undefined} grow={1}>
+                <Modal
+                    visible={isOpen}
+                    animationType="slide"
+                    transparent={transparent}
+                    hardwareAccelerated
+                    presentationStyle="fullScreen"
+                    onDismiss={onClose}
+                    onRequestClose={onClose}
+                    {...otherProps}>
+                    <BaseView alignItems="center" justifyContent="flex-start" bg={theme.colors.background}>
                         {children}
                     </BaseView>
-                </BaseSafeArea>
-            </SafeAreaProvider>
-        </Modal>
+                </Modal>
+            </BaseSafeArea>
+        </SafeAreaProvider>
     )
 }
