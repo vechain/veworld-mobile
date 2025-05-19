1bc2f3fb3e44ebb3a1342b2462621ba00d9bfb05
Michael Martin <52627078+Doublemme@users.noreply.github.com>
2025-05-19 15:09:02 +0200

    Fix :: Discover banner font size and functionality on android (#2947)
    * refactor: update analytics tracking and enhance banner components with Markdown
    
    * feat: add translations
    
    * refactor: update BaseCarousel
    
    * refactor: replace TouchableOpacity with Pressable in BaseCarouselItem
    
    * refactor: replace Pressable with TouchableOpacity in BaseCarouselItem for improved touch handling
    
    ---------
    
    Co-authored-by: Michael Martin <michael.martin1997@gmail.com>
    Co-authored-by: github-actions-language-translator <github-actions-language-translator@github.com>

--- src/Screens/Flows/App/DiscoverScreen/Components/Banners/VeBetterDaoBanner.tsx
+++ src/Screens/Flows/App/DiscoverScreen/Components/Banners/VeBetterDaoBanner.tsx
@@ -1,10 +1,11 @@
 import React from "react"
-import { StyleSheet, Image, ImageStyle, Text } from "react-native"
+import { StyleSheet, Image, ImageStyle } from "react-native"
 import { VeBetterDaoBannerB3MO, VeBetterLogoGreenSVG, VeBetterSVG } from "~Assets"
-import { BaseView, BaseText, WrapTranslation } from "~Components"
+import { BaseView } from "~Components"
 import { useThemedStyles } from "~Hooks"
 import { COLORS } from "~Constants"
 import { useI18nContext } from "~i18n"
+import Markdown from "react-native-markdown-display"
 
 export const VeBetterDaoBanner = () => {
     const { styles } = useThemedStyles(baseStyles)
@@ -12,22 +13,14 @@ export const VeBetterDaoBanner = () => {
 
     return (
         <BaseView flexDirection="row" borderRadius={12} px={16} py={12} bg={"#002234"} style={styles.container}>
-            <BaseView alignItems={"flex-start"} gap={16} flex={0.7} flexWrap="wrap">
+            <BaseView alignItems={"flex-start"} gap={16} flex={0.8}>
                 <BaseView flexDirection="row" alignItems="center" justifyContent="center" gap={4}>
                     <VeBetterLogoGreenSVG />
                     <VeBetterSVG color={COLORS.WHITE} width={72} height={14} />
                 </BaseView>
-                <BaseText color={"#EEF3F7"} numberOfLines={2} fontSize={14} fontFamily="Rubik-Regular">
-                    <WrapTranslation
-                        message={LL.BANNER_VEBETTER_DESC()}
-                        renderComponent={text => (
-                            <Text numberOfLines={2} style={styles.text}>
-                                <Text style={styles.textBold}>{"VeBetterDAO"}</Text>
-                                <Text style={styles.text}>{text}</Text>
-                            </Text>
-                        )}
-                    />
-                </BaseText>
+                <Markdown style={{ paragraph: styles.paragraph, body: styles.text, strong: { color: "#B1F16C" } }}>
+                    {LL.BANNER_VEBETTER_DESC()}
+                </Markdown>
             </BaseView>
             <Image source={VeBetterDaoBannerB3MO} style={styles.image as ImageStyle} />
         </BaseView>
@@ -47,14 +40,13 @@ const baseStyles = () =>
             right: 0,
             bottom: 0,
         },
-        text: {
-            fontFamily: "Rubik-Regular",
-            fontSize: 14,
-            color: "#EEF3F7",
+        paragraph: {
+            marginTop: 0,
+            marginBottom: 0,
         },
-        textBold: {
-            fontFamily: "Rubik-Bold",
-            fontSize: 14,
-            color: "#B1F16C",
+        text: {
+            fontFamily: "Rubik",
+            fontSize: 15,
+            color: "#EEF3F7",
         },
     })
