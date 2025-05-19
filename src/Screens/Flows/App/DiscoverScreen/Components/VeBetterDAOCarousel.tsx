cdb9e827c31e12d3e575c4d459411fc2e4042a95
Michael Martin <michael.martin1997@gmail.com>
2025-05-19 09:50:59 +0200

    refactor: update analytics tracking and enhance banner components with Markdown

--- src/Screens/Flows/App/DiscoverScreen/Components/VeBetterDAOCarousel.tsx
+++ src/Screens/Flows/App/DiscoverScreen/Components/VeBetterDAOCarousel.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useMemo } from "react"
 import { BaseCarousel, CarouselSlideItem, useFeatureFlags } from "~Components"
 import { VeBetterDaoBanner, StellaPayBanner } from "./Banners"
-import { mixpanel } from "~Utils/AnalyticsUtils"
 import { AnalyticsEvent } from "~Constants/Enums/AnalyticsEvent"
+import { useAnalyticTracking } from "~Hooks"
 
 const DAO_URL = "https://governance.vebetterdao.org"
 const STELLA_URL = "https://vebetter.stellapay.io/"
@@ -24,6 +24,7 @@ const slides: CarouselSlideItem[] = [
 
 export const VeBetterDAOCarousel = () => {
     const featureFlags = useFeatureFlags()
+    const track = useAnalyticTracking()
 
     const activeSlides = useMemo(() => {
         return slides.filter(slide => {
@@ -34,13 +35,16 @@ export const VeBetterDAOCarousel = () => {
         })
     }, [featureFlags.discoveryFeature.showStellaPayBanner])
 
-    const onSlidePress = useCallback((name: string) => {
-        if (name === "stella") {
-            mixpanel.track(AnalyticsEvent.DISCOVERY_STELLAPAY_BANNER_CLICKED)
-        } else {
-            mixpanel.track(AnalyticsEvent.DISCOVERY_VEBETTERDAO_BANNER_CLICKED)
-        }
-    }, [])
+    const onSlidePress = useCallback(
+        (name: string) => {
+            if (name === "stella") {
+                track(AnalyticsEvent.DISCOVERY_STELLAPAY_BANNER_CLICKED)
+            } else {
+                track(AnalyticsEvent.DISCOVERY_VEBETTERDAO_BANNER_CLICKED)
+            }
+        },
+        [track],
+    )
 
     return (
         <BaseCarousel
