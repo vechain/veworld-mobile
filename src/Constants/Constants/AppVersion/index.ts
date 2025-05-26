0becfb289bd968f90d81589202569ebd5923bde3
paolampilla <43100344+paolampilla@users.noreply.github.com>
2025-05-26 11:04:09 +0200

    fix: remove commented url

--- src/Constants/Constants/AppVersion/index.ts
+++ src/Constants/Constants/AppVersion/index.ts
@@ -1,4 +1,2 @@
 export const GOOGLE_STORE_URL = "https://play.google.com/store/apps/details?id=org.vechain.veworld.app"
 export const APPLE_STORE_URL = (countryCode: string) => `https://apps.apple.com/${countryCode}/app/veworld/id6446854569`
-
-// export const LATEST_APP_VERSION_S3_URL = (platform: "ios" | "android") => "update with s3 endpoint"
