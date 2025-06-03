24d071ffb459d144dfea87ded7260925806f61d9
paolampilla <43100344+paolampilla@users.noreply.github.com>
2025-06-03 15:28:18 +0200

    chore: add blacklist testnet

--- src/Hooks/useReportNFT/useReportNFT.ts
+++ src/Hooks/useReportNFT/useReportNFT.ts
@@ -3,26 +3,33 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 import { useCallback } from "react"
 import { abi, Transaction } from "thor-devkit"
 import { abis, AnalyticsEvent } from "~Constants"
-import { NFT_BLACKLIST_CONTRACT } from "~Constants/Constants/NFT"
+import { NFT_BLACKLIST_CONTRACT, NFT_BLACKLIST_CONTRACT_TESTNET } from "~Constants/Constants/NFT"
 import { useAnalyticTracking } from "~Hooks/useAnalyticTracking"
+import { useBlockchainNetwork } from "~Hooks/useBlockchainNetwork"
 import { RootStackParamListNFT, Routes } from "~Navigation"
 
 export const useReportNFT = () => {
     const nav = useNavigation<NativeStackNavigationProp<RootStackParamListNFT>>()
     const track = useAnalyticTracking()
+    const { isMainnet } = useBlockchainNetwork()
 
-    const buildReportTxClause = useCallback((nftAddress: string): Transaction.Clause[] => {
-        const reportAbi = abis.NFTBlacklist.NFTBlacklistAbis.report
-        if (!reportAbi) throw new Error("Function abi not found for reporting NFT collection")
-        const reportData = new abi.Function(reportAbi).encode(nftAddress)
-        return [
-            {
-                to: NFT_BLACKLIST_CONTRACT,
-                data: reportData,
-                value: "0x0",
-            },
-        ]
-    }, [])
+    const blacklistContract = isMainnet ? NFT_BLACKLIST_CONTRACT : NFT_BLACKLIST_CONTRACT_TESTNET
+
+    const buildReportTxClause = useCallback(
+        (nftAddress: string): Transaction.Clause[] => {
+            const reportAbi = abis.NFTBlacklist.NFTBlacklistAbis.report
+            if (!reportAbi) throw new Error("Function abi not found for reporting NFT collection")
+            const reportData = new abi.Function(reportAbi).encode(nftAddress)
+            return [
+                {
+                    to: blacklistContract,
+                    data: reportData,
+                    value: "0x0",
+                },
+            ]
+        },
+        [blacklistContract],
+    )
 
     const reportNFTCollection = useCallback(
         (nftAddress: string) => {
