3f3e3e1049c1e18a5f08762ca6169634c1d00fe8
paolampilla <43100344+paolampilla@users.noreply.github.com>
2025-06-05 09:30:33 +0200

    fix: bump migration version in helpers

--- src/Storage/Redux/Helpers.ts
+++ src/Storage/Redux/Helpers.ts
@@ -68,7 +68,7 @@ export const getPersistorConfig = async (mmkv: MMKV, encryptionKey: string): Pro
     return {
         key: "root",
         storage,
-        version: 17,
+        version: 18,
         blacklist: [NftSlice.name, PendingSlice.name],
         whitelist: [
             CurrencySlice.name,
