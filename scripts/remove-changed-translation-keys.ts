191e4121eda17d7594bb96b97032a80e301c8128
Michael Martin <michael.martin1997@gmail.com>
2025-07-09 18:25:15 +0200

    refactor(i18n): update translation key removal script and command for generating translation diffs

--- scripts/remove-changed-translation-keys.ts
+++ scripts/remove-changed-translation-keys.ts
@@ -1,21 +1,40 @@
-import fs from "fs"
+const { execSync } = require("child_process")
+const fs = require("fs")
 
-function removeChangedTranslationKeys() {
-    const args = process.argv.slice(2)
-    const changedKeys: string[] = JSON.parse(args[0])
-
+function removeChangedTranslationKeys(changedKeys: string[]) {
     const translationFiles = fs.readdirSync("src/i18n/translations")
-    translationFiles.forEach(file => {
+    translationFiles.forEach((file: string) => {
         if (file === "en.json") {
             return
         }
 
         const translation = JSON.parse(fs.readFileSync(`src/i18n/translations/${file}`, "utf8"))
-        changedKeys.forEach(key => {
+        changedKeys.forEach((key: string) => {
             delete translation[key]
         })
         fs.writeFileSync(`src/i18n/translations/${file}`, JSON.stringify(translation, null, 2))
     })
 }
 
-removeChangedTranslationKeys()
+function getChangedKeys() {
+    const oldI18n = execSync(`git show ${process.env.TRANSLATION_BASE_BRANCH}:src/i18n/translations/en.json`, {
+        encoding: "utf8",
+    })
+    const newI18n = fs.readFileSync("src/i18n/translations/en.json", "utf8")
+
+    const oldJson = JSON.parse(oldI18n)
+    const newJson = JSON.parse(newI18n)
+
+    const changedKeys: string[] = Object.keys(newJson).filter(key => oldJson[key] !== newJson[key])
+
+    console.log("Changed keys: ", changedKeys)
+
+    return changedKeys
+}
+
+function main() {
+    const changedKeys = getChangedKeys()
+    removeChangedTranslationKeys(changedKeys)
+}
+
+main()
