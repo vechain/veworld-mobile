fd0ec4968f33ef27dabd46c4b11e85b314d39af3
HiiiiD <salomone.marco@outlook.it>
2025-07-18 21:32:44 +0200

    chore: add more docs

--- src/Components/Base/BaseCarousel/FullscreenBaseCarousel.tsx
+++ src/Components/Base/BaseCarousel/FullscreenBaseCarousel.tsx
@@ -45,9 +45,16 @@ export const FullscreenBaseCarousel = ({
             data.map((_, idx, arr) => {
                 //If it's the first item, return 0
                 if (idx === 0) return 0
-                if (idx === arr.length - 1)
-                    return baseWidth - 3 * gap * (data.length - 2) + baseWidth * (data.length - 2)
-                return baseWidth * idx - 3 * gap * idx
+                // if (idx === arr.length - 1)
+                //     return baseWidth - 3 * gap * (data.length - 2) + baseWidth * (data.length - 2)
+                if (idx === arr.length - 1) {
+                    //Calculate the length of N-1 tabs
+                    const tabsNormalLength = baseWidth * (data.length - 1)
+                    //Gap of all the middle tabs (based on the calculation below)
+                    const middleTabsLength = 3 * gap * (data.length - 2)
+                    return tabsNormalLength - middleTabsLength
+                }
+                return (baseWidth - 3 * gap) * idx
             }),
         [baseWidth, data, gap],
     )
