d05705b8350ca5a6ac5f8dba70ee00b03091370c
HiiiiD <salomone.marco@outlook.it>
2025-07-21 16:27:03 +0200

    fix: add migration to remove TESTNET B3TR & VOT3 from mainnet

---
+++ src/Storage/Redux/Migrations/Migration23.ts
@@ -0,0 +1,35 @@
+import { PersistedState } from "redux-persist/es/types"
+import { ERROR_EVENTS, TEST_B3TR_ADDRESS, TEST_VOT3_ADDRESS } from "~Constants"
+import { AddressUtils, debug } from "~Utils"
+import { BalanceState } from "../Slices"
+
+export const Migration23 = (state: PersistedState): PersistedState => {
+    debug(ERROR_EVENTS.SECURITY, "Performing migration 23: Adding dapps notifications state")
+
+    // @ts-ignore
+    const currentState: BalanceState = state.balances
+
+    if (!currentState || Object.keys(currentState).length === 0) {
+        debug(ERROR_EVENTS.SECURITY, "================= **** No state to migrate **** =================")
+        return state
+    }
+
+    const newState: BalanceState = {
+        ...currentState,
+        mainnet: Object.fromEntries(
+            Object.entries(currentState.mainnet).map(([account, balances]) => [
+                account,
+                balances.filter(
+                    balance =>
+                        !AddressUtils.compareAddresses(balance.tokenAddress, TEST_B3TR_ADDRESS) &&
+                        !AddressUtils.compareAddresses(balance.tokenAddress, TEST_VOT3_ADDRESS),
+                ),
+            ]),
+        ),
+    }
+
+    return {
+        ...state,
+        notification: newState,
+    } as PersistedState
+}
