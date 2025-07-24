5538c13edc834269b364366b8e4fd973ca682ecf
HiiiiD <salomone.marco@outlook.it>
2025-07-24 12:53:46 +0200

    fix: fix TypeUtils function signature

--- src/Utils/TypeUtils/TypeUtils.ts
+++ src/Utils/TypeUtils/TypeUtils.ts
@@ -2,6 +2,6 @@
  * Assert that a value is defined (neither undefined nor null)
  * @param arg Value to check for
  */
-export const assertDefined = (arg: unknown): asserts arg is NonNullable<typeof arg> => {
+export function assertDefined(arg: unknown): asserts arg is NonNullable<typeof arg> {
     if (arg === undefined || arg === null) throw new TypeError("[assertDefined]: argument is not defined")
 }
