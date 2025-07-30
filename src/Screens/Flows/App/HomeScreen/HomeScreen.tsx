20bcd2aaa5279b16899b6ac6c3f99eeafc2abc14
paolampilla <43100344+paolampilla@users.noreply.github.com>
2025-07-30 10:27:25 +0200

    chore: standardize change account bottomsheet (#3123)

--- src/Screens/Flows/App/HomeScreen/HomeScreen.tsx
+++ src/Screens/Flows/App/HomeScreen/HomeScreen.tsx
@@ -265,7 +265,6 @@ export const HomeScreen = () => {
                         accounts={accounts}
                         setSelectedAccount={setSelectedAccount}
                         selectedAccount={selectedAccount}
-                        isBalanceVisible={isBalanceVisible}
                         ref={selectAccountBottomSheetRef}
                     />
 
