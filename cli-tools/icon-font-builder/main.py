f8eb17f7816b32b32620c0ec7bba38fc652604f4
Michael Martin <michael.martin1997@gmail.com>
2025-08-01 10:05:08 +0200

    feat: enhance icon font builder with improved map generation and font properties

--- cli-tools/icon-font-builder/main.py
+++ cli-tools/icon-font-builder/main.py
@@ -3,22 +3,29 @@ import fontforge
 import argparse
 import json
 import os
+import collections
 
 CACHE_DIR = "./svg_cache"
 
-def generate_map_file(map_data: dict[str, int], font_output: str):
-    content = "export const designSystemIconMap = {%s}" % ", ".join(f"\"{key}\": {value}" for key, value in map_data.items())
-    with open(f"{font_output}/designSystemIconMap.ts", "w") as f:
+def generate_map_file(map_data: dict[str, int], output_dir: str):
+    content = "export const designSystemIconMap = {\n%s\n}" % ",\n".join(f"    \"{key}\": {value}" for key, value in collections.OrderedDict(sorted(map_data.items())).items())
+    with open(f"{output_dir}/designSystemIconMap.ts", "w") as f:
         f.write(content)
         f.close()
 
-def build_font(font_name: str, font_map: str, font_prefix: str, font_output: str):
+def build_font(font_name: str, font_map: str, font_prefix: str, output_dir: str):
     font_map: dict[Literal["icons", "sets", "groups"], list[dict]] = json.load(open(font_map))
 
     if not os.path.exists(CACHE_DIR):
         os.makedirs(CACHE_DIR, mode=0o777)
 
     font = fontforge.font()
+    font.familyname = font_name
+    font.fontname = font_name
+    font.fullname = font_name
+    font.copyright = "VeChain Foundation 2025"
+    font.encoding = "UnicodeFull"
+    font.em=256
 
     map_data = {}
 
@@ -29,11 +36,11 @@ def build_font(font_name: str, font_map: str, font_prefix: str, font_output: str
 
         map_data[font_prefix + icon["name"]] = icon["code"]
 
-        glyph = font.createChar(icon["code"], font_prefix + icon["name"])
-        glyph.importOutlines(f"{CACHE_DIR}/{icon['name']}.svg")
+        glyph = font.createChar(icon["code"], icon["name"])
+        glyph.importOutlines(f"{CACHE_DIR}/{icon['name']}.svg", simplify=False, accuracy=0.5)
     
-    generate_map_file(map_data, font_output)
-    font.generate(f"{font_output}/{font_name}.ttf")
+    generate_map_file(map_data, output_dir)
+    font.generate(f"{output_dir}/{font_name}.ttf", flags=["glyph-map-file"])
 
 
 if __name__ == "__main__":
