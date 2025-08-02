c117ab33981cd61c20cc4fb5276638dc8954a65c
Michael Martin <michael.martin1997@gmail.com>
2025-08-02 16:27:26 +0200

    feat: implement migration to version 25 with new key pair for signing session tokens

--- src/Storage/Redux/Helpers.ts
+++ src/Storage/Redux/Helpers.ts
@@ -73,7 +73,7 @@ export const getPersistorConfig = async (mmkv: MMKV, encryptionKey: string): Pro
     return {
         key: "root",
         storage,
-        version: 24,
+        version: 25,
         blacklist: [NftSlice.name, PendingSlice.name],
         whitelist: [
             CurrencySlice.name,
