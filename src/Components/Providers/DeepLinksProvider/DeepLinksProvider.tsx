9e61c57c3e0c00b6a70ea8204a03f8be6d364855
HiiiiD <salomone.marco@outlook.it>
2025-08-12 15:01:25 +0200

    feat: create tab on deeplink

---
+++ src/Components/Providers/DeepLinksProvider/DeepLinksProvider.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react"
+import { Linking } from "react-native"
+import { useBrowserTab } from "~Hooks/useBrowserTab"
+export const DeepLinksProvider = ({ children }: { children: React.ReactNode }) => {
+    const { navigateWithTab } = useBrowserTab()
+    useEffect(() => {
+        const handleDeepLink = ({ url }: { url: string }) => {
+            const parsed = new URL(url)
+            const pathname = parsed.pathname
+            //Filter out all the useless elements
+            const splitPathname = pathname.split("/").filter(Boolean)
+            switch (splitPathname[0]) {
+                case "discover": {
+                    const lastElement = splitPathname[splitPathname.length - 1]
+                    const decodedURI = decodeURIComponent(lastElement)
+                    navigateWithTab({ url: decodedURI, title: decodedURI, navigationFn: () => {} })
+                    break
+                }
+                default:
+                    return
+            }
+        }
+
+        Linking.addEventListener("url", handleDeepLink)
+
+        return () => {
+            Linking.removeAllListeners("url")
+        }
+    }, [navigateWithTab])
+
+    return <>{children}</>
+}
