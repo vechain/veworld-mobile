03752f423718e072e6b20d0400ad1a3b856f04cc
Michael Martin <michael.martin1997@gmail.com>
2025-08-13 11:42:10 +0200

    refactor: update typography styles in BalanceView and VbdBalanceCard components for consistency

--- src/Screens/Flows/App/AssetDetailScreen/Components/BalanceView.tsx
+++ src/Screens/Flows/App/AssetDetailScreen/Components/BalanceView.tsx
@@ -101,7 +101,7 @@ export const BalanceView = ({
                     iconSize={26}
                     isCrossChainToken={!!tokenWithInfo.crossChainProvider}
                 />
-                <BaseText color={theme.colors.assetDetailsCard.title} typographyFont="subSubTitleSemiBold">
+                <BaseText color={theme.colors.assetDetailsCard.title} typographyFont="bodySemiBold">
                     {symbol}
                 </BaseText>
                 {isTokensOwnedLoading ? (
@@ -113,7 +113,7 @@ export const BalanceView = ({
                         width={60}
                     />
                 ) : (
-                    <BaseText color={theme.colors.assetDetailsCard.title} typographyFont="subSubTitleSemiBold">
+                    <BaseText color={theme.colors.assetDetailsCard.title} typographyFont="bodySemiBold">
                         {isBalanceVisible
                             ? BigNutils(balance?.balance).toHuman(decimals).toTokenFormatFull_string(7, formatLocale)
                             : "•••••"}
