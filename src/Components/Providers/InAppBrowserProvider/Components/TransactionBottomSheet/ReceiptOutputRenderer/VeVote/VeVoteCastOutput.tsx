2151cbfb6546e35deb76ad8582ead3e58de5d966
Marco Salomone <61231210+HiiiiD@users.noreply.github.com>
2025-08-13 17:41:04 +0200

    217 enahnce   add vevote vote cast event (#3179)
    * feat: add vevote cast activity to activity screen
    
    * feat: add translations
    
    * feat: setup vevote-cast business event
    
    * feat: add renderer for VeVote cast business event
    
    * feat: add translations
    
    * test: add tests
    
    ---------
    
    Co-authored-by: github-actions-language-translator <github-actions-language-translator@github.com>

---
+++ src/Components/Providers/InAppBrowserProvider/Components/TransactionBottomSheet/ReceiptOutputRenderer/VeVote/VeVoteCastOutput.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { useI18nContext } from "~i18n"
+import { BaseReceiptOutput, ReceiptOutputProps } from "../BaseReceiptOutput"
+
+type Props = ReceiptOutputProps<"VeVote_VoteCast(address,uint256,uint8,string,uint256,uint256[],address)">
+
+export const VeVoteCastOutput = ({ output, ...props }: Props) => {
+    const { LL } = useI18nContext()
+
+    return (
+        <BaseReceiptOutput
+            label={LL.RECEIPT_OUTPUT_VEVOTE_CAST()}
+            iconKey="icon-vote"
+            output={output}
+            testID="VEVOTE_VOTE_CAST"
+            {...props}
+        />
+    )
+}
