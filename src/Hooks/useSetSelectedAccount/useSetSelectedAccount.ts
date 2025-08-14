22038c27f572406a189d7407a5f32f1839cf0f9f
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-08-14 10:39:10 +0100

    refactor(hook): remove refetch stargate from account change hook

--- src/Hooks/useSetSelectedAccount/useSetSelectedAccount.ts
+++ src/Hooks/useSetSelectedAccount/useSetSelectedAccount.ts
@@ -1,12 +1,10 @@
 import { selectSelectedAccountOrNull, setSelectedAccount, useAppDispatch, useAppSelector } from "~Storage/Redux"
 import { useResetStacks } from "./useResetStacks"
-import { useFetchingStargate } from "../../StargateEventListener/Hooks/useFetchingStargate"
 import { AddressUtils } from "~Utils"
 
 export const useSetSelectedAccount = () => {
     const selectedAccount = useAppSelector(selectSelectedAccountOrNull)
     const { resetStacks } = useResetStacks()
-    const { refetchStargateData } = useFetchingStargate()
     const dispatch = useAppDispatch()
 
     const onSetSelectedAccount = ({ address }: { address?: string }) => {
@@ -16,11 +14,6 @@ export const useSetSelectedAccount = () => {
 
         resetStacks()
         address && dispatch(setSelectedAccount({ address }))
-
-        // Refetch Stargate data for the new account to ensure fresh data
-        if (address) {
-            refetchStargateData(address)
-        }
     }
 
     return { onSetSelectedAccount }
