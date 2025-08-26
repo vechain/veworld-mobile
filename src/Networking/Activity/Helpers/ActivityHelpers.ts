48608aba23ded0bfbc86f4549e6c2612e5fda925
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-08-26 13:02:00 +0100

    feat(activites): add NFT buy/sell cases on helper

--- src/Networking/Activity/Helpers/ActivityHelpers.ts
+++ src/Networking/Activity/Helpers/ActivityHelpers.ts
@@ -21,6 +21,7 @@ import {
     IndexedHistoryEvent,
     Network,
     NonFungibleTokenActivity,
+    NFTMarketplaceActivity,
     SignCertActivity,
     StargateActivity,
     SwapActivity,
@@ -506,6 +507,8 @@ const processActivity = (
         case ActivityType.TRANSFER_VET:
             return enrichActivityWithVetTransfer(activity, clause, direction)
         case ActivityType.TRANSFER_NFT:
+        case ActivityType.NFT_SALE:
+        case ActivityType.NFT_PURCHASE:
             return enrichActivityWithNFTData(activity, clause, direction)
         default:
             return enrichActivityWithDappData(activity, appName, appUrl)
@@ -606,6 +609,21 @@ export const createActivityFromIndexedHistoryEvent = (
                 contractAddress: contractAddress,
                 direction: direction,
             } as NonFungibleTokenActivity
+        }
+        case ActivityEvent.NFT_SALE:
+        case ActivityEvent.NFT_PURCHASE: {
+            const direction = AddressUtils.compareAddresses(from, selectedAccountAddress)
+                ? DIRECTIONS.UP
+                : DIRECTIONS.DOWN
+            return {
+                ...baseActivity,
+                tokenId: tokenId,
+                contractAddress: contractAddress,
+                direction: direction,
+                price: value ?? "0",
+                buyer: to ?? "",
+                seller: from ?? "",
+            } as NFTMarketplaceActivity
         }
         case ActivityEvent.SWAP_FT_TO_VET: {
             return {
