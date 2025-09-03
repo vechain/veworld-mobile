fee4901adab544a9ec93cf6ce6aa117404d9d712
HiiiiD <salomone.marco@outlook.it>
2025-09-03 18:39:10 +0200

    fix: final animation polish

--- src/Hooks/useScrollableBottomSheetList/useScrollableBottomSheetList.ts
+++ src/Hooks/useScrollableBottomSheetList/useScrollableBottomSheetList.ts
@@ -1,6 +1,7 @@
 import { MutableRefObject, useCallback, useMemo, useState } from "react"
-import { LayoutChangeEvent, ViewStyle } from "react-native"
-import { SCREEN_HEIGHT } from "~Constants"
+import { LayoutChangeEvent, useWindowDimensions, ViewStyle } from "react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { usePrevious } from "~Hooks/usePrevious"
 
 type Args = {
     onResize: (value: boolean) => void
@@ -12,35 +13,51 @@ type Args = {
  * Do not use @gorhom/bottom-sheet components, just use these
  */
 export const useScrollableBottomSheetList = ({ onResize, initialLayout }: Args) => {
-    const [height, setHeight] = useState(SCREEN_HEIGHT)
+    const { height: windowHeight } = useWindowDimensions()
+    const { bottom: bottomSafeAreaSize } = useSafeAreaInsets()
+
+    const maxHeight = useMemo(
+        () => Math.floor((windowHeight - bottomSafeAreaSize) * 0.85),
+        [bottomSafeAreaSize, windowHeight],
+    )
+
+    const [height, setHeight] = useState(maxHeight)
+    const previousHeight = usePrevious(height)
+    const [offsetY, setOffsetY] = useState(160)
     const onContentSizeChange = useCallback(
         (_: number, contentHeight: number) => {
             if (contentHeight <= height) {
-                onResize(true)
-                setHeight(contentHeight)
+                const overflows = contentHeight + offsetY >= height
+                const minValue = overflows ? maxHeight - offsetY - 100 : contentHeight
+                setHeight(minValue)
+                //If the height increased, just set it as a small viewport to set the correct size
+                if ((previousHeight ?? 0) < contentHeight) onResize(true)
+                //Otherwise make the component figure it out itself
+                else onResize(overflows ? false : true)
             } else {
                 onResize(false)
             }
         },
-        [height, onResize],
+        [height, maxHeight, offsetY, onResize, previousHeight],
     )
 
     const onLayout = useCallback(
         (e: LayoutChangeEvent) => {
             const _height = e.nativeEvent.layout.height
+            setOffsetY(e.nativeEvent.layout.y)
             if (initialLayout.current) return
 
-            if (_height < SCREEN_HEIGHT) {
+            if (_height < maxHeight) {
                 setHeight(_height)
                 initialLayout.current = true
             }
         },
-        [initialLayout],
+        [initialLayout, maxHeight],
     )
 
     const style = useMemo(() => ({ maxHeight: height, height } as ViewStyle), [height])
 
-    const resetHeight = useCallback(() => setHeight(SCREEN_HEIGHT), [])
+    const resetHeight = useCallback(() => setHeight(maxHeight), [maxHeight])
 
     return useMemo(
         () => ({ style, onLayout, onContentSizeChange, resetHeight }),
