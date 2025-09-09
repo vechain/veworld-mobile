157ee41fe7e48d5d8c19c4a5ff0e1172cf62e5e2
Marco Salomone <61231210+HiiiiD@users.noreply.github.com>
2025-09-09 23:40:01 +0200

    310 enhance   favs for urls (#3266)
    * fix: fix favorites
    
    * fix: initial round of UI fixes
    
    * fix: fix small things
    
    * feat: create migration
    
    * test: useDappBookmarking
    
    * test: add Migration27 tests
    
    * feat: add translations
    
    ---------
    
    Co-authored-by: Michael Martin <michael.martin1997@gmail.com>
    Co-authored-by: github-actions-language-translator <github-actions-language-translator@github.com>

---
+++ src/Screens/Flows/App/AppsScreen/Components/Favourites/FavouritesV2.tsx
@@ -0,0 +1,90 @@
+import React, { useCallback } from "react"
+import { FlatList, ListRenderItemInfo, StyleSheet } from "react-native"
+import { BaseIcon, BaseSpacer, BaseText, BaseTouchable, BaseView } from "~Components"
+import { COLORS, DiscoveryDApp } from "~Constants"
+import { useTheme } from "~Hooks"
+import { useI18nContext } from "~i18n"
+import { DAppCardV2 } from "./DAppCardV2"
+
+type BookmarkListProps = {
+    bookmarkedDApps: DiscoveryDApp[]
+    onDAppPress: (dapp: DiscoveryDApp) => void
+}
+
+const ItemSeparatorComponent = () => <BaseSpacer width={8} />
+const FooterComponent = () => <BaseSpacer width={16} />
+
+const BookmarkedDAppsList = ({ bookmarkedDApps, onDAppPress }: BookmarkListProps) => {
+    const renderItem = useCallback(
+        ({ item }: ListRenderItemInfo<DiscoveryDApp>) => {
+            return <DAppCardV2 dapp={item} onPress={() => onDAppPress(item)} showDappTitle={false} iconSize={72} />
+        },
+        [onDAppPress],
+    )
+
+    return (
+        <FlatList
+            data={bookmarkedDApps}
+            horizontal
+            keyExtractor={(_item, index) => _item?.href ?? index.toString()}
+            renderItem={renderItem}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.flatListContainer}
+            ItemSeparatorComponent={ItemSeparatorComponent}
+            ListFooterComponent={FooterComponent}
+        />
+    )
+}
+
+type FavouritesProps = {
+    bookmarkedDApps: DiscoveryDApp[]
+    onActionLabelPress: () => void
+    onDAppPress: (dapp: DiscoveryDApp) => void
+}
+
+export const FavouritesV2 = React.memo(({ bookmarkedDApps, onActionLabelPress, onDAppPress }: FavouritesProps) => {
+    const { LL } = useI18nContext()
+    const showBookmarkedDAppsList = bookmarkedDApps.length > 0
+    const theme = useTheme()
+
+    return (
+        <BaseView gap={16} flexDirection="column">
+            <BaseView flexDirection="row" justifyContent="space-between" px={16} alignItems="center">
+                <BaseText typographyFont="subSubTitleSemiBold" color={theme.isDark ? COLORS.GREY_100 : COLORS.GREY_800}>
+                    {LL.DISCOVER_TAB_FAVOURITES()}
+                </BaseText>
+
+                {showBookmarkedDAppsList && (
+                    <BaseTouchable action={onActionLabelPress}>
+                        <BaseView flexDirection="row">
+                            <BaseText
+                                typographyFont="buttonMedium"
+                                mx={2}
+                                color={theme.isDark ? COLORS.GREY_300 : COLORS.DARK_PURPLE}>
+                                {LL.DISCOVER_SEE_ALL_BOOKMARKS()}
+                            </BaseText>
+                            <BaseIcon
+                                name="icon-arrow-right"
+                                //This should be 12, but given that we're increasing the font size of the label
+                                //It makes sense to have it set as 14
+                                size={14}
+                                color={theme.isDark ? COLORS.GREY_300 : COLORS.DARK_PURPLE}
+                            />
+                        </BaseView>
+                    </BaseTouchable>
+                )}
+            </BaseView>
+
+            {showBookmarkedDAppsList && (
+                <BookmarkedDAppsList bookmarkedDApps={bookmarkedDApps.slice(0, 15)} onDAppPress={onDAppPress} />
+            )}
+        </BaseView>
+    )
+})
+
+const styles = StyleSheet.create({
+    flatListContainer: {
+        paddingLeft: 16,
+    },
+})
