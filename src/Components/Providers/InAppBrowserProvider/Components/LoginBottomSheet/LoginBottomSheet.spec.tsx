bd18f1fe95667bf81b54523c69134d94c800a419
HiiiiD <salomone.marco@outlook.it>
2025-09-11 10:22:38 +0200

    test: pr feedback for LoginBottomSheet

--- src/Components/Providers/InAppBrowserProvider/Components/LoginBottomSheet/LoginBottomSheet.spec.tsx
+++ src/Components/Providers/InAppBrowserProvider/Components/LoginBottomSheet/LoginBottomSheet.spec.tsx
@@ -86,7 +86,7 @@ describe("LoginBottomSheet", () => {
         expect(screen.queryByTestId("LOGIN_REQUEST_TITLE")).toBeNull()
     })
 
-    it.each(["in-app"] as const)("should be able to cancel login (%s)", async () => {
+    it("should be able to cancel login", async () => {
         const loginBsRef = { current: { present: jest.fn(), close: jest.fn() } }
         const postMessage = jest.fn()
         jest.spyOn(InteractionProvider, "useInteraction").mockReturnValue({
@@ -97,7 +97,6 @@ describe("LoginBottomSheet", () => {
                 type: "in-app",
                 appUrl: "https://vechain.org",
                 appName: "TEST APP",
-                isFirstRequest: false,
                 external: false,
                 genesisId: TESTNET_NETWORK.genesisBlock.id,
                 kind: "simple",
@@ -136,54 +135,9 @@ describe("LoginBottomSheet", () => {
         })
     })
 
-    it.each(["simple", "certificate", "typed-data"] as const)("should be able to login with (%s)", async kind => {
+    it("should be able to login without signing", async () => {
         const loginBsRef = { current: { present: jest.fn(), close: jest.fn() } }
         const postMessage = jest.fn()
-        const getValue = () => {
-            switch (kind) {
-                case "simple":
-                    return null
-                case "certificate":
-                    return {
-                        purpose: "identification",
-                        payload: {
-                            type: "text",
-                            content: "Test certificate: <<veworld_address>>",
-                        },
-                    }
-                case "typed-data":
-                    return {
-                        domain: {
-                            name: "Ether Mail",
-                            version: "1",
-                            chainId: "1176455790972829965191905223412607679856028701100105089447013101863",
-                            verifyingContract: "0x1CAB02Ec1922F1a5a55996de8c590161A88378b9",
-                        },
-                        types: {
-                            Person: [
-                                { name: "name", type: "string" },
-                                { name: "wallet", type: "address" },
-                            ],
-                            Mail: [
-                                { name: "from", type: "Person" },
-                                { name: "to", type: "Person" },
-                                { name: "contents", type: "string" },
-                            ],
-                        },
-                        value: {
-                            from: {
-                                name: "Cow",
-                                wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-                            },
-                            to: {
-                                name: "Bob",
-                                wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
-                            },
-                            contents: "Hello, Bob!",
-                        },
-                    }
-            }
-        }
         jest.spyOn(InteractionProvider, "useInteraction").mockReturnValue({
             loginBsRef,
             loginBsData: {
@@ -195,8 +149,153 @@ describe("LoginBottomSheet", () => {
                 isFirstRequest: false,
                 external: false,
                 genesisId: TESTNET_NETWORK.genesisBlock.id,
-                kind: kind,
-                value: getValue(),
+                kind: "simple",
+                value: null,
+            },
+            setLoginBsData: jest.fn(),
+        } as any)
+        jest.spyOn(InAppBrowserProvider, "useInAppBrowser").mockReturnValue({
+            postMessage,
+        } as any)
+        TestHelpers.render.renderComponentWithProps(<LoginBottomSheet />, {
+            wrapper: TestWrapper,
+            initialProps: {
+                preloadedState: {
+                    accounts: {
+                        accounts: [mockAccountWithDevice1],
+                        selectedAccount: mockAccountWithDevice1.address,
+                    },
+                    devices: [device1],
+                },
+            },
+        })
+        await act(() => {
+            loginBsRef.current.present()
+        })
+        expect(screen.queryByTestId("LOGIN_REQUEST_TITLE")).not.toBeNull()
+        await act(() => {
+            fireEvent.press(screen.getByTestId("LOGIN_REQUEST_BTN_SIGN"))
+        })
+        expect(postMessage).toHaveBeenCalledWith({
+            id: "0x1",
+            data: { signer: mockAccountWithDevice1.address },
+            method: RequestMethods.CONNECT,
+        })
+    })
+
+    it("should be able to login with certificate", async () => {
+        const loginBsRef = { current: { present: jest.fn(), close: jest.fn() } }
+        const postMessage = jest.fn()
+        jest.spyOn(InteractionProvider, "useInteraction").mockReturnValue({
+            loginBsRef,
+            loginBsData: {
+                method: RequestMethods.CONNECT,
+                id: "0x1",
+                type: "in-app",
+                appUrl: "https://vechain.org",
+                appName: "TEST APP",
+                isFirstRequest: false,
+                external: false,
+                genesisId: TESTNET_NETWORK.genesisBlock.id,
+                kind: "certificate",
+                value: {
+                    purpose: "identification",
+                    payload: {
+                        type: "text",
+                        content: "Test certificate: <<veworld_address>>",
+                    },
+                },
+            },
+            setLoginBsData: jest.fn(),
+        } as any)
+        jest.spyOn(InAppBrowserProvider, "useInAppBrowser").mockReturnValue({
+            postMessage,
+        } as any)
+        const wallet = HDNode.fromMnemonic(defaultMnemonicPhrase).derive(0)
+        ;(WalletEncryptionKeyHelper.decryptWallet as jest.Mock).mockResolvedValue({
+            mnemonic: defaultMnemonicPhrase,
+            rootAddress: wallet.address,
+            nonce: "nonce",
+        })
+
+        TestHelpers.render.renderComponentWithProps(<LoginBottomSheet />, {
+            wrapper: TestWrapper,
+            initialProps: {
+                preloadedState: {
+                    accounts: {
+                        accounts: [mockAccountWithDevice1],
+                        selectedAccount: mockAccountWithDevice1.address,
+                    },
+                    devices: [device1],
+                },
+            },
+        })
+        await act(() => {
+            loginBsRef.current.present()
+        })
+        expect(screen.queryByTestId("LOGIN_REQUEST_TITLE")).not.toBeNull()
+        await act(() => {
+            fireEvent.press(screen.getByTestId("LOGIN_REQUEST_BTN_SIGN"))
+        })
+        expect(postMessage).toHaveBeenCalledWith({
+            id: "0x1",
+            data: {
+                annex: {
+                    domain: "vechain.org",
+                    signer: mockAccountWithDevice1.address.toLowerCase(),
+                    timestamp: expect.any(Number),
+                },
+                signature: expect.any(String),
+            },
+            method: RequestMethods.CONNECT,
+        })
+    })
+
+    it("should be able to login with typed-data", async () => {
+        const loginBsRef = { current: { present: jest.fn(), close: jest.fn() } }
+        const postMessage = jest.fn()
+        jest.spyOn(InteractionProvider, "useInteraction").mockReturnValue({
+            loginBsRef,
+            loginBsData: {
+                method: RequestMethods.CONNECT,
+                id: "0x1",
+                type: "in-app",
+                appUrl: "https://vechain.org",
+                appName: "TEST APP",
+                isFirstRequest: false,
+                external: false,
+                genesisId: TESTNET_NETWORK.genesisBlock.id,
+                kind: "typed-data",
+                value: {
+                    domain: {
+                        name: "Ether Mail",
+                        version: "1",
+                        chainId: "1176455790972829965191905223412607679856028701100105089447013101863",
+                        verifyingContract: "0x1CAB02Ec1922F1a5a55996de8c590161A88378b9",
+                    },
+                    types: {
+                        Person: [
+                            { name: "name", type: "string" },
+                            { name: "wallet", type: "address" },
+                        ],
+                        Mail: [
+                            { name: "from", type: "Person" },
+                            { name: "to", type: "Person" },
+                            { name: "contents", type: "string" },
+                        ],
+                    },
+                    value: {
+                        from: {
+                            name: "Cow",
+                            wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+                        },
+                        to: {
+                            name: "Bob",
+                            wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+                        },
+                        contents: "Hello, Bob!",
+                    },
+                },
             },
             setLoginBsData: jest.fn(),
         } as any)
@@ -230,41 +329,18 @@ describe("LoginBottomSheet", () => {
             fireEvent.press(screen.getByTestId("LOGIN_REQUEST_BTN_SIGN"))
         })
 
-        switch (kind) {
-            case "simple":
-                expect(postMessage).toHaveBeenCalledWith({
-                    id: "0x1",
-                    data: { signer: mockAccountWithDevice1.address },
-                    method: RequestMethods.CONNECT,
-                })
-                break
-            case "certificate":
-                expect(postMessage).toHaveBeenCalledWith({
-                    id: "0x1",
-                    data: {
-                        annex: {
-                            domain: "vechain.org",
-                            signer: mockAccountWithDevice1.address.toLowerCase(),
-                            timestamp: expect.any(Number),
-                        },
-                        signature: expect.any(String),
-                    },
-                    method: RequestMethods.CONNECT,
-                })
-                break
-            case "typed-data":
-                expect(postMessage).toHaveBeenCalledWith({
-                    id: "0x1",
-                    data: {
-                        signer: mockAccountWithDevice1.address,
-                        signature:
-                            // eslint-disable-next-line max-len
-                            "0xbc10d67411b5a5409e96717dee98f37ef682712e8f37521c261f369b0f287bb71daeddfec1ce37c2b8faa19727d97ad8a1198895d6c6860f0f2ee8fe74d961b51b",
-                    },
-                    method: RequestMethods.CONNECT,
-                })
-        }
+        expect(postMessage).toHaveBeenCalledWith({
+            id: "0x1",
+            data: {
+                signer: mockAccountWithDevice1.address,
+                signature:
+                    // eslint-disable-next-line max-len
+                    "0xbc10d67411b5a5409e96717dee98f37ef682712e8f37521c261f369b0f287bb71daeddfec1ce37c2b8faa19727d97ad8a1198895d6c6860f0f2ee8fe74d961b51b",
+            },
+            method: RequestMethods.CONNECT,
+        })
     })
+
     it("should be able to replace typed data placeholder", async () => {
         const loginBsRef = { current: { present: jest.fn(), close: jest.fn() } }
         const postMessage = jest.fn()
