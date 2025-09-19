7c6486b8de1f3d83a8797d2256283a92f59dec03
HiiiiD <salomone.marco@outlook.it>
2025-09-19 16:37:38 +0200

    fix: complete animation

--- src/Screens/Flows/App/BalanceScreen/Components/Balance/SlotMachineText.tsx
+++ src/Screens/Flows/App/BalanceScreen/Components/Balance/SlotMachineText.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useMemo, useState } from "react"
-import { LayoutChangeEvent, LayoutRectangle, StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native"
+import React, { useCallback, useEffect, useMemo, useState } from "react"
+import { LayoutChangeEvent, LayoutRectangle, StyleSheet, Text } from "react-native"
 import LinearGradient from "react-native-linear-gradient"
 import Animated, {
     FadeIn,
     FadeOut,
+    LinearTransition,
     SharedValue,
     useAnimatedStyle,
     useSharedValue,
     withSpring,
-    withTiming,
 } from "react-native-reanimated"
 import { COLORS } from "~Constants"
 import { useThemedStyles } from "~Hooks"
@@ -17,24 +17,17 @@ type Props = {
     value: string
 }
 
-// const VALUE_ARRAY = Array.from({ length: 30 }, (_, idx) => ({ id: idx.toString(), value: idx % 10 }))
 const VALUE_ARRAY = Array.from({ length: 10 }, (_, idx) => ({ id: idx.toString(), value: idx % 10 }))
 
-const T = ({
+const SlotMachineTextElement = ({
     item,
     translate,
     index,
-    style,
-    opacity,
-    target,
     onLayout,
 }: {
     item: (typeof VALUE_ARRAY)[number]
     translate: SharedValue<number>
-    opacity: SharedValue<number>
     index: number
-    style: StyleProp<ViewStyle>
-    target: number
     onLayout: (e: LayoutChangeEvent) => void
 }) => {
     const { styles } = useThemedStyles(baseStyles)
@@ -42,12 +35,11 @@ const T = ({
     const animatedStyles = useAnimatedStyle(() => {
         return {
             transform: [{ translateY: -translate.value }],
-            // opacity: target === item.value ? 1 : opacity.value,
         }
-    }, [translate.value, opacity, target])
+    }, [translate.value])
     return (
         <Animated.Text
-            style={[styles.text, styles.absolute, animatedStyles, { top: 40 * index }, style]}
+            style={[styles.text, styles.absolute, animatedStyles, { top: 40 * index }]}
             key={item.id}
             exiting={FadeOut.duration(300)}
             entering={FadeIn.duration(300)}
@@ -62,7 +54,6 @@ export const SlotMachineText = ({ value }: Props) => {
 
     const translateY = useSharedValue(0)
     const opacity = useSharedValue(0)
-    const overflow = useSharedValue(0)
 
     const [sizes, setSizes] = useState<LayoutRectangle[]>([])
 
@@ -71,24 +62,27 @@ export const SlotMachineText = ({ value }: Props) => {
         return parseInt(value, 10)
     }, [value])
 
+    const onLayout = useCallback(
+        (index: number) => (e: LayoutChangeEvent) => {
+            const rectangle = { ...e.nativeEvent.layout }
+            setSizes(old => {
+                const newArr = [...old]
+                newArr[index] = rectangle
+                return newArr
+            })
+        },
+        [],
+    )
+
     useEffect(() => {
         if (!/\d/.test(value)) return
         if (sizes.length !== 10) return
-        overflow.value = 1
-
-        overflow.value = withTiming(0, { duration: 280 })
 
         translateY.value = withSpring(
             sizes.filter((_, idx) => idx < parsedValue).reduce((acc, curr) => acc + curr.height, 0),
             undefined,
         )
-    }, [opacity, overflow, parsedValue, sizes, translateY, value])
-
-    const animatedStyles = useAnimatedStyle(() => {
-        return {
-            overflow: overflow.value > 0 ? "visible" : "hidden",
-        }
-    }, [overflow.value])
+    }, [opacity, parsedValue, sizes, translateY, value])
 
     if (!/\d/.test(value))
         return (
@@ -98,43 +92,27 @@ export const SlotMachineText = ({ value }: Props) => {
         )
 
     return (
-        <View style={[styles.root]}>
+        <Animated.View style={[styles.root]} layout={LinearTransition.duration(300)}>
             <LinearGradient
-                colors={[
-                    COLORS.BALANCE_BACKGROUND,
-                    "rgba(29, 23, 58, 0.7)",
-                    "rgba(0, 0, 0, 0)",
-                    "rgba(0, 0, 0, 0)",
-                    "rgba(29, 23, 58, 0.7)",
-                    COLORS.BALANCE_BACKGROUND,
-                ]}
+                colors={[COLORS.BALANCE_BACKGROUND, "rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 0)", COLORS.BALANCE_BACKGROUND]}
                 angle={180}
                 useAngle
-                locations={[0, 0.2, 0.3, 0.7, 0.8, 1]}
+                locations={[0, 0.2, 0.7, 1]}
                 style={[StyleSheet.absoluteFill, styles.gradient]}
             />
-            <Animated.View style={[styles.innerContainer, animatedStyles]}>
-                <Text style={[styles.text, styles.hiddenText]}>{0}</Text>
+            <Animated.View style={[styles.innerContainer]} layout={LinearTransition.duration(300)}>
+                <Text style={[styles.text, styles.hiddenText]}>{parsedValue}</Text>
                 {VALUE_ARRAY.map((item, idx) => (
-                    <T
+                    <SlotMachineTextElement
+                        key={idx}
                         item={item}
                         index={idx}
                         translate={translateY}
-                        style={{}}
-                        opacity={opacity}
-                        target={parseInt(value, 10)}
-                        onLayout={e => {
-                            const rectangle = { ...e.nativeEvent.layout }
-                            setSizes(old => {
-                                const newArr = [...old]
-                                newArr[idx] = rectangle
-                                return newArr
-                            })
-                        }}
+                        onLayout={e => onLayout(idx)(e)}
                     />
                 ))}
             </Animated.View>
-        </View>
+        </Animated.View>
     )
 }
 
@@ -143,7 +121,7 @@ const baseStyles = () =>
         text: {
             color: COLORS.GREY_50,
             fontWeight: "600",
-            fontSize: 36,
+            fontSize: 38,
             fontFamily: "Inter-SemiBold",
             lineHeight: 40,
         },
@@ -158,11 +136,12 @@ const baseStyles = () =>
             overflow: "hidden",
             flexDirection: "column",
             justifyContent: "center",
-            height: 70,
+            height: 46,
         },
         innerContainer: {
             height: 40,
             position: "relative",
+            overflow: "hidden",
         },
         hiddenText: {
             opacity: 0,
