e53697f4abdf9bf93cb3a86a952f65a3be96bcd7
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-09-19 12:14:05 +0100

    feat(balance): add source tracking to TabRenderer dApp navigation

--- src/Screens/Flows/App/BalanceScreen/Tabs/TabRenderer.tsx
+++ src/Screens/Flows/App/BalanceScreen/Tabs/TabRenderer.tsx
@@ -1,10 +1,15 @@
 import React, { useMemo, useState } from "react"
 import { LayoutChangeEvent, StyleSheet } from "react-native"
-import { BaseSimpleTabs, BaseView } from "~Components"
+import { BaseSimpleTabs, BaseSpacer, BaseView } from "~Components"
 import { COLORS, ColorThemeType } from "~Constants"
 import { useThemedStyles } from "~Hooks"
 import { useI18nContext } from "~i18n"
+import { Routes } from "~Navigation"
 import { Tokens } from "./Tokens"
+import { FavouritesV2 } from "../../AppsScreen/Components/Favourites/FavouritesV2"
+import { selectBookmarkedDapps } from "~Storage/Redux/Selectors"
+import { useAppSelector } from "~Storage/Redux/Hooks"
+import { useDAppActions } from "../../AppsScreen/Hooks/useDAppActions"
 
 const TABS = ["TOKENS", "STAKING", "COLLECTIBLES"] as const
 
@@ -16,10 +21,18 @@ export const TabRenderer = ({ onLayout }: Props) => {
     const { LL } = useI18nContext()
     const { styles } = useThemedStyles(baseStyles)
     const [selectedTab, setSelectedTab] = useState<(typeof TABS)[number]>("TOKENS")
-
+    const bookmarkedDApps = useAppSelector(selectBookmarkedDapps)
+    const { onDAppPress } = useDAppActions(Routes.HOME)
+    const showFavorites = bookmarkedDApps.length > 0
     const labels = useMemo(() => TABS.map(tab => LL[`BALANCE_TAB_${tab}`]()), [LL])
     return (
         <BaseView style={styles.root} gap={16} onLayout={onLayout}>
+            {showFavorites && (
+                <>
+                    <FavouritesV2 bookmarkedDApps={bookmarkedDApps} onDAppPress={onDAppPress} renderCTASeeAll={false} />
+                    <BaseSpacer height={8} />
+                </>
+            )}
             <BaseSimpleTabs keys={TABS} labels={labels} selectedKey={selectedTab} setSelectedKey={setSelectedTab} />
             <BaseView>{selectedTab === "TOKENS" ? <Tokens /> : null}</BaseView>
         </BaseView>
