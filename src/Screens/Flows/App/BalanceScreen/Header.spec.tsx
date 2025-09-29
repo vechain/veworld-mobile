3a888713fec309968ee1fa295124888e9f33a156
HiiiiD <salomone.marco@outlook.it>
2025-09-29 17:50:16 +0200

    fix: fix header test

--- src/Screens/Flows/App/BalanceScreen/Header.spec.tsx
+++ src/Screens/Flows/App/BalanceScreen/Header.spec.tsx
@@ -14,7 +14,7 @@ describe("BalanceScreen -> Header", () => {
     beforeEach(() => {
         jest.clearAllMocks()
     })
-    it("should show the correct user", () => {
+    it("should show the humanized address if the alias is the standard alias", () => {
         ;(useVns as jest.Mock).mockReturnValue({ name: undefined })
         const qrCodeBottomSheetRef = {
             current: {
@@ -30,7 +30,44 @@ describe("BalanceScreen -> Header", () => {
             { wrapper: TestWrapper },
         )
 
-        expect(screen.getByTestId("BALANCE_HEADER_DISPLAY_USERNAME")).toHaveTextContent("Account 1")
+        expect(screen.getByTestId("BALANCE_HEADER_DISPLAY_USERNAME")).toHaveTextContent("0xCF13…2957")
+        expect(screen.queryByTestId("BALANCE_HEADER_VIEW_ONLY")).toBeNull()
+    })
+    it("should show the account alias if it is not standard", () => {
+        ;(useVns as jest.Mock).mockReturnValue({ name: undefined })
+        const qrCodeBottomSheetRef = {
+            current: {
+                present: jest.fn(),
+            },
+        }
+        TestHelpers.render.renderComponentWithProps(
+            <Header
+                scrollY={{ value: 0 } as any}
+                contentOffsetY={{ value: 0 } as any}
+                qrCodeBottomSheetRef={qrCodeBottomSheetRef as any}
+            />,
+            {
+                wrapper: TestWrapper,
+                initialProps: {
+                    preloadedState: {
+                        accounts: {
+                            accounts: [
+                                {
+                                    address: "0xCF130b42Ae33C5531277B4B7c0F1D994B8732957",
+                                    alias: "Test Account",
+                                    index: 0,
+                                    rootAddress: "0x90d70a5d0e9ce28336f7d45990b9c63c0a4142g0",
+                                    visible: true,
+                                },
+                            ],
+                            selectedAccount: "0xCF130b42Ae33C5531277B4B7c0F1D994B8732957",
+                        },
+                    },
+                },
+            },
+        )
+
+        expect(screen.getByTestId("BALANCE_HEADER_DISPLAY_USERNAME")).toHaveTextContent("Test Account")
         expect(screen.queryByTestId("BALANCE_HEADER_VIEW_ONLY")).toBeNull()
     })
     it("should show the vet domain if it is not veworld.vet", () => {
