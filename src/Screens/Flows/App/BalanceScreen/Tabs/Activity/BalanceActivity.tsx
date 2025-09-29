b68b44595749c6610d39a648065389bdd36be448
HiiiiD <salomone.marco@outlook.it>
2025-09-29 12:45:48 +0200

    fix: refactor + cleanup + skeleton + invalidation

--- src/Screens/Flows/App/BalanceScreen/Tabs/Activity/BalanceActivity.tsx
+++ src/Screens/Flows/App/BalanceScreen/Tabs/Activity/BalanceActivity.tsx
@@ -1,90 +1,30 @@
 import { useNavigation } from "@react-navigation/native"
-import { useQuery } from "@tanstack/react-query"
-import moment from "moment"
-import React, { useCallback, useMemo } from "react"
+import React, { useCallback } from "react"
 import { FlatList, ListRenderItemInfo } from "react-native"
 import { BaseSpacer, BaseText, BaseView } from "~Components"
 import { COLORS } from "~Constants"
 import { useTheme } from "~Hooks"
 import { useI18nContext } from "~i18n"
-import { Activity, ActivityEvent, FungibleToken, TransactionOutcomes } from "~Model"
+import { Activity, FungibleToken, TransactionOutcomes } from "~Model"
 import { Routes } from "~Navigation"
-import { createActivityFromIndexedHistoryEvent, fetchIndexedHistoryEvent } from "~Networking"
 import { ActivityItemRenderer } from "~Screens/Flows/App/ActivityScreen/Components/ActivityItemRenderer"
-import { selectSelectedAccount, selectSelectedNetwork, useAppSelector } from "~Storage/Redux"
-import { DateUtils, StringUtils } from "~Utils"
+import { SkeletonActivity } from "../../Components/Activity/SkeletonActivity"
+import { useBalanceActivities } from "../../Hooks/useBalanceActivities"
+import { useActivityTimestampRenderer } from "../../Hooks/useTimestampRenderer"
 import { BalanceTab } from "../types"
-
-const getActivityEventsByTab = (tab: BalanceTab): ActivityEvent[] => {
-    switch (tab) {
-        case "TOKENS":
-            return [
-                ActivityEvent.SWAP_FT_TO_FT,
-                ActivityEvent.SWAP_FT_TO_VET,
-                ActivityEvent.SWAP_VET_TO_FT,
-                ActivityEvent.TRANSFER_FT,
-                ActivityEvent.TRANSFER_SF,
-                ActivityEvent.TRANSFER_VET,
-                ActivityEvent.B3TR_ACTION,
-                ActivityEvent.B3TR_CLAIM_REWARD,
-                ActivityEvent.B3TR_SWAP_B3TR_TO_VOT3,
-                ActivityEvent.B3TR_SWAP_VOT3_TO_B3TR,
-                ActivityEvent.B3TR_UPGRADE_GM,
-            ]
-        case "STAKING":
-            return [
-                ActivityEvent.STARGATE_CLAIM_REWARDS_BASE,
-                ActivityEvent.STARGATE_CLAIM_REWARDS_DELEGATE,
-                ActivityEvent.STARGATE_DELEGATE,
-                ActivityEvent.STARGATE_DELEGATE_ONLY,
-                ActivityEvent.STARGATE_STAKE,
-                ActivityEvent.STARGATE_UNDELEGATE,
-                ActivityEvent.STARGATE_UNSTAKE,
-            ]
-        case "COLLECTIBLES":
-            return [ActivityEvent.NFT_SALE, ActivityEvent.TRANSFER_NFT]
-    }
-}
 
 const ItemSeparatorComponent = () => <BaseSpacer height={8} />
 
+const LOADING_ITEMS = [{ id: "1" }, { id: "2" }, { id: "3" }, { id: "4" }]
+
 export const BalanceActivity = ({ tab }: { tab: BalanceTab }) => {
-    const { LL, locale: _locale } = useI18nContext()
+    const { LL } = useI18nContext()
     const theme = useTheme()
-    const selectedAccount = useAppSelector(selectSelectedAccount)
-    const selectedNetwork = useAppSelector(selectSelectedNetwork)
     const nav = useNavigation()
-    const locale = useMemo(() => DateUtils.getMomentLocale(_locale), [_locale])
 
-    const fetchActivities = useCallback(async () => {
-        return await fetchIndexedHistoryEvent(selectedAccount.address, 0, selectedNetwork, getActivityEventsByTab(tab))
-    }, [selectedAccount.address, selectedNetwork, tab])
+    const timestampRenderer = useActivityTimestampRenderer()
 
-    const timestampRenderer = useCallback(
-        (timestamp: number) => {
-            const ts = moment(timestamp)
-            if (ts.isSame(moment(), "day")) return ts.locale(locale).format(`HH:mm - [${LL.TODAY()}]`)
-            if (ts.isSame(moment().subtract(1, "day"), "day"))
-                return ts.locale(locale).format(`HH:mm - [${LL.YESTERDAY()}]`)
-            if (moment().diff(ts, "year") >= 1)
-                return StringUtils.toTitleCase(
-                    ts.locale(locale).format(`HH:mm - [${DateUtils.formatDate(ts, locale, { includeYear: true })}]`),
-                )
-            return StringUtils.toTitleCase(ts.locale(locale).format(`HH:mm - [${DateUtils.formatDate(ts, locale)}]`))
-        },
-        [LL, locale],
-    )
-
-    const { data, isLoading } = useQuery({
-        queryKey: ["BALANCE_ACTIVITIES", tab, selectedAccount.address, selectedNetwork.genesis.id],
-        queryFn: fetchActivities,
-        select(_data) {
-            return _data.data
-                .slice(0, 4)
-                .map(evt => createActivityFromIndexedHistoryEvent(evt, selectedAccount.address, selectedNetwork))
-                .filter((activity): activity is NonNullable<typeof activity> => activity !== null)
-        },
-    })
+    const { data, isLoading } = useBalanceActivities({ tab })
 
     const onActivityPress = useCallback(
         (activity: Activity, token?: FungibleToken, isSwap?: boolean, decodedClauses?: TransactionOutcomes) => {
@@ -107,6 +47,10 @@ export const BalanceActivity = ({ tab }: { tab: BalanceTab }) => {
         [onActivityPress, timestampRenderer],
     )
 
+    const renderLoadingItem = useCallback(() => {
+        return <SkeletonActivity />
+    }, [])
+
     if (!isLoading && data?.length === 0) return null
 
     return (
@@ -114,12 +58,21 @@ export const BalanceActivity = ({ tab }: { tab: BalanceTab }) => {
             <BaseText typographyFont="subSubTitleSemiBold" color={theme.isDark ? COLORS.GREY_50 : COLORS.DARK_PURPLE}>
                 {LL.ACTIVITY()}
             </BaseText>
-            <FlatList
-                renderItem={renderItem}
-                data={data ?? []}
-                ItemSeparatorComponent={ItemSeparatorComponent}
-                keyExtractor={item => item.id}
-            />
+            {isLoading ? (
+                <FlatList
+                    renderItem={renderLoadingItem}
+                    data={LOADING_ITEMS}
+                    ItemSeparatorComponent={ItemSeparatorComponent}
+                    keyExtractor={item => item.id}
+                />
+            ) : (
+                <FlatList
+                    renderItem={renderItem}
+                    data={data ?? []}
+                    ItemSeparatorComponent={ItemSeparatorComponent}
+                    keyExtractor={item => item.id}
+                />
+            )}
         </BaseView>
     )
 }
