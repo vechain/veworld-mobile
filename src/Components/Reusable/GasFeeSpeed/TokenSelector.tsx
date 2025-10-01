f9aeda6dd51870bfc1c93e565f34923bced9b6b4
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-10-01 16:10:37 +0100

    refactor(style): add a more reliable style on token selector

--- src/Components/Reusable/GasFeeSpeed/TokenSelector.tsx
+++ src/Components/Reusable/GasFeeSpeed/TokenSelector.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react"
 import { StyleProp, StyleSheet, ViewStyle } from "react-native"
-import { BaseButton, BaseIcon } from "~Components/Base"
+import { BaseButton, BaseIcon, BaseView } from "~Components/Base"
 import { B3TR, COLORS, ColorThemeType, VET, VTHO } from "~Constants"
 import { useThemedStyles } from "~Hooks"
 import { selectAllTokens, useAppSelector } from "~Storage/Redux"
@@ -36,26 +36,31 @@ export const TokenSelector = ({ token, onPress, style }: Props) => {
     return (
         <BaseButton
             leftIcon={
-                <TokenImage
-                    icon={icon}
-                    isVechainToken={isVechainToken(token)}
-                    isCrossChainToken={isCrossChainToken}
-                    iconSize={16}
-                />
+                <BaseView style={styles.iconWrapper}>
+                    <TokenImage
+                        icon={icon}
+                        isVechainToken={isVechainToken(token)}
+                        isCrossChainToken={isCrossChainToken}
+                        iconSize={16}
+                    />
+                </BaseView>
             }
             rightIcon={
-                <BaseIcon
-                    name="icon-chevrons-up-down"
-                    size={16}
-                    color={theme.isDark ? COLORS.GREY_300 : COLORS.GREY_600}
-                />
+                <BaseView style={styles.iconWrapper}>
+                    <BaseIcon
+                        name="icon-chevrons-up-down"
+                        size={16}
+                        color={theme.isDark ? COLORS.GREY_300 : COLORS.GREY_600}
+                    />
+                </BaseView>
             }
             action={onPress}
             variant="solid"
             px={12}
             py={8}
             style={[styles.root, style]}
-            textColor={theme.isDark ? COLORS.GREY_100 : COLORS.GREY_800}>
+            textColor={theme.isDark ? COLORS.GREY_100 : COLORS.GREY_800}
+            numberOfLines={1}>
             {token}
         </BaseButton>
     )
@@ -64,9 +69,12 @@ export const TokenSelector = ({ token, onPress, style }: Props) => {
 const baseStyles = (theme: ColorThemeType) =>
     StyleSheet.create({
         root: {
-            gap: 8,
             borderColor: theme.isDark ? theme.colors.transparent : COLORS.GREY_200,
             borderWidth: theme.isDark ? 0 : 1,
             backgroundColor: theme.isDark ? COLORS.DARK_PURPLE_DISABLED : COLORS.WHITE,
+            justifyContent: "center",
+        },
+        iconWrapper: {
+            marginHorizontal: 4,
         },
     })
