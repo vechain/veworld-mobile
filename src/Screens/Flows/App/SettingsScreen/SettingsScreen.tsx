ebf86e151b7f157aabbdc0059583d0cfaf6060dc
HiiiiD <salomone.marco@outlook.it>
2025-10-02 09:52:53 +0200

    fix: remove duplication

--- src/Screens/Flows/App/SettingsScreen/SettingsScreen.tsx
+++ src/Screens/Flows/App/SettingsScreen/SettingsScreen.tsx
@@ -34,6 +34,8 @@ type BackupBannerItem = {
     title: string
 }
 
+type SettingsItem = SettingsRowItem | DividerItem | BackupBannerItem
+
 export const SettingsScreen = () => {
     const { LL } = useI18nContext()
     const devFeaturesEnabled = useAppSelector(selectAreDevFeaturesEnabled)
@@ -86,7 +88,7 @@ export const SettingsScreen = () => {
     }, [LL])
 
     const renderItem = useCallback(
-        (props: { item: SettingsRowItem | DividerItem | BackupBannerItem }) => {
+        (props: { item: SettingsItem }) => {
             const { item } = props
 
             switch (item.element) {
@@ -151,152 +153,134 @@ const baseStyles = () =>
         },
     })
 
+const DIVIDER = {
+    element: "divider",
+    title: "Support_divider",
+    height: 1,
+} satisfies DividerItem
+
+const BACKUP_BANNER = {
+    element: "backupBanner",
+    title: "Backup_Warning",
+} satisfies BackupBannerItem
+
 const getLists = (
     LL: TranslationFunctions,
     devEnabled: boolean,
     notificationFeatureEnabled: boolean,
     isObservedAccount: boolean,
-): { settingsList: (SettingsRowItem | DividerItem | BackupBannerItem)[] } => {
-    if (isObservedAccount) {
-        return {
-            settingsList: [
-                {
-                    element: "settingsRow",
-                    title: LL.TITLE_GENERAL_SETTINGS(),
-                    screenName: Routes.SETTINGS_GENERAL,
-                    icon: "icon-settings",
-                },
-                {
-                    element: "settingsRow",
-                    title: LL.TITLE_MANAGE_WALLET(),
-                    screenName: Routes.WALLET_MANAGEMENT,
-                    icon: "icon-wallet",
-                },
-                {
-                    element: "settingsRow",
-                    title: LL.TITLE_NETWORKS(),
-                    screenName: Routes.SETTINGS_NETWORK,
-                    icon: "icon-globe",
-                },
-                {
-                    element: "divider",
-                    title: "Support_divider",
-                    height: 1,
-                },
-                {
-                    element: "settingsRow",
-                    title: LL.TITLE_GET_SUPPORT(),
-                    screenName: Routes.BROWSER,
-                    icon: "icon-help-circle",
-                    url: "https://support.veworld.com",
-                },
-                {
-                    element: "settingsRow",
-                    title: LL.TITLE_GIVE_FEEDBACK(),
-                    screenName: Routes.BROWSER,
-                    icon: "icon-message-square",
-                    url: "https://forms.office.com/e/Vq1CUJD9Vy",
-                },
-                {
-                    element: "settingsRow",
-                    title: LL.TITLE_ABOUT(),
-                    screenName: Routes.SETTINGS_ABOUT,
-                    icon: "icon-info",
-                },
-            ],
-        }
-    }
-    const settingsList: (SettingsRowItem | DividerItem | BackupBannerItem)[] = [
-        {
+): { settingsList: SettingsItem[] } => {
+    const tiles = {
+        GENERAL_SETTINGS: {
             element: "settingsRow",
             title: LL.TITLE_GENERAL_SETTINGS(),
             screenName: Routes.SETTINGS_GENERAL,
             icon: "icon-settings",
         },
-        {
+        MANAGE_WALLET: {
             element: "settingsRow",
             title: LL.TITLE_MANAGE_WALLET(),
             screenName: Routes.WALLET_MANAGEMENT,
             icon: "icon-wallet",
         },
-
-        {
-            element: "settingsRow",
-            title: LL.TITLE_TRANSACTIONS(),
-            screenName: Routes.SETTINGS_TRANSACTIONS,
-            icon: "icon-transaction",
-        },
-        {
+        NETWORKS: {
             element: "settingsRow",
             title: LL.TITLE_NETWORKS(),
             screenName: Routes.SETTINGS_NETWORK,
             icon: "icon-globe",
         },
-        {
-            element: "settingsRow",
-            title: LL.TITLE_CONNECTED_APPS(),
-            screenName: Routes.SETTINGS_CONNECTED_APPS,
-            icon: "icon-apps",
-        },
-        {
-            element: "settingsRow",
-            title: LL.TITLE_CONTACTS(),
-            screenName: Routes.SETTINGS_CONTACTS,
-            icon: "icon-users",
-        },
-        {
-            element: "settingsRow",
-            title: LL.TITLE_PRIVACY(),
-            screenName: Routes.SETTINGS_PRIVACY,
-            icon: "icon-shield-check",
-        },
-        {
-            element: "backupBanner",
-            title: "Backup_Warning",
-        },
-        {
-            element: "divider",
-            title: "Support_divider",
-            height: 1,
-        },
-        {
+        GET_SUPPORT: {
             element: "settingsRow",
             title: LL.TITLE_GET_SUPPORT(),
             screenName: Routes.BROWSER,
             icon: "icon-help-circle",
             url: "https://support.veworld.com",
         },
-        {
+        GIVE_FEEDBACK: {
             element: "settingsRow",
             title: LL.TITLE_GIVE_FEEDBACK(),
             screenName: Routes.BROWSER,
             icon: "icon-message-square",
             url: "https://forms.office.com/e/Vq1CUJD9Vy",
         },
-        {
+        ABOUT: {
             element: "settingsRow",
             title: LL.TITLE_ABOUT(),
             screenName: Routes.SETTINGS_ABOUT,
             icon: "icon-info",
         },
-    ]
-
-    if (notificationFeatureEnabled) {
-        settingsList.splice(4, 0, {
+        TRANSACTIONS: {
+            element: "settingsRow",
+            title: LL.TITLE_TRANSACTIONS(),
+            screenName: Routes.SETTINGS_TRANSACTIONS,
+            icon: "icon-transaction",
+        },
+        CONNECTED_APPS: {
+            element: "settingsRow",
+            title: LL.TITLE_CONNECTED_APPS(),
+            screenName: Routes.SETTINGS_CONNECTED_APPS,
+            icon: "icon-apps",
+        },
+        CONTACTS: {
+            element: "settingsRow",
+            title: LL.TITLE_CONTACTS(),
+            screenName: Routes.SETTINGS_CONTACTS,
+            icon: "icon-users",
+        },
+        PRIVACY: {
+            element: "settingsRow",
+            title: LL.TITLE_PRIVACY(),
+            screenName: Routes.SETTINGS_PRIVACY,
+            icon: "icon-shield-check",
+        },
+        NOTIFICATIONS: {
             element: "settingsRow",
             title: LL.TITLE_NOTIFICATIONS(),
             screenName: Routes.SETTINGS_NOTIFICATIONS,
             icon: "icon-bell-ring",
-        })
-    }
-
-    if (devEnabled) {
-        settingsList.push({
+        },
+        ALERTS: {
             element: "settingsRow",
             title: LL.TITLE_ALERTS(),
             screenName: Routes.SETTINGS_ALERTS,
             icon: "icon-bell",
-        })
+        },
+    } satisfies Record<string, SettingsItem>
+
+    if (isObservedAccount) {
+        return {
+            settingsList: [
+                tiles.GENERAL_SETTINGS,
+                tiles.MANAGE_WALLET,
+                tiles.NETWORKS,
+                DIVIDER,
+                tiles.GET_SUPPORT,
+                tiles.GIVE_FEEDBACK,
+                tiles.ABOUT,
+            ],
+        }
+    }
+    const settingsList: SettingsItem[] = [
+        tiles.GENERAL_SETTINGS,
+        tiles.MANAGE_WALLET,
+        tiles.TRANSACTIONS,
+        tiles.NETWORKS,
+        tiles.CONNECTED_APPS,
+        tiles.CONTACTS,
+        tiles.PRIVACY,
+        BACKUP_BANNER,
+        DIVIDER,
+        tiles.GET_SUPPORT,
+        tiles.GIVE_FEEDBACK,
+        tiles.ABOUT,
+    ]
+
+    if (notificationFeatureEnabled) {
+        settingsList.splice(4, 0, tiles.NOTIFICATIONS)
+    }
+
+    if (devEnabled) {
+        settingsList.push(tiles.ALERTS)
     }
 
     return { settingsList }
