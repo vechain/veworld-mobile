2ed02478b50e2d85bc25d0adb2334b8aa55e501a
paolampilla <paolampilla@gmail.com>
2025-10-02 05:31:53 +0200

    chore: add strings and logos

--- src/Screens/Flows/App/BalanceScreen/Tabs/TabRenderer.tsx
+++ src/Screens/Flows/App/BalanceScreen/Tabs/TabRenderer.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from "react"
 import { LayoutChangeEvent, StyleSheet } from "react-native"
 import Animated from "react-native-reanimated"
-import { BaseSimpleTabs, BaseSpacer, BaseView } from "~Components"
+import { BaseButton, BaseSimpleTabs, BaseSpacer, BaseText, BaseView, Icon } from "~Components"
 import { COLORS, ColorThemeType } from "~Constants"
 import { useThemedStyles } from "~Hooks"
 import { useI18nContext } from "~i18n"
@@ -12,12 +12,102 @@ import { AccountUtils } from "~Utils"
 import { FavouritesV2 } from "../../AppsScreen/Components/Favourites/FavouritesV2"
 import { useDAppActions } from "../../AppsScreen/Hooks/useDAppActions"
 import { Tokens } from "./Tokens"
+import { B3trLogoSVG, VeBetterFullLogoSVG } from "~Assets"
+import { StatsCard } from "../Components/VeBetterDao/StatsCard"
 
 const TABS = ["TOKENS", "STAKING", "COLLECTIBLES"] as const
 
 type Props = {
     onLayout: (e: LayoutChangeEvent) => void
 }
+
+const NewUserVeBetterCard = () => {
+    const { styles, theme } = useThemedStyles(style)
+    const { LL } = useI18nContext()
+
+    return (
+        <BaseView style={styles.root} testID="VEBETTER_DAO_NEW_USER_CARD">
+            <BaseView flexDirection="column" gap={16}>
+                <BaseView flexDirection="row" gap={6}>
+                    <BaseText typographyFont="body" color={theme.isDark ? COLORS.WHITE : COLORS.PURPLE}>
+                        {LL.VBD_JOIN_THE()}
+                    </BaseText>
+                    <VeBetterFullLogoSVG height={16} />
+                    <BaseText color={theme.isDark ? COLORS.WHITE : COLORS.PURPLE}>{LL.VBD_MOVEMENT()}</BaseText>
+                </BaseView>
+
+                <BaseView pt={20} px={24} pb={20} gap={16} flexDirection="column" style={styles.inner}>
+                    <BaseView flexDirection="row" gap={12} justifyContent="space-between" alignItems="center">
+                        <Icon name="icon-leafs" color={COLORS.LIME_GREEN} size={24} />
+                        <BaseText>{LL.VBD_TOTAL_ACTIONS()}</BaseText>
+                        <BaseText>{"999.9M"}</BaseText>
+                    </BaseView>
+                    <BaseView flexDirection="row" gap={12} justifyContent="space-between" w={100}>
+                        <B3trLogoSVG color={COLORS.LIME_GREEN} width={24} height={24} />
+                        <BaseText>{LL.VBD_TOTAL_REWARDED()}</BaseText>
+                        <BaseText>{"999.9M"}</BaseText>
+                    </BaseView>
+                </BaseView>
+            </BaseView>
+
+            <BaseView>
+                <BaseText>{LL.VBD_GET_REWARDED()}</BaseText>
+                <BaseText>{LL.VBD_CONTRIBUTE_OFFSET()}</BaseText>
+            </BaseView>
+
+            <BaseView w={100} flexDirection="column" gap={8}>
+                <BaseView flexDirection="row" gap={8}>
+                    <StatsCard label="co2" value={999} />
+                    <StatsCard label="energy" value={999} />
+                </BaseView>
+                <BaseView flexDirection="row" gap={8}>
+                    <StatsCard label="water" value={999} />
+                    <StatsCard label="plastic" value={999} />
+                </BaseView>
+            </BaseView>
+
+            <BaseButton title={LL.VBD_START_YOUR_IMPACT()} action={() => {}} />
+        </BaseView>
+    )
+}
+
+const style = (theme: ColorThemeType) =>
+    StyleSheet.create({
+        root: {
+            backgroundColor: theme.isDark ? COLORS.PURPLE : COLORS.WHITE,
+            gap: 24,
+            paddingTop: 32,
+            paddingBottom: 24,
+            paddingHorizontal: 24,
+            position: "relative",
+            flexDirection: "column",
+            borderRadius: 16,
+        },
+        actionsText: {
+            fontWeight: 600,
+            fontSize: 40,
+            fontFamily: "Inter-SemiBold",
+            lineHeight: 40,
+        },
+        inner: {
+            borderWidth: 1,
+            borderColor: "rgba(185, 181, 207, 0.15)",
+            backgroundColor: theme.isDark ? "rgba(89, 82, 127, 0.25)" : theme.colors.transparent,
+            borderRadius: 16,
+            position: "relative",
+        },
+        b3mo: {
+            position: "absolute",
+            right: -24,
+            top: 8,
+            width: 196,
+            height: 196,
+        },
+        b3moImage: {
+            width: "100%",
+            height: "100%",
+        },
+    })
 
 export const TabRenderer = ({ onLayout }: Props) => {
     const { LL } = useI18nContext()
@@ -32,6 +122,7 @@ export const TabRenderer = ({ onLayout }: Props) => {
     const labels = useMemo(() => TABS.map(tab => LL[`BALANCE_TAB_${tab}`]()), [LL])
     return (
         <Animated.View style={styles.root} onLayout={onLayout}>
+            <NewUserVeBetterCard />
             {showFavorites && (
                 <>
                     <FavouritesV2
--- src/i18n/i18n-types.ts
+++ src/i18n/i18n-types.ts
@@ -2050,19 +2050,6 @@ type RootTranslation = {
 	 */
 	CONNECTED_APP_ASKING_FOR_ACCESS_2: string
 	/**
-	 * {​d​a​p​p​N​a​m​e​}​ ​w​i​l​l​ ​n​o​ ​l​o​n​g​e​r​ ​h​a​v​e​ ​a​c​c​e​s​s​ ​t​o​:
-	 * @param {string} dappName
-	 */
-	DISCONNECTED_APP_ASKING_FOR_ACCESS: RequiredParams<'dappName'>
-	/**
-	 * N​o​ ​l​o​n​g​e​r​ ​r​e​q​u​e​s​t​ ​t​r​a​n​s​a​c​t​i​o​n​s​ ​t​o​ ​s​e​n​d​ ​t​o​ ​V​e​C​h​a​i​n​ ​T​h​o​r​.
-	 */
-	DISCONNECTED_APP_ASKING_FOR_ACCESS_1: string
-	/**
-	 * N​o​ ​l​o​n​g​e​r​ ​r​e​q​u​e​s​t​ ​y​o​u​r​ ​s​i​g​n​a​t​u​r​e​ ​o​n​ ​c​e​r​t​i​f​i​c​a​t​e​s​ ​o​r​ ​i​d​e​n​t​i​f​i​c​a​t​i​o​n​ ​a​n​d​ ​a​g​r​e​e​m​e​n​t​s​.
-	 */
-	DISCONNECTED_APP_ASKING_FOR_ACCESS_2: string
-	/**
 	 * Y​o​u​ ​h​a​v​e​ ​p​r​e​v​i​o​u​s​l​y​ ​c​o​n​n​e​c​t​e​d​ ​t​o​ ​{​d​a​p​p​}​ ​w​i​t​h​ ​a​ ​d​i​f​f​e​r​e​n​t​ ​a​c​c​o​u​n​t​.​ ​D​o​ ​y​o​u​ ​w​a​n​t​ ​t​o​ ​s​w​i​t​c​h​ ​t​o​ ​t​h​e​ ​a​c​c​o​u​n​t​ ​c​o​n​n​e​c​t​e​d​ ​t​o​ ​{​d​a​p​p​}​?
 	 * @param {string} dapp
 	 */
@@ -2075,10 +2062,6 @@ type RootTranslation = {
 	 * C​o​n​n​e​c​t​i​o​n​ ​r​e​q​u​e​s​t​s
 	 */
 	CONNECTED_APP_CONNECTION_REQUESTS: string
-	/**
-	 * D​i​s​c​o​n​n​e​c​t​i​o​n​ ​r​e​q​u​e​s​t
-	 */
-	DISCONNECTION_REQUEST: string
 	/**
 	 * C​o​n​t​r​a​c​t
 	 */
@@ -2448,6 +2431,23 @@ type RootTranslation = {
 	 * D​e​v​i​c​e​ ​P​i​n
 	 */
 	DEVICE_PIN: string
+	/**
+	 * {​d​a​p​p​N​a​m​e​}​ ​w​i​l​l​ ​n​o​ ​l​o​n​g​e​r​ ​h​a​v​e​ ​a​c​c​e​s​s​ ​t​o​:
+	 * @param {string} dappName
+	 */
+	DISCONNECTED_APP_ASKING_FOR_ACCESS: RequiredParams<'dappName'>
+	/**
+	 * N​o​ ​l​o​n​g​e​r​ ​r​e​q​u​e​s​t​ ​t​r​a​n​s​a​c​t​i​o​n​s​ ​t​o​ ​s​e​n​d​ ​t​o​ ​V​e​C​h​a​i​n​ ​T​h​o​r​.
+	 */
+	DISCONNECTED_APP_ASKING_FOR_ACCESS_1: string
+	/**
+	 * N​o​ ​l​o​n​g​e​r​ ​r​e​q​u​e​s​t​ ​y​o​u​r​ ​s​i​g​n​a​t​u​r​e​ ​o​n​ ​c​e​r​t​i​f​i​c​a​t​e​s​ ​o​r​ ​i​d​e​n​t​i​f​i​c​a​t​i​o​n​ ​a​n​d​ ​a​g​r​e​e​m​e​n​t​s​.
+	 */
+	DISCONNECTED_APP_ASKING_FOR_ACCESS_2: string
+	/**
+	 * D​i​s​c​o​n​n​e​c​t​i​o​n​ ​r​e​q​u​e​s​t
+	 */
+	DISCONNECTION_REQUEST: string
 	/**
 	 * M​a​k​e​ ​y​o​u​r​ ​o​w​n​ ​D​A​p​p​ ​o​n​ ​V​e​c​h​a​i​n
 	 */
@@ -4495,7 +4495,6 @@ type RootTranslation = {
 	 * S​u​c​c​e​s​s​!
 	 */
 	SUCCESS_GENERIC: string
-
 	/**
 	 * V​i​e​w​ ​o​p​e​r​a​t​i​o​n​ ​d​e​t​a​i​l​.
 	 */
@@ -4913,6 +4912,22 @@ type RootTranslation = {
 	 */
 	VALUE_TITLE: string
 	/**
+	 * c​o​n​t​r​i​b​u​t​e​ ​t​o​ ​t​h​e​ ​o​f​f​s​e​t
+	 */
+	VBD_CONTRIBUTE_OFFSET: string
+	/**
+	 * g​e​t​ ​r​e​w​a​r​d​e​d​ ​f​o​r​ ​y​o​u​r​ ​a​c​t​i​o​n​s​ ​a​n​d
+	 */
+	VBD_GET_REWARDED: string
+	/**
+	 * J​o​i​n​ ​t​h​e
+	 */
+	VBD_JOIN_THE: string
+	/**
+	 * m​o​v​e​m​e​n​t
+	 */
+	VBD_MOVEMENT: string
+	/**
 	 * R​e​w​a​r​d​s​ ​e​a​r​n​e​d
 	 */
 	VBD_REWARDS_EARNED: string
@@ -4921,17 +4936,21 @@ type RootTranslation = {
 	 */
 	VBD_SEE_PROFILE: string
 	/**
+	 * S​t​a​r​t​ ​y​o​u​r​ ​i​m​p​a​c​t
+	 */
+	VBD_START_YOUR_IMPACT: string
+	/**
+	 * A​v​o​i​d​e​d
+	 */
+	VBD_STAT_AVOIDED: string
+	/**
 	 * C​O​2
 	 */
 	VBD_STAT_CO2: string
 	/**
-	 * T​r​e​e​s
+	 * C​o​n​s​e​r​v​e​d
 	 */
-	VBD_STAT_TREES: string
-	/**
-	 * W​a​t​e​r
-	 */
-	VBD_STAT_WATER: string
+	VBD_STAT_CONSERVED: string
 	/**
 	 * E​n​e​r​g​y
 	 */
@@ -4949,21 +4968,29 @@ type RootTranslation = {
 	 */
 	VBD_STAT_SAVED: string
 	/**
-	 * C​o​n​s​e​r​v​e​d
+	 * T​r​e​e​s
 	 */
-	VBD_STAT_CONSERVED: string
+	VBD_STAT_TREES: string
 	/**
-	 * A​v​o​i​d​e​d
+	 * W​a​t​e​r
 	 */
-	VBD_STAT_AVOIDED: string
+	VBD_STAT_WATER: string
 	/**
-	 * Y​o​u​r​ ​o​f​f​s​e​t
+	 * T​o​t​a​l​ ​B​e​t​t​e​r​ ​a​c​t​i​o​n​s
 	 */
-	VBD_YOUR_OFFSET: string
+	VBD_TOTAL_ACTIONS: string
+	/**
+	 * T​o​t​a​l​ ​B​3​T​R​ ​r​e​w​a​r​d​e​d
+	 */
+	VBD_TOTAL_REWARDED: string
 	/**
 	 * Y​o​u​r​ ​B​e​t​t​e​r​ ​A​c​t​i​o​n​s
 	 */
 	VBD_YOUR_BETTER_ACTIONS: string
+	/**
+	 * Y​o​u​r​ ​o​f​f​s​e​t
+	 */
+	VBD_YOUR_OFFSET: string
 	/**
 	 * V​e​c​h​a​i​n​ ​n​e​w​s​ ​&​ ​u​p​d​a​t​e​s
 	 */
@@ -4981,13 +5008,13 @@ type RootTranslation = {
 	 */
 	VEWORLD: string
 	/**
-	 * V​i​e​w​ ​o​n​ ​e​x​p​l​o​r​e​r
-	 */
-	VIEW_ON_EXPLORER: string
-	/**
 	 * V​i​e​w​ ​o​n​l​y
 	 */
 	VIEW_ONLY: string
+	/**
+	 * V​i​e​w​ ​o​n​ ​e​x​p​l​o​r​e​r
+	 */
+	VIEW_ON_EXPLORER: string
 	/**
 	 * w​a​l​l​e​t​s​ ​s​a​v​e​d​ ​o​n​ ​G​o​o​g​l​e​ ​D​r​i​v​e​.
 	 */
@@ -7179,18 +7206,6 @@ Thanks for your patience!
 	 */
 	CONNECTED_APP_ASKING_FOR_ACCESS_2: () => LocalizedString
 	/**
-	 * {dappName} will no longer have access to:
-	 */
-	DISCONNECTED_APP_ASKING_FOR_ACCESS: (arg: { dappName: string }) => LocalizedString
-	/**
-	 * No longer request transactions to send to VeChain Thor.
-	 */
-	DISCONNECTED_APP_ASKING_FOR_ACCESS_1: () => LocalizedString
-	/**
-	 * No longer request your signature on certificates or identification and agreements.
-	 */
-	DISCONNECTED_APP_ASKING_FOR_ACCESS_2: () => LocalizedString
-	/**
 	 * You have previously connected to {dapp} with a different account. Do you want to switch to the account connected to {dapp}?
 	 */
 	CONNECTED_APP_CHANGE_ACCOUNT_REQUEST_DESCRIPTION: (arg: { dapp: string }) => LocalizedString
@@ -7202,10 +7217,6 @@ Thanks for your patience!
 	 * Connection requests
 	 */
 	CONNECTED_APP_CONNECTION_REQUESTS: () => LocalizedString
-	/**
-	 * Disconnection request
-	 */
-	DISCONNECTION_REQUEST: () => LocalizedString
 	/**
 	 * Contract
 	 */
@@ -7574,6 +7585,22 @@ Thanks for your patience!
 	 * Device Pin
 	 */
 	DEVICE_PIN: () => LocalizedString
+	/**
+	 * {dappName} will no longer have access to:
+	 */
+	DISCONNECTED_APP_ASKING_FOR_ACCESS: (arg: { dappName: string }) => LocalizedString
+	/**
+	 * No longer request transactions to send to VeChain Thor.
+	 */
+	DISCONNECTED_APP_ASKING_FOR_ACCESS_1: () => LocalizedString
+	/**
+	 * No longer request your signature on certificates or identification and agreements.
+	 */
+	DISCONNECTED_APP_ASKING_FOR_ACCESS_2: () => LocalizedString
+	/**
+	 * Disconnection request
+	 */
+	DISCONNECTION_REQUEST: () => LocalizedString
 	/**
 	 * Make your own DApp on Vechain
 	 */
@@ -10000,6 +10027,22 @@ Here's what it's all about:
 	 */
 	VALUE_TITLE: () => LocalizedString
 	/**
+	 * contribute to the offset
+	 */
+	VBD_CONTRIBUTE_OFFSET: () => LocalizedString
+	/**
+	 * get rewarded for your actions and
+	 */
+	VBD_GET_REWARDED: () => LocalizedString
+	/**
+	 * Join the
+	 */
+	VBD_JOIN_THE: () => LocalizedString
+	/**
+	 * movement
+	 */
+	VBD_MOVEMENT: () => LocalizedString
+	/**
 	 * Rewards earned
 	 */
 	VBD_REWARDS_EARNED: () => LocalizedString
@@ -10008,17 +10051,21 @@ Here's what it's all about:
 	 */
 	VBD_SEE_PROFILE: () => LocalizedString
 	/**
+	 * Start your impact
+	 */
+	VBD_START_YOUR_IMPACT: () => LocalizedString
+	/**
+	 * Avoided
+	 */
+	VBD_STAT_AVOIDED: () => LocalizedString
+	/**
 	 * CO2
 	 */
 	VBD_STAT_CO2: () => LocalizedString
 	/**
-	 * Trees
+	 * Conserved
 	 */
-	VBD_STAT_TREES: () => LocalizedString
-	/**
-	 * Water
-	 */
-	VBD_STAT_WATER: () => LocalizedString
+	VBD_STAT_CONSERVED: () => LocalizedString
 	/**
 	 * Energy
 	 */
@@ -10036,21 +10083,29 @@ Here's what it's all about:
 	 */
 	VBD_STAT_SAVED: () => LocalizedString
 	/**
-	 * Conserved
+	 * Trees
 	 */
-	VBD_STAT_CONSERVED: () => LocalizedString
+	VBD_STAT_TREES: () => LocalizedString
 	/**
-	 * Avoided
+	 * Water
 	 */
-	VBD_STAT_AVOIDED: () => LocalizedString
+	VBD_STAT_WATER: () => LocalizedString
 	/**
-	 * Your offset
+	 * Total Better actions
 	 */
-	VBD_YOUR_OFFSET: () => LocalizedString
+	VBD_TOTAL_ACTIONS: () => LocalizedString
+	/**
+	 * Total B3TR rewarded
+	 */
+	VBD_TOTAL_REWARDED: () => LocalizedString
 	/**
 	 * Your Better Actions
 	 */
 	VBD_YOUR_BETTER_ACTIONS: () => LocalizedString
+	/**
+	 * Your offset
+	 */
+	VBD_YOUR_OFFSET: () => LocalizedString
 	/**
 	 * Vechain news & updates
 	 */
@@ -10068,13 +10123,13 @@ Here's what it's all about:
 	 */
 	VEWORLD: () => LocalizedString
 	/**
-	 * View on explorer
-	 */
-	VIEW_ON_EXPLORER: () => LocalizedString
-	/**
 	 * View only
 	 */
 	VIEW_ONLY: () => LocalizedString
+	/**
+	 * View on explorer
+	 */
+	VIEW_ON_EXPLORER: () => LocalizedString
 	/**
 	 * wallets saved on Google Drive.
 	 */
