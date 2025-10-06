08c78ab7eb9f8812d30bceab92245ba499c15b7d
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-10-06 16:42:10 +0100

    feat(theme): fix bacground awareness

--- src/Screens/Flows/App/AppsScreen/AppsScreen.tsx
+++ src/Screens/Flows/App/AppsScreen/AppsScreen.tsx
@@ -64,7 +64,7 @@ export const AppsScreen = () => {
 
     return (
         <Layout
-            bg={theme.isDark ? COLORS.DARK_PURPLE : COLORS.WHITE}
+            bg={theme.colors.background}
             noBackButton
             noMargin
             fixedHeader={
