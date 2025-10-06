5dffadf3362b91a99124419486913844058924e5
Marco Salomone <61231210+HiiiiD@users.noreply.github.com>
2025-10-06 19:30:33 +0200

    390 balance   update all avatar logos to be circular (#3363)
    * fix: make all icons rounded
    
    * fix: migrate address send screen
    
    * fix: cleanup
    
    * feat: add ledger logo
    
    * fix: fix ledger
    
    * fix: remove useless bg
    
    * fix: fix broken import
    
    * fix: fix a couple of issues

--- src/Components/Reusable/Account/AccountIcon.tsx
+++ src/Components/Reusable/Account/AccountIcon.tsx
@@ -1,19 +1,33 @@
 import React, { memo } from "react"
 import { StyleSheet, ViewProps } from "react-native"
 import { SvgXml } from "react-native-svg"
+import { NewLedgerLogo } from "~Assets"
 import { BaseView } from "~Components/Base"
+import { COLORS, ColorThemeType } from "~Constants"
+import { useThemedStyles } from "~Hooks"
+import { Device, DEVICE_TYPE } from "~Model"
 import { PicassoUtils } from "~Utils"
 
 type AccountIconProps = {
-    address: string
+    account: {
+        type?: DEVICE_TYPE
+        device?: Device
+        address: string
+    }
     size?: number
     borderRadius?: number
 }
 
-export const AccountIcon: React.FC<AccountIconProps> = memo(({ address, size, borderRadius }) => {
+export const AccountIcon: React.FC<AccountIconProps> = memo(({ account, size, borderRadius }) => {
+    const { theme, styles } = useThemedStyles(accountIconStyles)
     return (
-        <BaseView>
-            <PicassoAddressIcon address={address} size={size} borderRadius={borderRadius} />
+        <BaseView style={styles.container}>
+            <PicassoAddressIcon address={account.address} size={size} borderRadius={borderRadius} />
+            {(account.type === DEVICE_TYPE.LEDGER || account.device?.type === DEVICE_TYPE.LEDGER) && (
+                <BaseView borderRadius={99} p={4} style={styles.ledger}>
+                    <NewLedgerLogo width={8} height={8} color={theme.isDark ? COLORS.GREY_700 : COLORS.WHITE} />
+                </BaseView>
+            )}
         </BaseView>
     )
 })
@@ -25,7 +39,7 @@ type PicassoAddressIconProps = {
 } & ViewProps
 
 export const PicassoAddressIcon: React.FC<PicassoAddressIconProps> = memo(
-    ({ address, size = 50, borderRadius = 8, style, ...otherProps }) => {
+    ({ address, size = 50, borderRadius = 99, style, ...otherProps }) => {
         const uri = PicassoUtils.getPicassoImgSrc(address).toString()
 
         return (
@@ -39,3 +53,15 @@ export const PicassoAddressIcon: React.FC<PicassoAddressIconProps> = memo(
 const picassoIconStyles = StyleSheet.create({
     view: { overflow: "hidden" },
 })
+
+const accountIconStyles = (theme: ColorThemeType) =>
+    StyleSheet.create({
+        container: { position: "relative" },
+        ledger: {
+            position: "absolute",
+            bottom: -2,
+            right: -2,
+            zIndex: 1,
+            backgroundColor: theme.isDark ? COLORS.GREY_100 : COLORS.GREY_700,
+        },
+    })
