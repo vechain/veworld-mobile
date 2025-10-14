c90d1f02d8e460d45bdc8d820d02968727c92740
HiiiiD <salomone.marco@outlook.it>
2025-10-14 19:25:52 +0200

    fix: trigger CI

--- src/Hooks/useFormatFiat/useFormatFiat.tsx
+++ src/Hooks/useFormatFiat/useFormatFiat.tsx
@@ -69,6 +69,7 @@ export const useFormatFiat = (intlOptions?: FormatFiatConfig) => {
                 // Move symbol after
                 res = `${formattedAmount} ${symbol}`
             } else {
+                // Move symbol before
                 res = `${symbol}${formattedAmount}`
             }
 
