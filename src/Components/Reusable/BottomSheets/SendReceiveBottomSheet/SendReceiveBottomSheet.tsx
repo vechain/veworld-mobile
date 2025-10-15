450ba599d0260da1d586c5d3ce1728cf371a494b
Michael Martin <michael.martin1997@gmail.com>
2025-10-15 17:24:05 +0200

    feat: detection working on ios

--- src/Components/Reusable/BottomSheets/SendReceiveBottomSheet/SendReceiveBottomSheet.tsx
+++ src/Components/Reusable/BottomSheets/SendReceiveBottomSheet/SendReceiveBottomSheet.tsx
@@ -1,15 +1,23 @@
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types"
-import { Canvas, Fill, Points, Skia, vec } from "@shopify/react-native-skia"
+import { Canvas, Circle, Fill, Path, Points, Rect, RoundedRect, Skia, vec } from "@shopify/react-native-skia"
 import { Camera } from "expo-camera"
-import React, { forwardRef, RefObject, useCallback, useEffect, useMemo, useState } from "react"
-import { LayoutChangeEvent, StyleSheet, Text, TouchableOpacity, View } from "react-native"
-import { useDerivedValue, useSharedValue } from "react-native-reanimated"
+import React, { forwardRef, RefObject, useCallback, useEffect, useMemo, useRef, useState } from "react"
+import {
+    LayoutChangeEvent,
+    PixelRatio,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    useWindowDimensions,
+    View,
+} from "react-native"
+import Animated, { runOnJS, useDerivedValue, useSharedValue } from "react-native-reanimated"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
-import { Camera as RNVCamera, useCameraDevice, useCodeScanner } from "react-native-vision-camera"
+import { Code, DrawableFrame, Camera as RNVCamera, useCameraDevice, useCodeScanner } from "react-native-vision-camera"
 import { BaseBottomSheet, BaseIcon, BaseSpacer, BaseText, BaseView } from "~Components/Base"
 import { BaseTabs } from "~Components/Base/BaseTabs"
 import { WalletConnectIcon } from "~Components/Reusable/WalletConnectIconSvg"
-import { COLORS } from "~Constants"
+import { COLORS, SCREEN_HEIGHT, SCREEN_WIDTH } from "~Constants"
 import { useAppState, useBottomSheetModal, useCameraPermissions, useThemedStyles } from "~Hooks"
 import { useQrScanDetection } from "~Hooks/useQrScanDetection"
 import { useI18nContext } from "~i18n"
@@ -188,6 +196,8 @@ const SendReceiveBottomSheetContent = <TTabs extends SendReceiveBsTab[] | readon
         labels,
     ])
 
+    const pR = useSharedValue(PixelRatio.get())
+
     const device = useCameraDevice("back")
     const offsetX = useDerivedValue(() => rootX.value + cameraX.value, [rootX.value, cameraX.value])
     const offsetY = useDerivedValue(() => rootY.value + cameraY.value, [rootY.value, cameraY.value])
@@ -199,9 +209,27 @@ const SendReceiveBottomSheetContent = <TTabs extends SendReceiveBsTab[] | readon
         [onClose, onScan],
     )
 
-    const points = useSharedValue<{ x: number; y: number }[]>([])
+    const insets = useSafeAreaInsets()
 
-    const onCodeScanned = useQrScanDetection({ offsetX, offsetY, size: 200, onScan: handleScan, points })
+    const points = useSharedValue<{ d: string }[]>([])
+
+    const [codes, setCodes] = useState<Code[]>([])
+    const [frame, setFrame] = useState<DrawableFrame>(null!)
+
+    const sharedCodes = useSharedValue<Code[]>([])
+    const sharedFrame = useSharedValue<DrawableFrame>(undefined!)
+
+    const onCodeScanned = useQrScanDetection({
+        offsetX,
+        offsetY,
+        size: 200,
+        onScan: handleScan,
+        points,
+        codes: sharedCodes,
+        frame: sharedFrame,
+        setCodes,
+        setFrame,
+    })
 
     const codeScanner = useCodeScanner({
         codeTypes: ["qr"],
@@ -214,8 +242,39 @@ const SendReceiveBottomSheetContent = <TTabs extends SendReceiveBsTab[] | readon
         return Skia.RRectXY(Skia.XYWHRect(offsetX.value, offsetY.value, 200, 200), 16, 16)
     })
 
+    const derivedCodeFrame = useDerivedValue(() => {
+        const frames: { x: string; y: string }[] = sharedCodes.value.map(code => {
+            if (code.frame === undefined) return { x: "0%", y: "0%" }
+            return {
+                x:
+                    (parseFloat(
+                        `${100 - ((code.frame.y + code.frame.width / 2) / sharedFrame.value?.height || 0) * 100}`,
+                    ) /
+                        100) *
+                        SCREEN_WIDTH -
+                    100,
+                y:
+                    (parseFloat(`${((code.frame.x + code.frame.height / 2) / (sharedFrame.value?.width || 0)) * 100}`) /
+                        100) *
+                        (SCREEN_HEIGHT - insets.bottom) -
+                    100,
+            }
+            // return {
+            //     x: 100 - ((code.frame.y + code.frame.width / 2) / sharedFrame.value.height) * SCREEN_HEIGHT + 100,
+            //     y: ((code.frame.x + code.frame.height / 2) / sharedFrame.value.width) * SCREEN_WIDTH + 100,
+            // }
+        })
+        console.log("frames", frames)
+        return frames
+    }, [sharedCodes.value, sharedFrame.value, insets.bottom])
+
     const pointsV = useDerivedValue(() => {
-        return points.value.map(p => vec(p.x, p.y))
+        console.log("points", points.value)
+        console.log("W", SCREEN_WIDTH)
+        console.log("H", SCREEN_HEIGHT)
+
+        return points.value.map(p => p.d)
+        // return points.value.map(p => vec(p.x / 2, p.y / 2))
         // return [vec(100, 100), vec(150, 100), vec(150, 150), vec(100, 150)]
     })
 
@@ -231,9 +290,28 @@ const SendReceiveBottomSheetContent = <TTabs extends SendReceiveBsTab[] | readon
             )}
 
             <Canvas style={StyleSheet.absoluteFill}>
-                <Points points={pointsV} style="fill" strokeWidth={2} color={"green"} mode="polygon" />
                 <Fill color={COLORS.BALANCE_BACKGROUND_80} clip={skiaClip} invertClip />
             </Canvas>
+
+            {derivedCodeFrame.value.map((code, index) => {
+                // if (code.frame === undefined) return <></>
+                return (
+                    <Animated.View
+                        key={"scanner-focus-no" + index}
+                        style={{
+                            left: code.x,
+                            top: code.y,
+                            position: "absolute",
+                            color: COLORS.GREEN_600,
+                            borderWidth: 1,
+                            borderColor: "red",
+                            width: 200,
+                            height: 200,
+                            borderRadius: 16,
+                        }}
+                    />
+                )
+            })}
             <BaseView style={[StyleSheet.absoluteFill, styles.cameraChildren]}>{children}</BaseView>
         </BaseView>
     ) : (
--- src/Hooks/useQrScanDetection/useQrScanDetection.ts
+++ src/Hooks/useQrScanDetection/useQrScanDetection.ts
@@ -1,6 +1,7 @@
 import { useCallback } from "react"
+import { PixelRatio } from "react-native"
 import { SharedValue } from "react-native-reanimated"
-import { Code } from "react-native-vision-camera"
+import { Code, DrawableFrame, runAtTargetFps } from "react-native-vision-camera"
 
 type Args = {
     /**
@@ -19,40 +20,65 @@ type Args = {
      * Function triggered when the scan is successful
      */
     onScan: (data: string) => Promise<void>
-    points: SharedValue<{ x: number; y: number }[]>
+    points: SharedValue<{ d: string }[]>
+    codes: SharedValue<Code[]>
+    frame: SharedValue<DrawableFrame>
+    setCodes: (codes: Code[]) => void
+    setFrame: (frame: DrawableFrame) => void
 }
 
-export const useQrScanDetection = ({ offsetX, offsetY, size, onScan, points: _points }: Args) => {
+export const useQrScanDetection = ({
+    offsetX,
+    offsetY,
+    size,
+    onScan,
+    points: _points,
+    codes: _codes,
+    frame: _frame,
+    setCodes: _setCodes,
+    setFrame: _setFrame,
+}: Args) => {
     return useCallback(
-        async (codes: Code[]) => {
+        async (codes: Code[], frame: DrawableFrame) => {
             if (codes.length === 0) return
-            //We'll read just the first code
-            const code = codes[0]
-            if (!code.frame || !code.value) return
-            const points = [
-                //Top left
-                {
-                    x: code.frame.x - code.frame.width / 2,
-                    y: code.frame.y + code.frame.height / 5,
-                },
-                //Top right
-                {
-                    x: code.frame.x + code.frame.width,
-                    y: code.frame.y + code.frame.height / 5,
-                },
-                //Bottom right
-                {
-                    x: code.frame.x + code.frame.width,
-                    y: code.frame.y + code.frame.height * 1.5,
-                },
-                //Bottom left
-                {
-                    x: code.frame.x - code.frame.width / 2,
-                    y: code.frame.y + code.frame.height * 1.5,
-                },
-            ]
+            if (codes[0].frame) {
+                _codes.value = codes
+                _frame.value = frame
+                _setCodes(codes)
+                _setFrame(frame)
+                const codesFrame = codes[0].frame
+                const topLeftX = codesFrame.x
+                const topLeftY = codesFrame.y
+                const topRightX = codesFrame.x + codesFrame.width
+                const topRightY = codesFrame.y
+                const bottomRightX = codesFrame.x + codesFrame.width
+                const bottomRightY = codesFrame.y + codesFrame.height
+                const bottomLeftX = codesFrame.x
+                const bottomLeftY = codesFrame.y + codesFrame.height
 
-            _points.value = [...points]
+                _points.value = [
+                    {
+                        d: `M${topLeftX + codesFrame.width / 3} ${topLeftY} H${topLeftX} V${
+                            topLeftY + codesFrame.height / 3
+                        }`,
+                    },
+                    {
+                        d: `M${topRightX - codesFrame.width / 3} ${topRightY} H${topRightX} V${
+                            topRightY + codesFrame.height / 3
+                        }`,
+                    },
+                    {
+                        d: `M${bottomRightX - codesFrame.width / 3} ${bottomRightY} H${bottomRightX} V${
+                            bottomRightY - codesFrame.height / 3
+                        }`,
+                    },
+                    {
+                        d: `M${bottomLeftX + codesFrame.width / 3} ${bottomLeftY} H${bottomLeftX} V${
+                            bottomLeftY - codesFrame.height / 3
+                        }`,
+                    },
+                ]
+            }
 
             const biggerRect = {
                 x: offsetX.value * 0.8,
@@ -62,19 +88,19 @@ export const useQrScanDetection = ({ offsetX, offsetY, size, onScan, points: _po
             }
 
             //Outside of detection area
-            if (
-                points.every(point => {
-                    return (
-                        point.x >= biggerRect.x &&
-                        point.x <= biggerRect.x + biggerRect.width &&
-                        point.y >= biggerRect.y &&
-                        point.y <= biggerRect.y + biggerRect.height
-                    )
-                })
-            ) {
-                return onScan(code.value)
-            }
+            // if (
+            //     points.every(point => {
+            //         return (
+            //             point.x >= biggerRect.x &&
+            //             point.x <= biggerRect.x + biggerRect.width &&
+            //             point.y >= biggerRect.y &&
+            //             point.y <= biggerRect.y + biggerRect.height
+            //         )
+            //     })
+            // ) {
+            //     return onScan(code.value)
+            // }
         },
-        [_points, offsetX.value, offsetY.value, onScan, size],
+        [_points, offsetX.value, offsetY.value, size, _setCodes, _setFrame],
     )
 }
