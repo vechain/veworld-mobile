7b6f9e5ec5029864a6ebe69214d6f605542827dd
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-10-22 14:05:23 +0100

    perf(hooks): optimize useNftBookmarking callback stability
    Read favorite state at execution time instead of closure
    to prevent callback recreation on every toggle

--- src/Hooks/useNftBookmarking/useNftBookmarking.ts
+++ src/Hooks/useNftBookmarking/useNftBookmarking.ts
@@ -1,4 +1,5 @@
 import { useCallback, useMemo } from "react"
+import { useStore } from "react-redux"
 import { AnalyticsEvent } from "~Constants"
 import { useAnalyticTracking } from "~Hooks"
 import {
@@ -9,15 +10,19 @@ import {
     useAppDispatch,
     useAppSelector,
 } from "~Storage/Redux"
+import { RootState } from "~Storage/Redux/Types"
 
 export const useNftBookmarking = (address: string, tokenId: string) => {
     const dispatch = useAppDispatch()
+    const store = useStore()
     const track = useAnalyticTracking()
     const selectedAccount = useAppSelector(selectSelectedAccount)
     const selectedNetwork = useAppSelector(selectSelectedNetwork)
     const isFavorite = useAppSelector(state => isNftFavorite(state, address, tokenId))
 
     const toggleFavorite = useCallback(() => {
+        const currentIsFavorite = isNftFavorite(store.getState() as RootState, address, tokenId)
+
         dispatch(
             toggleFavoriteAction({
                 address,
@@ -27,12 +32,12 @@ export const useNftBookmarking = (address: string, tokenId: string) => {
             }),
         )
 
-        track(isFavorite ? AnalyticsEvent.NFT_FAVORITE_REMOVED : AnalyticsEvent.NFT_FAVORITE_ADDED, {
+        track(currentIsFavorite ? AnalyticsEvent.NFT_FAVORITE_REMOVED : AnalyticsEvent.NFT_FAVORITE_ADDED, {
             address,
             tokenId,
             network: selectedNetwork.name,
         })
-    }, [dispatch, address, tokenId, selectedAccount.address, selectedNetwork, track, isFavorite])
+    }, [store, dispatch, address, tokenId, selectedAccount.address, selectedNetwork, track])
 
     return useMemo(
         () => ({
