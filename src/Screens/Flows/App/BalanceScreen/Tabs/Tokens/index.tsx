8c65b30ad68b98b0d61cd062df213f7ec6518669
HiiiiD <salomone.marco@outlook.it>
2025-10-23 09:22:57 +0200

    fix: fix carousel spacing

--- src/Screens/Flows/App/BalanceScreen/Tabs/Tokens/index.tsx
+++ src/Screens/Flows/App/BalanceScreen/Tabs/Tokens/index.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { BaseSpacer, BaseView } from "~Components"
+import { SCREEN_WIDTH } from "~Constants"
+import { useIsVeBetterUser } from "~Hooks/useIsVeBetterUser"
+import { useShareVeBetterCard } from "~Hooks/useShareVeBetterCard"
+import { BannersCarousel } from "../../Components/BannerCarousel"
+import { AddTokensCard } from "../../Components/Tokens/AddTokensCard"
+import { VeBetterDaoActionGroup } from "../../Components/VeBetterDao/VeBetterDaoActionGroup"
 import { VeBetterDaoCard } from "../../Components/VeBetterDao/VeBetterDaoCard"
 import { BalanceActivity } from "../Activity/BalanceActivity"
 import { TokensTopSection } from "./TokensTopSection"
-import { AddTokensCard } from "../../Components/Tokens/AddTokensCard"
-import { BannersCarousel } from "../../Components/BannerCarousel"
-import { SCREEN_WIDTH } from "~Constants"
-import { VeBetterDaoActionGroup } from "../../Components/VeBetterDao/VeBetterDaoActionGroup"
-import { useShareVeBetterCard } from "~Hooks/useShareVeBetterCard"
-import { useIsVeBetterUser } from "~Hooks/useIsVeBetterUser"
 
 type Props = {
     isEmptyStateShown?: boolean
@@ -26,8 +26,7 @@ export const Tokens = ({ isEmptyStateShown = false }: Props) => {
             <BalanceActivity tab="TOKENS" />
             {!isEmptyStateShown && (
                 <>
-                    <BaseSpacer height={40} />
-                    <BannersCarousel location="home_screen" baseWidth={SCREEN_WIDTH - 48} padding={0} />
+                    <BannersCarousel location="home_screen" baseWidth={SCREEN_WIDTH - 48} padding={0} mt={40} />
 
                     {isVeBetterUser && (
                         <>
