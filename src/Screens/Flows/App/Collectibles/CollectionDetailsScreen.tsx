9d4303477fbc9751588d9fa59de875636d190790
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-10-24 10:02:15 +0100

    feat(collectibles): improve CollectionDetailsScreen UX

--- src/Screens/Flows/App/Collectibles/CollectionDetailsScreen.tsx
+++ src/Screens/Flows/App/Collectibles/CollectionDetailsScreen.tsx
@@ -1,6 +1,7 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import React, { useCallback, useMemo, useState } from "react"
-import { BaseView, BaseSpacer, Layout, BaseText } from "~Components"
+import { BaseView, BaseSpacer, Layout, BaseText, BaseSkeleton } from "~Components"
+import { useTheme } from "~Hooks"
 import { useI18nContext } from "~i18n"
 import { Routes } from "~Navigation"
 import { RootStackParamListHome } from "~Navigation/Stacks/HomeStack"
@@ -17,10 +18,13 @@ type Props = NativeStackScreenProps<RootStackParamListHome, Routes.COLLECTIBLES_
 
 export const CollectionDetailsScreen: React.FC<Props> = ({ route }) => {
     const { LL } = useI18nContext()
+    const theme = useTheme()
     const [viewMode, setViewMode] = useState<CollectiblesViewMode>("GALLERY")
     const [nftCount, setNftCount] = useState<number>(0)
     const selectedAccount = useAppSelector(selectSelectedAccount)
-    const { data: collectionMetadata } = useCollectionMetadata(route.params.collectionAddress)
+    const { data: collectionMetadata, isLoading: isLoadingCollectionMetadata } = useCollectionMetadata(
+        route.params.collectionAddress,
+    )
 
     const isObservedAccount = useMemo(() => {
         return AccountUtils.isObservedAccount(selectedAccount)
@@ -37,25 +41,48 @@ export const CollectionDetailsScreen: React.FC<Props> = ({ route }) => {
     const collectionName = collectionMetadata?.name || LL.BD_COLLECTION()
     const countLabel = nftCount === 1 ? LL.COMMON_COLLECTIBLE() : LL.COMMON_COLLECTIBLES()
 
+    const renderCountLabel = useMemo(() => {
+        if (isLoadingCollectionMetadata)
+            return (
+                <BaseSkeleton
+                    height={22}
+                    width={100}
+                    boneColor={theme.colors.skeletonBoneColor}
+                    highlightColor={theme.colors.skeletonHighlightColor}
+                />
+            )
+        return (
+            <BaseText typographyFont="bodyMedium">
+                {nftCount} {countLabel}
+            </BaseText>
+        )
+    }, [
+        nftCount,
+        countLabel,
+        isLoadingCollectionMetadata,
+        theme.colors.skeletonBoneColor,
+        theme.colors.skeletonHighlightColor,
+    ])
+
     return (
         <Layout
-            title={collectionName}
+            title={isLoadingCollectionMetadata ? LL.BD_COLLECTION() : collectionName}
             safeAreaTestID="Collection_Details_Screen"
             fixedBody={
-                <BaseView flex={1} px={16} pt={16}>
-                    <ChangeCollectionsListView selectedView={viewMode} onViewChange={handleViewChange} />
-                    <BaseSpacer height={24} />
-                    <BaseText typographyFont="bodyMedium">
-                        {nftCount} {countLabel}
-                    </BaseText>
-                    <BaseSpacer height={24} />
-                    <CollectionsScreen
-                        collectionAddress={route.params.collectionAddress}
-                        viewMode={viewMode}
-                        isObservedAccount={isObservedAccount}
-                        onNftCountChange={handleNftCountChange}
-                    />
-                </BaseView>
+                !isLoadingCollectionMetadata && (
+                    <BaseView flex={1} px={16} pt={16}>
+                        <ChangeCollectionsListView selectedView={viewMode} onViewChange={handleViewChange} />
+                        <BaseSpacer height={24} />
+                        <BaseText typographyFont="bodyMedium">{renderCountLabel}</BaseText>
+                        <BaseSpacer height={24} />
+                        <CollectionsScreen
+                            collectionAddress={route.params.collectionAddress}
+                            viewMode={viewMode}
+                            isObservedAccount={isObservedAccount}
+                            onNftCountChange={handleNftCountChange}
+                        />
+                    </BaseView>
+                )
             }
         />
     )
