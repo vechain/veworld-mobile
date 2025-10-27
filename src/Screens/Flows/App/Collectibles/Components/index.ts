306e62481ce5c1fff9ec617f5109f3d8ece5afe7
Michael Martin <michael.martin1997@gmail.com>
2025-10-27 14:59:26 +0100

    refactor(collectibles): remove SortCollectiblesBottomSheet

--- src/Screens/Flows/App/Collectibles/Components/index.ts
+++ src/Screens/Flows/App/Collectibles/Components/index.ts
@@ -1,4 +1,3 @@
 export * from "./CollectionCard"
 export * from "./CollectionNftsList"
-export * from "./SortCollectiblesBottomSheet"
 export * from "./SkeletonCollectionCard"
--- src/Screens/Flows/App/Collectibles/Hooks/useCollectionMetadata.spec.ts
+++ src/Screens/Flows/App/Collectibles/Hooks/useCollectionMetadata.spec.ts
@@ -3,14 +3,30 @@ import { useCollectionMetadata } from "./useCollectionMetadata"
 import { TestWrapper } from "~Test"
 
 describe("useCollectionMetadata", () => {
-    beforeEach(() => {
-        jest.clearAllMocks()
-    })
-
     it("should fetch collection metadata correctly", async () => {
-        const { result, waitFor } = renderHook(() => useCollectionMetadata("0x0"), {
-            wrapper: TestWrapper,
-        })
+        const { result, waitFor } = renderHook(
+            () => useCollectionMetadata("0xfc32a9895c78ce00a1047d602bd81ea8134cc32b"),
+            {
+                wrapper: TestWrapper,
+                initialProps: {
+                    preloadedState: {
+                        accounts: {
+                            accounts: [
+                                {
+                                    address: "0xf077b491b355e64048ce21e3a6fc4751eeea77fa",
+                                    alias: "Test Account",
+                                    index: 0,
+                                    rootAddress: "0x90d70a5d0e9ce28336f7d45990b9c63c0a4142g0",
+                                    visible: true,
+                                },
+                            ],
+                            selectedAccount: "0xf077b491b355e64048ce21e3a6fc4751eeea77fa",
+                        },
+                    },
+                },
+            },
+        )
+
         await waitFor(() => {
             expect(result.current.data).toBeDefined()
         })
