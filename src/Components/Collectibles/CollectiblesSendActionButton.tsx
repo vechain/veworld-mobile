4768ff63651427f3aa4c7f7444fd7b5898cfe929
Marco Salomone <61231210+HiiiiD@users.noreply.github.com>
2025-10-28 16:39:57 +0100

    fix: collapse bs on send clicked (#3466)
    * fix: collapse bs on send clicked
    
    * fix: fix ts checks

--- src/Components/Collectibles/CollectiblesSendActionButton.tsx
+++ src/Components/Collectibles/CollectiblesSendActionButton.tsx
@@ -7,18 +7,20 @@ import { CollectiblesActionButton } from "./CollectiblesActionButton"
 type Props = {
     address: string
     tokenId: string
+    onClose: () => void
 }
-export const CollectiblesSendActionButton = ({ address, tokenId }: Props) => {
+export const CollectiblesSendActionButton = ({ address, tokenId, onClose }: Props) => {
     const { LL } = useI18nContext()
 
     const nav = useNavigation()
 
     const onPress = useCallback(async () => {
+        onClose()
         nav.navigate(Routes.INSERT_ADDRESS_SEND, {
             contractAddress: address,
             tokenId,
         })
-    }, [address, nav, tokenId])
+    }, [address, nav, onClose, tokenId])
 
     return (
         <CollectiblesActionButton
