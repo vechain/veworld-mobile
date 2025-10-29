d7e95ac5398bf5226f4579b9bc153d9d35b1e7b8
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-10-29 15:30:28 +0000

    feat(i18n): add translation keys

--- src/i18n/i18n-types.ts
+++ src/i18n/i18n-types.ts
@@ -1843,6 +1843,14 @@ type RootTranslation = {
 	 */
 	COMMON_24H_VOLUME: string
 	/**
+	 * 2​4​h​ ​R​a​n​g​e
+	 */
+	COMMON_24H_PRICE_RANGE: string
+	/**
+	 * T​o​k​e​n​ ​s​t​a​t​s
+	 */
+	COMMON_TOKEN_STATS: string
+	/**
 	 * A​s​s​e​t​s
 	 */
 	COMMON_ASSETS: string
@@ -5013,6 +5021,14 @@ type RootTranslation = {
 	 */
 	TOKEN_CUSTOM_SUBTITLE: string
 	/**
+	 * A​c​t​i​v​i​t​y
+	 */
+	TOKEN_DETAIL_ACTIVITY_TAB: string
+	/**
+	 * B​a​l​a​n​c​e
+	 */
+	TOKEN_DETAIL_BALANCE_TAB: string
+	/**
 	 * T​o​k​e​n​ ​I​D
 	 */
 	TOKEN_ID: string
@@ -5056,14 +5072,6 @@ type RootTranslation = {
 	 * T​y​p​e
 	 */
 	TYPE: string
-	/**
-	 * B​a​l​a​n​c​e
-	 */
-	TOKEN_DETAIL_BALANCE_TAB: string
-	/**
-	 * A​c​t​i​v​i​t​y
-	 */
-	TOKEN_DETAIL_ACTIVITY_TAB: string
 	/**
 	 * ~​ ​{​s​e​c​o​n​d​s​|​t​o​S​e​c​o​n​d​s​D​u​r​a​t​i​o​n​}
 	 * @param {number} seconds
@@ -7204,6 +7212,14 @@ This action is irreversible
 	 * 24h Volume
 	 */
 	COMMON_24H_VOLUME: () => LocalizedString
+	/**
+	 * 24h Range
+	 */
+	COMMON_24H_PRICE_RANGE: () => LocalizedString
+	/**
+	 * Token stats
+	 */
+	COMMON_TOKEN_STATS: () => LocalizedString
 	/**
 	 * Assets
 	 */
@@ -10330,6 +10346,14 @@ Here's what it's all about:
 	 */
 	TOKEN_CUSTOM_SUBTITLE: () => LocalizedString
 	/**
+	 * Activity
+	 */
+	TOKEN_DETAIL_ACTIVITY_TAB: () => LocalizedString
+	/**
+	 * Balance
+	 */
+	TOKEN_DETAIL_BALANCE_TAB: () => LocalizedString
+	/**
 	 * Token ID
 	 */
 	TOKEN_ID: () => LocalizedString
@@ -10373,14 +10397,6 @@ Here's what it's all about:
 	 * Type
 	 */
 	TYPE: () => LocalizedString
-	/**
-	 * Balance
-	 */
-	TOKEN_DETAIL_BALANCE_TAB: () => LocalizedString
-	/**
-	 * Activity
-	 */
-	TOKEN_DETAIL_ACTIVITY_TAB: () => LocalizedString
 	/**
 	 * ~ {seconds|toSecondsDuration}
 	 */
