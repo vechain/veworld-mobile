0e90b08631cf35f263f6c10264f1b9a60229fe86
Jonathan Ferreira <jonathanferreira23@gmail.com>
2025-10-29 22:30:33 +0000

    refactor(asset-details): usage

--- src/Screens/Flows/App/AssetDetailScreenSheet/AssetDetailScreenSheet.tsx
+++ src/Screens/Flows/App/AssetDetailScreenSheet/AssetDetailScreenSheet.tsx
@@ -2,7 +2,12 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import React, { useMemo, useState } from "react"
 import { StyleSheet } from "react-native"
 import { LineChart } from "react-native-wagmi-charts"
-import { DEFAULT_LINE_CHART_DATA, getCoinGeckoIdBySymbol, useSmartMarketChart } from "~Api/Coingecko"
+import {
+    DEFAULT_LINE_CHART_DATA,
+    getCoinGeckoIdBySymbol,
+    useSmartMarketChart,
+    useTokenSocialLinks,
+} from "~Api/Coingecko"
 import { BaseSpacer, BaseText, BaseView, TokenSymbol } from "~Components"
 import { TokenImage } from "~Components/Reusable/TokenImage"
 import { ColorThemeType } from "~Constants"
@@ -24,6 +29,7 @@ export const AssetDetailScreenSheet = ({ route }: Props) => {
     const { token } = route.params
     const { styles, theme } = useThemedStyles(baseStyles)
     const currency = useAppSelector(selectCurrency)
+    const socialLinks = useTokenSocialLinks(token.tokenInfo) ?? {}
 
     const isCrossChainToken = useMemo(() => !!token.crossChainProvider, [token.crossChainProvider])
     const name = useTokenDisplayName(token)
@@ -80,7 +86,11 @@ export const AssetDetailScreenSheet = ({ route }: Props) => {
                 )}
             </LineChart.Provider>
             {token.symbol && (
-                <AssetStats tokenSymbol={token.symbol} tokenDescription={token.tokenInfo?.description?.en} />
+                <AssetStats
+                    tokenSymbol={token.symbol}
+                    tokenDescription={token.tokenInfo?.description?.en}
+                    socialLinks={socialLinks ?? {}}
+                />
             )}
         </AssetDetailScreenWrapper>
     )
