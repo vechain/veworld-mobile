f48872f0b5ca6c80c985db592aaedeb06e978783
HiiiiD <salomone.marco@outlook.it>
2025-11-05 13:19:03 +0100

    feat: fix status bar

--- src/Components/Base/BaseStatusBar.tsx
+++ src/Components/Base/BaseStatusBar.tsx
@@ -1,10 +1,11 @@
 import React, { memo, useMemo } from "react"
 import { StatusBar, StatusBarProps } from "react-native"
+import { useFeatureFlags } from "~Components/Providers"
+import { COLORS } from "~Constants"
 import { useTheme } from "~Hooks"
 import { Routes } from "~Navigation"
 import { selectCurrentScreen, useAppSelector } from "~Storage/Redux"
 import { computeBarStyle } from "./Helpers/ComputeBarStyle"
-import { COLORS } from "~Constants"
 
 type Props = {
     hero?: boolean
@@ -19,6 +20,7 @@ export const BaseStatusBar = memo((props: Props) => {
     const theme = useTheme()
 
     const routeName = useAppSelector(selectCurrentScreen)
+    const featureFlags = useFeatureFlags()
 
     const barStyle = useMemo(() => {
         if (!props.root) return computeBarStyle(props.hero, theme.isDark)
@@ -26,13 +28,22 @@ export const BaseStatusBar = memo((props: Props) => {
             case Routes.HOME:
             case Routes.APPS_SEARCH:
             case Routes.APPS_TABS_MANAGER:
-            case Routes.TOKEN_DETAILS:
             case Routes.BROWSER:
                 return "light-content"
+            case Routes.TOKEN_DETAILS:
+                return featureFlags?.betterWorldFeature?.balanceScreen?.tokens?.enabled
+                    ? "light-content"
+                    : computeBarStyle(props.hero, theme.isDark)
             default:
                 return computeBarStyle(props.hero, theme.isDark)
         }
-    }, [props.hero, props.root, routeName, theme.isDark])
+    }, [
+        featureFlags?.betterWorldFeature?.balanceScreen?.tokens?.enabled,
+        props.hero,
+        props.root,
+        routeName,
+        theme.isDark,
+    ])
 
     const backgroundColor = useMemo(() => {
         if (!props.root) return props.transparent ? theme.colors.transparent : theme.colors.background
@@ -43,11 +54,19 @@ export const BaseStatusBar = memo((props: Props) => {
             case Routes.BROWSER:
                 return COLORS.BALANCE_BACKGROUND
             case Routes.TOKEN_DETAILS:
-                return COLORS.BLACK
+                if (featureFlags?.betterWorldFeature?.balanceScreen?.tokens?.enabled) return COLORS.BLACK
+                return props.transparent ? theme.colors.transparent : theme.colors.background
             default:
                 return props.transparent ? theme.colors.transparent : theme.colors.background
         }
-    }, [props.root, props.transparent, routeName, theme.colors.background, theme.colors.transparent])
+    }, [
+        featureFlags?.betterWorldFeature?.balanceScreen?.tokens?.enabled,
+        props.root,
+        props.transparent,
+        routeName,
+        theme.colors.background,
+        theme.colors.transparent,
+    ])
 
     return <StatusBar translucent={props.hero} barStyle={barStyle} backgroundColor={backgroundColor} {...props} />
 })
