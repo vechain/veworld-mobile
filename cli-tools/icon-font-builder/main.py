28647b1be0442fcbdef7598fc4f1990cccaec2e4
paolampilla <paolampilla@gmail.com>
2025-11-06 17:07:07 +0100

    chore: icon font generator

--- cli-tools/icon-font-builder/main.py
+++ cli-tools/icon-font-builder/main.py
@@ -4,11 +4,15 @@ import argparse
 import json
 import os
 import collections
+import shutil
 
 CACHE_DIR = "./svg_cache"
 
 def generate_map_file(map_data: dict[str, int], output_dir: str):
-    content = "export const designSystemIconMap = {\n%s\n}" % ",\n".join(f"    \"{key}\": {value}" for key, value in collections.OrderedDict(sorted(map_data.items())).items())
+    items = [f"    \"{key}\": {value}" for key, value in collections.OrderedDict(sorted(map_data.items())).items()]
+    if items:
+        items[-1] = items[-1] + ","
+    content = "export const designSystemIconMap = {\n%s\n}" % ",\n".join(items)
     with open(f"{output_dir}/designSystemIconMap.ts", "w") as f:
         f.write(content)
         f.close()
@@ -39,8 +43,14 @@ def build_font(font_name: str, font_map: str, font_prefix: str, output_dir: str)
         glyph = font.createChar(icon["code"], icon["name"])
         glyph.importOutlines(f"{CACHE_DIR}/{icon['name']}.svg", simplify=False, accuracy=0.5)
     
+    temp_ttf_path = f"./{font_name}.ttf"
+    font.generate(temp_ttf_path, flags=["glyph-map-file"])
+    
+    output_ttf_path = f"{output_dir}/{font_name}.ttf"
+    os.makedirs(output_dir, exist_ok=True)
+    shutil.move(temp_ttf_path, output_ttf_path)
+    
     generate_map_file(map_data, output_dir)
-    font.generate(f"{output_dir}/{font_name}.ttf", flags=["glyph-map-file"])
 
 
 if __name__ == "__main__":
@@ -48,6 +58,6 @@ if __name__ == "__main__":
     parser.add_argument("--font-map", type=str, required=False, default="./DesignSystemIcons.json", help="Path to the font map JSON file")
     parser.add_argument("--font-name", type=str, required=False, default="DesignSystemIcons", help="Name of the font")
     parser.add_argument("--font-prefix", type=str, required=False, default="icon-", help="Prefix of the font")
-    parser.add_argument("--output-dir", type=str, required=False, default=".", help="Output directory")
+    parser.add_argument("--output-dir", type=str, required=False, default="../../src/Assets/Fonts/DesignSystemIcons", help="Output directory")
     args = parser.parse_args()
     build_font(args.font_name, args.font_map, args.font_prefix, args.output_dir)
