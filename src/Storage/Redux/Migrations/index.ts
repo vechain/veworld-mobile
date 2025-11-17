a98fcfa35624553b0b02488e4f9581741ed695a5
paolampilla <paolampilla@gmail.com>
2025-11-17 16:26:18 +0100

    chore: remove migration

--- src/Storage/Redux/Migrations/index.ts
+++ src/Storage/Redux/Migrations/index.ts
@@ -31,7 +31,6 @@ import { Migration30 } from "./Migration30"
 import { Migration31 } from "./Migration31"
 import { Migration32 } from "./Migration32"
 import { Migration33 } from "./Migration33"
-import { Migration34 } from "~Storage/Redux/Migrations/Migration34.ts"
 
 export const migrationUpdates: MigrationManifest = {
     2: state => Migration2(state),
@@ -66,5 +65,4 @@ export const migrationUpdates: MigrationManifest = {
     31: state => Migration31(state),
     32: state => Migration32(state),
     33: state => Migration33(state),
-    34: state => Migration34(state),
 }
