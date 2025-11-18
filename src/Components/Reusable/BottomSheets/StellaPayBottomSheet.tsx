b2a4d0cba1ee607e246ca8dd5edece807ea48ab4
Michael Martin <michael.martin1997@gmail.com>
2025-11-18 12:39:30 +0100

    refactor(StellaPayBottomSheet): replace browser navigation with tab navigation

--- src/Components/Reusable/BottomSheets/StellaPayBottomSheet.tsx
+++ src/Components/Reusable/BottomSheets/StellaPayBottomSheet.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject, useMemo } from "react"
+import React, { RefObject, useCallback, useMemo } from "react"
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types"
 import { BaseBottomSheet, BaseText, BaseView, BaseButton, BaseIcon } from "~Components/Base"
 import { StellaPayCard, StellaPayLogoSVG } from "~Assets"
@@ -9,15 +9,16 @@ import { StyleSheet } from "react-native"
 import { useI18nContext } from "~i18n"
 import { useNavigation } from "@react-navigation/native"
 import { RootStackParamListHome, Routes } from "~Navigation"
-import { useBottomSheetModal, useBrowserNavigation } from "~Hooks"
+import { useBottomSheetModal } from "~Hooks"
 import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { useBrowserTab } from "~Hooks/useBrowserTab"
 
 type Props = {}
 
 export const StellaPayBottomSheet = React.forwardRef<BottomSheetModalMethods, Props>(({}, ref) => {
     const { styles } = useThemedStyles(baseStyles)
     const { LL } = useI18nContext()
-    const { navigateToBrowser } = useBrowserNavigation()
+    const { navigateWithTab } = useBrowserTab()
     const nav = useNavigation<NativeStackNavigationProp<RootStackParamListHome>>()
 
     const { onClose } = useBottomSheetModal({ externalRef: ref as RefObject<BottomSheetModalMethods> })
@@ -43,6 +44,15 @@ export const StellaPayBottomSheet = React.forwardRef<BottomSheetModalMethods, Pr
             ] as const,
         [LL],
     )
+
+    const onClick = useCallback(async () => {
+        await navigateWithTab({
+            url: "https://vebetter.stellapay.io/",
+            title: "Stella Pay",
+            navigationFn: url => nav.navigate(Routes.BROWSER, { url, returnScreen: Routes.HOME }),
+        })
+        onClose()
+    }, [navigateWithTab, onClose, nav])
 
     return (
         <BaseBottomSheet ref={ref} snapPoints={["92%"]} backgroundStyle={{ backgroundColor: COLORS.PURPLE }}>
@@ -86,12 +96,7 @@ export const StellaPayBottomSheet = React.forwardRef<BottomSheetModalMethods, Pr
                     bgColor={COLORS.LIME_GREEN}
                     textColor={COLORS.PURPLE}
                     rightIcon={<BaseIcon name="icon-arrow-right" size={24} color={COLORS.PURPLE} />}
-                    action={() => {
-                        onClose()
-                        navigateToBrowser("https://vebetter.stellapay.io/", url =>
-                            nav.navigate(Routes.BROWSER, { url, returnScreen: Routes.HOME }),
-                        )
-                    }}>
+                    action={onClick}>
                     {LL.STELLA_PAY_BOTTOM_SHEET_GET_YOUR_FREE_CARD()}
                 </BaseButton>
             </BaseView>
