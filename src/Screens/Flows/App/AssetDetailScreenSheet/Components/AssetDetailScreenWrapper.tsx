ce637eb01b9f064c01116103f978361459dd4657
Michael Martin <michael.martin1997@gmail.com>
2025-11-21 18:30:56 +0100

    feat: custom draggable list

--- src/Screens/Flows/App/AssetDetailScreenSheet/Components/AssetDetailScreenWrapper.tsx
+++ src/Screens/Flows/App/AssetDetailScreenSheet/Components/AssetDetailScreenWrapper.tsx
@@ -1,7 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
-import React, { PropsWithChildren, useCallback, useEffect, useMemo, useRef, useState } from "react"
-import { ScrollView, StyleSheet, View } from "react-native"
-import { NestableScrollContainer } from "react-native-draggable-flatlist"
+import React, { PropsWithChildren, useCallback, useEffect, useMemo } from "react"
+import { StyleSheet, ViewProps } from "react-native"
 import { Gesture, GestureDetector } from "react-native-gesture-handler"
 import Animated, {
     Extrapolation,
@@ -9,10 +8,13 @@ import Animated, {
     runOnJS,
     useAnimatedReaction,
     useAnimatedStyle,
+    useDerivedValue,
     useSharedValue,
+    withDecay,
     withSpring,
     withTiming,
 } from "react-native-reanimated"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { BaseSafeArea } from "~Components"
 import { BaseBottomSheetHandle } from "~Components/Base/BaseBottomSheetHandle"
 import { COLORS, ColorThemeType, SCREEN_HEIGHT } from "~Constants"
@@ -28,15 +30,21 @@ const PADDING_BOTTOM = 32
 const DEFAULT_TRANSLATION = 16
 
 export const AssetDetailScreenWrapper = ({ children, handle = true }: Props) => {
-    const [scrollPosition, setScrollPosition] = useState(0)
+    // const [scrollPosition, setScrollPosition] = useState(0)
 
     const { styles, theme } = useThemedStyles(baseStyles)
 
     const nav = useNavigation()
 
     const height = useSharedValue(SCREEN_HEIGHT)
+    // const scrollHeight = useSharedValue(0)
     const translateY = useSharedValue(SCREEN_HEIGHT)
-    const touchStartY = useSharedValue(0)
+    // const touchStartY = useSharedValue(0)
+    const scrollY = useSharedValue(0)
+
+    // const isScrollableLocked = useDerivedValue(() => {
+    //     return scrollY.value > 0
+    // })
 
     const animatedS = useAnimatedStyle(() => {
         return {
@@ -71,39 +79,42 @@ export const AssetDetailScreenWrapper = ({ children, handle = true }: Props) => 
         },
     )
 
-    const gesture = useMemo(() => {
-        return Gesture.Pan()
-            .manualActivation(true)
-            .onTouchesDown(e => {
-                touchStartY.value = e.changedTouches[0].y
-            })
-            .onTouchesMove((e, state) => {
-                console.log("onTouchesMove", e.changedTouches[0].y - touchStartY.value)
-                if (e.changedTouches[0].y - touchStartY.value > 2) {
-                    console.log("activate")
-                    state.activate()
-                } else {
-                    console.log("fail")
-                    state.fail()
-                }
-            })
-            .onUpdate(v => {
-                translateY.value = Math.max(v.translationY, -DEFAULT_TRANSLATION) + DEFAULT_TRANSLATION
-                console.log("scrollPosition", scrollPosition)
-            })
-            .onEnd(e => {
-                "worklet"
-                // If more than 20%, then close the bottomsheet (navigate to previous page)
-                if (translateY.value >= height.value / 5 && scrollPosition === 0) {
-                    onClose()
-                    return
-                }
-                if ((e.translationY < 3 && scrollPosition === 0) || e.translationY > 0) {
-                    console.log("tolo", scrollPosition)
-                    translateY.value = withSpring(DEFAULT_TRANSLATION, { mass: 4, damping: 120, stiffness: 900 })
-                }
-            })
-    }, [height.value, touchStartY, translateY, scrollPosition, onClose])
+    const gesture = Gesture.Pan()
+        // .onBegin(e => {
+        //     console.log("onBegin")
+        // })
+        // .onStart(e => {
+        //     console.log("onStart", e.translationY)
+        //     touchStartY.value = e.translationY
+        // })
+        // .onChange(v => {
+        //     "worklet"
+        //     const lowestSnapPoint = DEFAULT_TRANSLATION
+
+        //     const negativeScrollableContentOffset = !isScrollableLocked.value ? scrollHeight.value * -1 : 0
+
+        //     const draggedPosition = touchStartY.value + v.translationY
+
+        //     const accumulatedDraggedPosition = draggedPosition + negativeScrollableContentOffset
+
+        //     const clampedPosition = clamp(accumulatedDraggedPosition, height.value, 0)
+        // })
+        .onUpdate(v => {
+            translateY.value = Math.max(v.translationY, -DEFAULT_TRANSLATION) + DEFAULT_TRANSLATION
+            // console.log("onUpdate", translateY.value)
+        })
+        .onEnd(() => {
+            "worklet"
+            // If more than 20%, then close the bottomsheet (navigate to previous page)
+            if (translateY.value >= height.value / 5 && scrollY.value === 0) {
+                onClose()
+                return
+            }
+
+            translateY.value = withSpring(DEFAULT_TRANSLATION, { mass: 4, damping: 120, stiffness: 900 })
+        })
+        .enabled(scrollY.value === 0)
+        .simultaneousWithExternalGesture(Gesture.Native().enabled(scrollY.value !== 0))
 
     useEffect(() => {
         const unsubscribe = nav.addListener("beforeRemove", e => {
@@ -122,6 +133,11 @@ export const AssetDetailScreenWrapper = ({ children, handle = true }: Props) => 
         }
     }, [height.value, nav, translateY])
 
+    useDerivedValue(() => {
+        // console.log("scrollY.value", height.value)
+        return scrollY.value
+    }, [scrollY.value, height.value])
+
     return (
         <BaseSafeArea grow={1} style={styles.safeArea} bg="transparent">
             <Animated.View
@@ -130,7 +146,7 @@ export const AssetDetailScreenWrapper = ({ children, handle = true }: Props) => 
                     nav.goBack()
                 }}
             />
-            <GestureDetector gesture={Gesture.Simultaneous(Gesture.Native(), gesture)}>
+            <GestureDetector gesture={gesture}>
                 <Animated.View
                     style={[styles.root, animatedS]}
                     onLayout={e => {
@@ -145,9 +161,8 @@ export const AssetDetailScreenWrapper = ({ children, handle = true }: Props) => 
                             style={styles.handle}
                         />
                     )}
-                    <NestableScrollContainer bounces={false} showsVerticalScrollIndicator={false}>
-                        {children}
-                    </NestableScrollContainer>
+
+                    <DraggableView>{children}</DraggableView>
                 </Animated.View>
             </GestureDetector>
         </BaseSafeArea>
@@ -175,5 +190,65 @@ const baseStyles = (theme: ColorThemeType) =>
             marginTop: 0,
             paddingTop: 16,
             paddingBottom: 16,
+            zIndex: 1,
         },
     })
+
+const DraggableView = ({
+    children,
+    enableContentPanning = true,
+
+    style,
+    ...props
+}: PropsWithChildren<{ enableContentPanning?: boolean } & ViewProps>) => {
+    const contentInitialOffset = useSharedValue(0)
+    const contentHeight = useSharedValue(0)
+    const initialposition = useSharedValue(0)
+    const translateY = useSharedValue(0)
+    const { bottom, top } = useSafeAreaInsets()
+
+    const containerAbsoluteHeight = useMemo(() => {
+        return SCREEN_HEIGHT - top - bottom
+    }, [top, bottom])
+
+    const clampedTranslateY = useDerivedValue(() => {
+        const scrollableAreaHeight = contentHeight.value - containerAbsoluteHeight
+
+        return Math.max(Math.min(translateY.value, 0), Math.min(0, -scrollableAreaHeight - contentInitialOffset.value))
+    })
+
+    const panningGesture = useMemo(() => {
+        return Gesture.Pan()
+            .enabled(enableContentPanning)
+            .runOnJS(false)
+            .shouldCancelWhenOutside(false)
+            .onStart(() => {
+                initialposition.value = clampedTranslateY.value
+            })
+            .onChange(e => {
+                translateY.value = e.translationY + initialposition.value
+            })
+            .onEnd(e => {
+                translateY.value = withDecay({ velocity: e.velocityY })
+            })
+    }, [enableContentPanning, initialposition, clampedTranslateY.value, translateY])
+
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateY: clampedTranslateY.value }],
+        }
+    }, [translateY.value])
+    return (
+        <GestureDetector gesture={panningGesture}>
+            <Animated.View
+                style={[animatedStyles, style]}
+                {...props}
+                onLayout={e => {
+                    contentHeight.value = e.nativeEvent.layout.height
+                    contentInitialOffset.value = e.nativeEvent.layout.y
+                }}>
+                {children}
+            </Animated.View>
+        </GestureDetector>
+    )
+}
