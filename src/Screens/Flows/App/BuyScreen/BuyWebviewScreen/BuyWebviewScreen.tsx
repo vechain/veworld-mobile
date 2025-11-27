aca7a8d7a22b6e136437298c4f41e4ffde76c307
HiiiiD <salomone.marco@outlook.it>
2025-11-27 14:44:43 +0100

    fix: fix colors

--- src/Screens/Flows/App/BuyScreen/BuyWebviewScreen/BuyWebviewScreen.tsx
+++ src/Screens/Flows/App/BuyScreen/BuyWebviewScreen/BuyWebviewScreen.tsx
@@ -1,12 +1,13 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import React, { useMemo } from "react"
 import { CoinbasePayWebView, Layout, TransakPayWebView } from "~Components"
+import { CoinifyPayWebView } from "~Components/Reusable/CoinifyPayWebView"
+import { useTheme } from "~Hooks"
+import { DEVICE_TYPE } from "~Model"
 import { RootStackParamListBuy, Routes } from "~Navigation"
 import { selectSelectedAccountAddress, selectSelectedAccountOrNull, useAppSelector } from "~Storage/Redux"
 import { PlatformUtils } from "~Utils"
 import { PaymentProvidersEnum } from "../Hooks"
-import { CoinifyPayWebView } from "~Components/Reusable/CoinifyPayWebView"
-import { DEVICE_TYPE } from "~Model"
 
 type Props = NativeStackScreenProps<RootStackParamListBuy, Routes.BUY_WEBVIEW>
 const isProd = process.env.NODE_ENV === "production"
@@ -17,6 +18,7 @@ export const BuyWebviewScreen: React.FC<Props> = ({ route }) => {
     const selectedAccountAddress = useAppSelector(selectSelectedAccountAddress)
     const selectedAccount = useAppSelector(selectSelectedAccountOrNull)
     const { provider, providerName } = route.params || {}
+    const theme = useTheme()
 
     const ifTest = useMemo(() => (isProd ? "" : " (STAGING)"), [])
     const isLedgerAccount = selectedAccount?.device?.type === DEVICE_TYPE.LEDGER
@@ -27,6 +29,7 @@ export const BuyWebviewScreen: React.FC<Props> = ({ route }) => {
         <Layout
             hasSafeArea={isAndroid}
             title={`${providerName}${ifTest}`}
+            bg={provider === PaymentProvidersEnum.Transak ? theme.colors.transak : undefined}
             fixedBody={
                 <>
                     {provider === PaymentProvidersEnum.CoinbasePay && !isLedgerAccount && (
