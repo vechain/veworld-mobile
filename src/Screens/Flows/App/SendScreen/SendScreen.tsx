70b8a5d0d4b6d39d3a730c6ebe9b019a503ece49
paolampilla <paolampilla@gmail.com>
2025-12-01 14:24:06 +0100

    chore: fix conflicts

--- src/Screens/Flows/App/SendScreen/SendScreen.tsx
+++ src/Screens/Flows/App/SendScreen/SendScreen.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useNavigation, useRoute } from "@react-navigation/native"
-import { useTheme } from "~Hooks"
+import { useTheme, useThemedStyles } from "~Hooks"
 import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack"
 import React, { ReactElement, useCallback, useMemo, useRef, useState } from "react"
+import { StyleSheet } from "react-native"
+import Animated, { EntryAnimationsValues, ExitAnimationsValues, useSharedValue } from "react-native-reanimated"
 import { BaseButton, BaseView, Layout } from "~Components"
 import { FungibleTokenWithBalance } from "~Model"
 import { RootStackParamListHome, Routes } from "~Navigation"
 import { useI18nContext } from "~i18n"
 import { CloseIconHeaderButton } from "~Components/Reusable/HeaderButtons"
 import { SelectAmountSendComponent } from "./02-SelectAmountSendScreen/SelectAmountSendComponent"
+import { EnteringFromLeftAnimation, EnteringFromRightAnimation } from "./Animations/Entering"
+import { ExitingToLeftAnimation, ExitingToRightAnimation } from "./Animations/Exiting"
 
 // TODO(send-flow-v2): Add proper step types based on the logic implemented in each child step component
 type SendFlowStep = "selectAmount" | "insertAddress" | "summary"
@@ -35,9 +39,12 @@ type SendFlowState = {
 type NavigationProps = NativeStackNavigationProp<RootStackParamListHome, Routes.SEND_TOKEN>
 type RouteProps = NativeStackScreenProps<RootStackParamListHome, Routes.SEND_TOKEN>["route"]
 
+const ORDER: SendFlowStep[] = ["selectAmount", "insertAddress", "summary"]
+
 export const SendScreen = (): ReactElement => {
     const { LL } = useI18nContext()
     const theme = useTheme()
+    const { styles } = useThemedStyles(baseStyles)
     const navigation = useNavigation<NavigationProps>()
     const route = useRoute<RouteProps>()
     const [step, setStep] = useState<SendFlowStep>("selectAmount")
@@ -47,6 +54,8 @@ export const SendScreen = (): ReactElement => {
     const [isNextDisabled, setIsNextDisabled] = useState(true)
 
     const amountHandlerRef = useRef<(() => void) | null>(null)
+    const previousStep = useSharedValue<typeof step | undefined>(undefined)
+    const nextStep = useSharedValue<typeof step | undefined>(undefined)
 
     const handleClose = useCallback(() => {
         navigation.goBack()
@@ -57,33 +66,76 @@ export const SendScreen = (): ReactElement => {
         [handleClose],
     )
 
-    const goToInsertAddress = useCallback((amount: string, token: FungibleTokenWithBalance, fiatAmount?: string) => {
-        setFlowState(current => ({
-            ...current,
-            amount,
-            token,
-            fiatAmount,
-            // reset downstream state when amount/token changes
-            address: undefined,
-        }))
-        setStep("insertAddress")
-    }, [])
+    const goToInsertAddress = useCallback(
+        (amount: string, token: FungibleTokenWithBalance, fiatAmount?: string) => {
+            setFlowState(current => ({
+                ...current,
+                amount,
+                token,
+                fiatAmount,
+                // reset downstream state when amount/token changes
+                address: undefined,
+            }))
+            nextStep.value = "insertAddress"
+            previousStep.value = step
+            setStep("insertAddress")
+        },
+        [nextStep, previousStep, step],
+    )
 
-    const goToSummary = useCallback((address: string) => {
-        setFlowState(current => ({
-            ...current,
-            address,
-        }))
-        setStep("summary")
-    }, [])
+    const goToSummary = useCallback(
+        (address: string) => {
+            setFlowState(current => ({
+                ...current,
+                address,
+            }))
+            nextStep.value = "summary"
+            previousStep.value = step
+            setStep("summary")
+        },
+        [nextStep, previousStep, step],
+    )
 
     const goBackToSelectAmount = useCallback(() => {
+        nextStep.value = "selectAmount"
+        previousStep.value = step
         setStep("selectAmount")
-    }, [])
+    }, [nextStep, previousStep, step])
 
     const goBackToInsertAddress = useCallback(() => {
+        nextStep.value = "insertAddress"
+        previousStep.value = step
         setStep("insertAddress")
-    }, [])
+    }, [nextStep, previousStep, step])
+
+    const Entering = useCallback(
+        (values: EntryAnimationsValues) => {
+            "worklet"
+            if (!previousStep.value || !nextStep.value)
+                return {
+                    initialValues: values,
+                    animations: {},
+                }
+            if (ORDER.indexOf(nextStep.value) > ORDER.indexOf(previousStep.value))
+                return EnteringFromRightAnimation(values)
+            return EnteringFromLeftAnimation(values)
+        },
+        [nextStep.value, previousStep.value],
+    )
+
+    const Exiting = useCallback(
+        (values: ExitAnimationsValues) => {
+            "worklet"
+            if (!previousStep.value || !nextStep.value)
+                return {
+                    initialValues: values,
+                    animations: {},
+                }
+            if (ORDER.indexOf(nextStep.value) > ORDER.indexOf(previousStep.value)) return ExitingToLeftAnimation(values)
+            return ExitingToRightAnimation(values)
+        },
+        [nextStep.value, previousStep.value],
+    )
 
     const renderStep = useMemo(() => {
         // TODO(send-flow-v2): Implement proper step types based on the logic implemented in each child step component
@@ -133,7 +185,11 @@ export const SendScreen = (): ReactElement => {
             noBackButton
             headerTitleAlignment="center"
             headerRightElement={headerRightElement}
-            body={<BaseView flex={1}>{renderStep}</BaseView>}
+            fixedBody={
+                <Animated.View style={styles.flexElement} entering={Entering} exiting={Exiting} key={step}>
+                    {renderStep}
+                </Animated.View>
+            }
             footer={
                 <BaseView flexDirection="row" justifyContent="space-between" pt={16} bg={theme.colors.background}>
                     {footerConfig.left && (
@@ -160,3 +216,8 @@ export const SendScreen = (): ReactElement => {
         />
     )
 }
+
+const baseStyles = () =>
+    StyleSheet.create({
+        flexElement: { flex: 1, paddingHorizontal: 24 },
+    })
