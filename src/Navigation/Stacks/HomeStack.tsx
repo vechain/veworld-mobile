58fd94db2726a70f35452f5bd143c850c550d61f
paolampilla <paolampilla@gmail.com>
2025-12-10 12:28:33 +0100

    chore: add ff

--- src/Navigation/Stacks/HomeStack.tsx
+++ src/Navigation/Stacks/HomeStack.tsx
@@ -397,7 +397,7 @@ export const HomeStack = () => {
                         options={{ headerShown: false }}
                     />
                 )}
-                {betterWorldFeature.balanceScreen.collectibles.enabled && (
+                {betterWorldFeature.balanceScreen.sendCollectibles.enabled && (
                     <Screen name={Routes.SEND_NFT} component={SendNFTScreen} options={{ headerShown: false }} />
                 )}
                 {betterWorldFeature.balanceScreen.collectibles.enabled && (
