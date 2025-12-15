f9a24b390be4331ac4cb9f54d0477d0e0409e22f
Michael Martin <michael.martin1997@gmail.com>
2025-12-15 11:32:59 +0100

    feat: enhance useGetDappMetadataFromUrl to support strict URL comparison

--- src/Hooks/useFetchFeaturedDApps/useGetDappMetadata.ts
+++ src/Hooks/useFetchFeaturedDApps/useGetDappMetadata.ts
@@ -3,13 +3,23 @@ import { useAppSelector } from "~Storage/Redux/Hooks"
 import { selectFeaturedDapps } from "~Storage/Redux"
 import URIUtils from "~Utils/URIUtils"
 
-export const useGetDappMetadataFromUrl = (url: string) => {
+/**
+ * Get the dapp metadata from the url
+ * @param url - The url to get the dapp metadata from
+ * @param strict - If strict is true, the url will be compared strictly, otherwise it will be compared by the second level domain
+ * @returns The dapp metadata
+ */
+export const useGetDappMetadataFromUrl = (url: string, strict: boolean = true) => {
     const allDapps = useAppSelector(selectFeaturedDapps)
 
     const dappMetadata = useMemo(() => {
-        const foundDapp = allDapps.find(app => URIUtils.compareSecondLevelDomains(app.href, url))
+        const foundDapp = allDapps.find(app =>
+            strict
+                ? new URL(app.href).origin === new URL(url).origin
+                : URIUtils.compareSecondLevelDomains(app.href, url),
+        )
         return foundDapp
-    }, [allDapps, url])
+    }, [allDapps, url, strict])
 
     return dappMetadata
 }
