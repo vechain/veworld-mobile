09a997ecdb61aac1dc0760ea1907010f7ed376bf
Michael Martin <michael.martin1997@gmail.com>
2025-12-16 18:44:49 +0100

    refactor: streamline NFT transfer handling by consolidating feedback logic

--- src/TransferEventListener/Handlers/TransferEventHandlers.ts
+++ src/TransferEventListener/Handlers/TransferEventHandlers.ts
@@ -6,23 +6,14 @@ import { FeedbackSeverity, FeedbackType } from "~Components/Providers/FeedbackPr
 import { Feedback } from "~Components/Providers/FeedbackProvider/Events"
 import { NAVIGATION_REF, Routes } from "~Navigation"
 import { TranslationFunctions } from "~i18n"
+import { FetchIncomingTransfersResponse } from "~Networking/API/Types"
+import { AccountWithDevice } from "~Model"
 
-export const handleNFTTransfers = async (
-    { visibleAccounts, transfers, selectedAccount, network, updateNFTs }: NFTTransferHandlerProps,
+const showTransferReceivedFeedback = (
+    transfers: FetchIncomingTransfersResponse["data"],
+    selectedAccount: AccountWithDevice,
     LL: TranslationFunctions,
 ) => {
-    if (transfers.length === 0) return
-
-    // Update NFTs for accounts that have been changed
-    const changedAccounts = uniq([...transfers.map(t => t.to), ...transfers.map(t => t.from)])
-    changedAccounts.forEach(accountAddress => updateNFTs({ network, accountAddress }))
-
-    // Send one message. Only one will be displayed on screen so don't send multiple messages
-    const transfer = transfers[0]
-    const foundAccount = findFirstInvolvedAccount(visibleAccounts, transfer)
-    if (!foundAccount) return
-
-    // Send one message. Only one will be displayed on screen so don't send multiple messages for the selected account
     const selectedAccountTransfers = transfers.filter(t => AddressUtils.compareAddresses(t.to, selectedAccount.address))
 
     if (selectedAccountTransfers.length > 0) {
@@ -46,6 +37,24 @@ export const handleNFTTransfers = async (
     }
 }
 
+export const handleNFTTransfers = async (
+    { visibleAccounts, transfers, selectedAccount, network, updateNFTs }: NFTTransferHandlerProps,
+    LL: TranslationFunctions,
+) => {
+    if (transfers.length === 0) return
+
+    // Update NFTs for accounts that have been changed
+    const changedAccounts = uniq([...transfers.map(t => t.to), ...transfers.map(t => t.from)])
+    changedAccounts.forEach(accountAddress => updateNFTs({ network, accountAddress }))
+
+    // Send one message. Only one will be displayed on screen so don't send multiple messages
+    const transfer = transfers[0]
+    const foundAccount = findFirstInvolvedAccount(visibleAccounts, transfer)
+    if (!foundAccount) return
+
+    showTransferReceivedFeedback(transfers, selectedAccount, LL)
+}
+
 export const handleTokenTransfers = (
     { transfers, selectedAccount, updateBalances }: TokenTransferHandlerProps,
     LL: TranslationFunctions,
@@ -57,28 +66,7 @@ export const handleTokenTransfers = (
 
     changedAccounts.forEach(accountAddress => updateBalances({ accountAddress }))
 
-    // Send one message. Only one will be displayed on screen so don't send multiple messages for the selected account
-    const selectedAccountTransfers = transfers.filter(t => AddressUtils.compareAddresses(t.to, selectedAccount.address))
-
-    if (selectedAccountTransfers.length > 0) {
-        Feedback.show({
-            message:
-                selectedAccountTransfers.length > 1
-                    ? LL.FEEDBACK_N_TRANSFERS_RECEIVED({ count: selectedAccountTransfers.length })
-                    : LL.FEEDBACK_TRANSFER_RECEIVED(),
-            severity: FeedbackSeverity.SUCCESS,
-            type: FeedbackType.ALERT,
-            icon: "icon-arrow-down",
-            onPress: () => {
-                NAVIGATION_REF.navigate(Routes.HISTORY_STACK, {
-                    screen: Routes.HISTORY,
-                    params: {
-                        screen: Routes.ACTIVITY_ALL,
-                    },
-                })
-            },
-        })
-    }
+    showTransferReceivedFeedback(transfers, selectedAccount, LL)
 }
 
 export default { handleNFTTransfers, handleTokenTransfers }
