f3e7d05cef3caaafe279c77e4aaa49496f25300d
Michael Martin <52627078+Doublemme@users.noreply.github.com>
2025-12-18 15:30:16 +0100

    Fix :: fiat value on step 3 send flow (#3657)
    * fix(TokenReceiverCard): update fiat amount handling and dependencies in useEffect
    
    * feat: enhance truncateToMaxDecimals to support zero-padding for decimal values
    
    ---------
    
    Co-authored-by: Michael Martin <michael.martin1997@gmail.com>

--- src/Components/Reusable/Send/03-SummarySend/Components/TokenReceiverCard.tsx
+++ src/Components/Reusable/Send/03-SummarySend/Components/TokenReceiverCard.tsx
@@ -56,9 +56,9 @@ export const TokenReceiverCard = () => {
         )
 
         const [integerPart, decimalPart] = truncateToMaxDecimals(
-            nextFiatAmount.value,
+            isInputInFiat ? flowState.fiatAmount ?? "0" : nextFiatAmount.value ?? "0",
             // Always use 2 decimals for converted value
-            { kind: "fiat" },
+            { kind: "fiat", fillWithZeros: true },
         ).split(/[.,]/)
 
         const formatter = getNumberFormatter({
@@ -87,7 +87,16 @@ export const TokenReceiverCard = () => {
             }),
             displayFiatAmount: fiatValue,
         }
-    }, [decimalSeparator, exchangeRate, flowState.amount, flowState.fiatAmount, flowState.token, formatLocale, locale])
+    }, [
+        decimalSeparator,
+        exchangeRate,
+        flowState.amount,
+        flowState.fiatAmount,
+        flowState.token,
+        formatLocale,
+        locale,
+        isInputInFiat,
+    ])
 
     return (
         <Animated.View>
