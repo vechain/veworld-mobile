f64a258b27317d7d8d536d832aeadcb932f3755f
Michael Martin <michael.martin1997@gmail.com>
2026-01-29 09:41:45 +0100

    fix: remove duplicate vertical scroll indicator from BalanceScreen

--- src/Screens/Flows/App/BalanceScreen/BalanceScreen.tsx
+++ src/Screens/Flows/App/BalanceScreen/BalanceScreen.tsx
@@ -115,7 +115,6 @@ export const BalanceScreen = () => {
                     stickyHeaderIndices={[0]}
                     showsVerticalScrollIndicator={false}
                     style={styles.scrollViewRoot}
-                    showsVerticalScrollIndicator={false}
                     contentContainerStyle={styles.scrollViewContent}>
                     <Animated.View>
                         <AnimatedLinearGradient
