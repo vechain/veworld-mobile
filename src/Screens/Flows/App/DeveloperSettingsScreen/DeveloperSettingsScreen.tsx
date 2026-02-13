5bab10b9231224afdf7c701eef3b238682aed65d
hughlivingstone <hugh.livingstone@vechain.org>
2026-02-13 12:30:12 +0000

    feat: add smart wallet logout button in developer settings

--- src/Screens/Flows/App/DeveloperSettingsScreen/DeveloperSettingsScreen.tsx
+++ src/Screens/Flows/App/DeveloperSettingsScreen/DeveloperSettingsScreen.tsx
@@ -1,11 +1,18 @@
 import React from "react"
-import { BaseView, Layout } from "~Components"
+import { BaseButton, BaseView, Layout } from "~Components"
+import { useSmartWallet } from "~Hooks"
 import { useI18nContext } from "~i18n"
+import { DEVICE_TYPE } from "~Model"
+import { selectSelectedAccount, useAppSelector } from "~Storage/Redux"
 import { IndexerSettings } from "./Components/IndexerSettings"
 import { NotificationCenterSettings } from "./Components/NotificationCenterSettings"
 
 export const DeveloperSettingsScreen = () => {
     const { LL } = useI18nContext()
+    const { logout } = useSmartWallet()
+    const selectedAccount = useAppSelector(selectSelectedAccount)
+
+    const hasSelectedSmartWallet = selectedAccount?.device?.type === DEVICE_TYPE.SMART_WALLET
 
     return (
         <Layout
@@ -14,6 +21,7 @@ export const DeveloperSettingsScreen = () => {
                 <BaseView flexGrow={1} gap={8}>
                     <NotificationCenterSettings />
                     <IndexerSettings />
+                    {hasSelectedSmartWallet && <BaseButton title={LL.COMMON_BTN_SIGN_OUT()} action={logout} />}
                 </BaseView>
             }
         />
