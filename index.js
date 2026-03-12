ee270cc0299643847133f2d1754ba0f1ba6b1cba
Michael Martin <michael.martin1997@gmail.com>
2026-03-12 09:58:58 +0100

    chore: update react-native-fast-url and react-native-quick-base64 dependencies

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ import {
     useAppSelector,
 } from "~Storage/Redux"
 import * as Sentry from "@sentry/react-native"
-import "react-native-fast-url/src/polyfill"
+import "react-native-fast-url"
 import { InAppBrowserProvider } from "~Components/Providers/InAppBrowserProvider"
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
 import { clientPersister, queryClient, RQ_CACHE_MAX_AGE } from "~Api/QueryProvider"
