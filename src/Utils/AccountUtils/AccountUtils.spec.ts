e8b7bae39849361178ed94eca79ba126d9c46d6e
Michael Martin <michael.martin1997@gmail.com>
2026-03-12 16:26:36 +0100

    refactor: account utils tests

--- src/Utils/AccountUtils/AccountUtils.spec.ts
+++ src/Utils/AccountUtils/AccountUtils.spec.ts
@@ -3,8 +3,10 @@ import { Contact, ContactType, WalletAccount } from "~Model"
 import { getAccountForIndex, getNextIndex, isObservedAccount, nextAlias, updateAccountVns } from "./AccountUtils"
 import { Vns } from "~Model/Vns"
 
-const accounts = [TestHelpers.data.account1D1, TestHelpers.data.account2D1]
-const contact1 = TestHelpers.data.getContact(0, ContactType.KNOWN)
+const { account1D1, account2D1, getContact, device1, account5D1Observed, account4D1 } = TestHelpers.data
+
+const accounts = [account1D1, account2D1]
+const contact1 = getContact(0, ContactType.KNOWN)
 
 const vnsData: Vns[] = [
     { name: "test-dev.vet", address: "0xf077b491b355e64048ce21e3a6fc4751eeea77fa" },
@@ -24,13 +26,13 @@ describe("AccountUtils", () => {
     })
 
     it("getAccountForIndex - should throw when no xPub", () => {
-        expect(() => getAccountForIndex(0, { ...TestHelpers.data.device1, xPub: undefined }, 0)).toThrow(
+        expect(() => getAccountForIndex(0, { ...device1, xPub: undefined }, 0)).toThrow(
             "The XPub can't be null for HD devices",
         )
     })
 
     it("updateAccountVns - should return an account with the vnsName property", () => {
-        const account = updateAccountVns(TestHelpers.data.account1D1, vnsData) as WalletAccount
+        const account = updateAccountVns(account1D1, vnsData) as WalletAccount
         expect(account.vnsName).toBe("test-dev.vet")
     })
 
@@ -40,15 +42,15 @@ describe("AccountUtils", () => {
     })
 
     it("updateAccountVns - should return undefined if no address found in vnsData", () => {
-        const contact = updateAccountVns(TestHelpers.data.account2D1, vnsData) as WalletAccount
+        const contact = updateAccountVns(account2D1, vnsData) as WalletAccount
         expect(contact.vnsName).toBeUndefined()
     })
 
     it("isObservedAccount - should return true", () => {
-        expect(isObservedAccount(TestHelpers.data.account5D1Observed)).toBe(true)
+        expect(isObservedAccount(account5D1Observed)).toBe(true)
     })
 
     it("isObservedAccount - should return false", () => {
-        expect(isObservedAccount(TestHelpers.data.account4D1)).toBe(false)
+        expect(isObservedAccount(account4D1)).toBe(false)
     })
 })
